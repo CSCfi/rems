62aff22005e2ba895bd838c76e3f44cf718acc54
tjalkane <tjalkane@users.noreply.github.com>
2017-02-06 13:02:39 +0200

    fixed exception when trying to run handler tests and removed example db tests

--- test/clj/rems/test/handler.clj
+++ test/clj/rems/test/handler.clj
@@ -4,12 +4,10 @@
             [rems.handler :refer :all]))
 
 (deftest test-app
-  ;TODO FIXME
-  ;(testing "main route"
-  ;  (let [response ((app) (request :get "/"))]
-  ;    (is (= 200 (:status response)))))
+  (testing "main route"
+    (let [response (app (request :get "/"))]
+      (is (= 200 (:status response)))))
 
-  ;(testing "not-found route"
-  ;  (let [response ((app) (request :get "/invalid"))]
-  ;    (is (= 404 (:status response))))))
-)
+  (testing "not-found route"
+    (let [response (app (request :get "/invalid"))]
+      (is (= 404 (:status response))))))
