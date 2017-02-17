30adc61dc594e54b7dfa152cf35870fe9e342a0b
Markku Rontu <markku.rontu@iki.fi>
2017-02-17 14:04:36 +0200

    feat: document context namespace

--- src/clj/rems/context.clj
+++ src/clj/rems/context.clj
@@ -1,3 +1,7 @@
-(ns rems.context)
+(ns rems.context
+  "Collection of the global variables for REMS.
+
+   When referring, please make your use greppable with the prefix context,
+   i.e. context/*root-path*.")
 
 (def ^:dynamic *root-path* nil)
