e9b514e936dcaffc51892fc23bfbfed21fd823c1
Markku Rontu <markku.rontu@iki.fi>
2017-03-09 11:52:54 +0200

    feat: initial DB transfer script

---
+++ resources/sql/transfer.sql
@@ -0,0 +1,33 @@
+CREATE CAST (transfer.rms_workflow_visibility AS public.scope)
+WITH INOUT
+AS IMPLICIT;
+
+CREATE CAST (transfer.rms_application_form_meta_visibility AS public.scope)
+WITH INOUT
+AS IMPLICIT;
+
+DELETE FROM public.rms_resource CASCADE;
+DELETE FROM public.rms_resource_prefix CASCADE;
+DELETE FROM public.rms_workflow CASCADE;
+DELETE FROM public.rms_catalogue_item CASCADE;
+--DELETE FROM public.rms_application_form_meta CASCADE;
+
+--INSERT INTO public.rms_workflow (id, owneruserid, modifieruserid, title, fnlround, visibility, start, endt)
+--SELECT t.id, t.owneruserid, t.modifieruserid, t.title, t.fnlround, public.scope(t.visibility::text), t.start, t.end FROM transfer.rms_workflow t;
+INSERT INTO public.rms_workflow
+SELECT * FROM transfer.rms_workflow;
+
+INSERT INTO public.rms_resource_prefix
+SELECT * FROM transfer.rms_resource_prefix;
+
+INSERT INTO public.rms_resource
+SELECT * FROM transfer.rms_resource;
+
+INSERT INTO public.rms_application_form_meta
+SELECT * FROM transfer.rms_application_form_meta;
+
+INSERT INTO public.rms_catalogue_item
+SELECT * FROM transfer.rms_catalogue_item;
+
+DROP CAST IF EXISTS (transfer.rms_workflow_visibility AS public.scope);
+DROP CAST IF EXISTS (transfer.rms_application_form_meta_visibility AS public.scope);
