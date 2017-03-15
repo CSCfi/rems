8c87fcdae9893c954e23d4248deb014f5ee514c3
Markku Rontu <markku.rontu@iki.fi>
2017-03-15 13:41:54 +0200

    feat: update transfer documentation
    Transfer schema creation is now a separate step.

--- transfer-db.sh
+++ transfer-db.sh
@@ -19,7 +19,7 @@ docker run -it --link rems_mysql:mysql -v `readlink -f $1`:/tmp/data.sql --rm ma
 #docker run -it --rm --link rems_test:postgres postgres psql -h postgres -U rems
 
 # Create target schema in Postgres
-docker run -it --rm postgres psql -h $PGHOST -U $PGUSER -c 'DROP SCHEMA IF EXISTS transfer CASCADE; CREATE SCHEMA transfer;'
+#docker run -it --rm postgres psql -h $PGHOST -U postgres -c 'DROP SCHEMA IF EXISTS transfer CASCADE; CREATE SCHEMA transfer;' rems;
 
 # Load data from MariaDB into Postgres
 docker run -it --rm --link rems_mysql:mysql dimitri/pgloader pgloader --set "search_path='transfer'" --verbose mysql://root:rems_test@rems_mysql/rems_mysql postgresql://$PGUSER@$PGHOST/rems
