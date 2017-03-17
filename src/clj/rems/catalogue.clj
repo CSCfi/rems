aad8a91532012b0301b48de5691a82b374ec1ef0
Markku Rontu <markku.rontu@iki.fi>
2017-03-17 15:08:46 +0200

    refactor: split catalogue components to own file

---
+++ src/clj/rems/catalogue.clj
@@ -0,0 +1,58 @@
+(ns rems.catalogue
+  (:require [hiccup.element :refer [link-to image]]
+            [rems.cart :as cart]
+            [rems.form :as form]
+            [rems.text :refer :all]
+            [rems.db.core :as db]
+            [rems.context :as context]))
+
+;; TODO duplication between cart and catalogue to be factored out
+
+(defn get-catalogue-item-title [item]
+  (let [localized-title (get-in item [:localizations context/*lang* :title])]
+    (or localized-title (:title item))))
+
+(defn cart-item [item]
+  [:tr
+   [:td {:data-th ""} (get-catalogue-item-title item)]
+   [:td.actions {:data-th ""}
+    (cart/apply-button item)
+    (cart/remove-from-cart-button item)]])
+
+(defn cart-list [items]
+  (when-not (empty? items)
+    [:div.outer-cart
+     [:div.inner-cart
+      [:div.cart-title
+       [:i.fa.fa-shopping-cart]
+       [:span (text :t.cart/header)]]
+      [:table.rems-table.cart
+       (for [item (sort-by get-catalogue-item-title items)]
+         (cart-item item))]
+      [:div.full.actions (cart/checkout-cart-button)]]]))
+
+(defn urn-catalogue-item? [{:keys [resid]}]
+  (and resid (.startsWith resid "http://urn.fi")))
+
+(defn catalogue-item [item]
+  (let [resid (:resid item)
+        title (get-catalogue-item-title item)
+        component (if (urn-catalogue-item? item)
+                    [:a.catalogue-item-link {:href resid :target :_blank} title]
+                    title)]
+    [:tr
+     [:td {:data-th (text :t.catalogue/header)} component]
+     [:td.actions {:data-th ""} (cart/add-to-cart-button item)]]))
+
+(defn catalogue-list [items]
+  [:table.rems-table
+   [:tr
+    [:th (text :t.catalogue/header)]
+    [:th ""]]
+   (for [item (sort-by get-catalogue-item-title items)]
+     (catalogue-item item))])
+
+(defn catalogue []
+  (list
+   (cart-list (cart/get-cart-items))
+   (catalogue-list (db/get-localized-catalogue-items))))
--- test/clj/rems/test/contents.clj
+++ test/clj/rems/test/contents.clj
@@ -4,50 +4,9 @@
             [rems.context :as context]
             [rems.contents :refer :all]))
 
-;; TODO: factor out if needed elswwhere
+;; TODO: factor out if needed elsewhere
 (use-fixtures
   :once
   (fn [f]
     (binding [context/*tempura* (fn [[k]] (str k))]
       (f))))
-
-(defn check-catalogue-item-text [props]
-  (hiccup-text (first (hiccup-find [:td] (catalogue-item props)))))
-
-(deftest test-catalogue-item
-  (testing "catalogue item with urn"
-    (let [urn "http://urn.fi/urn:nbn:fi:lb-201403262"
-          c (catalogue-item {:title "U" :resid urn})
-          link (first (hiccup-find [:a] c))]
-      (is (= :a.catalogue-item-link (first link)) "is a link")
-      (is (= urn (:href (second link))) "links to the urn")
-      (is (= :_blank (:target (second link))) "opens in new tab")))
-
-  (testing "catalogue item with localizations"
-    (let [props {:title "NO" :localizations {:fi {:title "FI"} :en {:title "EN"}}}]
-      (testing "without localizations"
-        (is (= "NO" (check-catalogue-item-text (dissoc props :localizations)))))
-      (testing "for Finnish session"
-        (binding [context/*lang* :fi]
-          (is (= "FI" (check-catalogue-item-text props)))))
-      (testing "for English session"
-        (binding [context/*lang* :en]
-          (is (= "EN" (check-catalogue-item-text props))))))))
-
-(defn check-row-text [row text]
-  (is (= text (hiccup-text (first (hiccup-find [:td] row))))))
-
-(deftest test-catalogue-list
-  (let [c (catalogue-list [{:title "B"} {:title "A"} {:title "C"}])
-        rows (rest (hiccup-find [:tr] c))]
-    (is (= 3 (count rows)))
-    (check-row-text (nth rows 0) "A")
-    (check-row-text (nth rows 1) "B")
-    (check-row-text (nth rows 2) "C")))
-
-(deftest test-cart-list
-  (let [c (cart-list [{:title "D"} {:title "C"}])
-        rows (hiccup-find [:tr] c)]
-    (is (= 2 (count rows)))
-    (check-row-text (first rows) "C")
-    (check-row-text (second rows) "D")))
