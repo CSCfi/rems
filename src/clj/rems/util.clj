81bbdf880201e57b0f6805b7624586fdcc8a35d4
Markku Rontu <markku.rontu@iki.fi>
2017-03-17 15:37:15 +0200

    refactor: introduce util file

---
+++ src/clj/rems/util.clj
@@ -0,0 +1,14 @@
+(ns rems.util)
+
+(defn index-by
+  "Index the collection coll with given keys ks.
+
+  Result is a map indexed by the first key
+  that contains a map indexed by the second key."
+  [ks coll]
+  (if (empty? ks)
+    (first coll)
+    (->> coll
+         (group-by (first ks))
+         (map (fn [[k v]] [k (index-by (rest ks) v)]))
+         (into {}))))
