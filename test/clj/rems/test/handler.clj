803e72476be1d088407dd086921c638064ff36e0
Markku Rontu <markku.rontu@iki.fi>
2017-03-21 16:09:30 +0200

    feat: test authorization (FAILING)

--- test/clj/rems/test/handler.clj
+++ test/clj/rems/test/handler.clj
@@ -3,6 +3,8 @@
             [clojure.string :as s]
             [hickory.core :as h]
             [ring.mock.request :refer :all]
+            [ring.util.codec :refer [form-decode]]
+            [hiccup-find.core :refer :all]
             [rems.handler :refer :all]
             [mount.core :as mount]
             [rems.db.core :as db]
@@ -22,6 +24,8 @@
     (f)
     (mount/stop)))
 
+;;; test helpers
+
 (defn- pass-cookies [to-request from-response]
   (let [set-cookie (get-in from-response [:headers "Set-Cookie"])]
     (header to-request "cookie" (s/join "; " set-cookie))))
@@ -30,6 +34,56 @@
   (let [token-regex #"<input id=\"__anti-forgery-token\" name=\"__anti-forgery-token\" type=\"hidden\" value=\"([^\"]*)\">"
         [_ token] (re-find token-regex (:body response))]
     token))
+
+(defn new-context [app] {:app app})
+
+(defn ctx->html [ctx]
+  (-> ctx (:response) (:body) (h/parse) (h/as-hiccup) (second)))
+
+(defn dispatch [{:keys [app cookie csrf-token response] :as ctx} req]
+  (let [req (if cookie
+              (header req "cookie" (s/join "; " cookie))
+              req)
+        send-token? (and (= :post (:request-method req)) csrf-token)
+        req (if send-token?
+              (body req (merge {"__anti-forgery-token" csrf-token}
+                               (when (:body req) (form-decode (slurp (:body req))))))
+              req)]
+    (let [response (app req)
+          set-cookie (get-in response [:headers "Set-Cookie"])]
+      (println (:request-method req)
+               (:uri req)
+               (remove nil? [(when cookie :send-cookie)
+                             (when send-token? :send-csrf-token)])
+               "=>"
+               (:status response)
+               (remove nil? [(when set-cookie :set-cookie)]))
+      (merge ctx
+             {:cookie (or set-cookie cookie)
+              :csrf-token (get-csrf-token response)
+              :status (:status response)
+              :response response}
+             ))))
+
+(defn login [ctx username]
+  (dispatch ctx (-> (request :get "/Shibboleth.sso/Login")
+                    (assoc :fake-username username))))
+
+(defn follow-redirect [ctx]
+  (assert (contains? #{302 303} (:status ctx)) (str "Not redirect " (:status ctx) ""))
+  (let [location (get-in (:response ctx) [:headers "Location"])
+        location (subs location 16)]
+    (dispatch ctx (request :get location))))
+
+(defn print-inputs [ctx]
+  (let [html (ctx->html ctx)]
+    (clojure.pprint/pprint (hiccup-find [:input] html)))
+  ctx)
+
+
+
+
+;;; tests
 
 (deftest test-app
   (testing "main route"
@@ -54,6 +108,8 @@
     (let [response (app (request :get "/invalid"))]
       (is (= 404 (:status response))))))
 
+
+
 (deftest test-csrf
   (testing "cart routes"
     (testing "without CSRF token"
@@ -73,6 +129,8 @@
             response (app req)]
         (is (= 303 (:status response)))))))
 
+
+
 (deftest test-language-switch
   (let [login (app (request :get "/Shibboleth.sso/Login"))
         catalogue (app (-> (request :get "/catalogue")
@@ -89,3 +147,25 @@
           "language switch redirects back")
       (is (.contains (:body catalogue-fi) "kori")
           "language switches to finnish"))))
+
+
+
+(deftest test-authz
+  (testing "when one user makes an application"
+    (-> (new-context app)
+        (login "alice")
+        (follow-redirect)
+        (dispatch (request :post "/cart/add" {"id" "1"}))
+        (follow-redirect)
+        (cart-contains-item "1")
+        (dispatch (request :get "/form/1"))
+        (dispatch (request :post "/form/1/save" {"field2" "alice field2"}))
+        (follow-redirect))
+    (testing "and another user goes to the same application"
+      (let [html (-> (new-context app)
+                     (login "bob")
+                     (follow-redirect)
+                     (dispatch (request :get "/form/1/1"))
+                     ctx->html)]
+        (is (not= "alice field2" (:value (hiccup-attrs (first (hiccup-find [:input {:name "field2"}] html)))))
+            "bob shouldn't see alice's applications")))))
