3bfbea757578322a007d4d832079ce37ff72b647
Markku Rontu <markku.rontu@iki.fi>
2017-04-06 13:01:27 +0300

    fix: use get-user-id

--- src/clj/rems/db/approvals.clj
+++ src/clj/rems/db/approvals.clj
@@ -3,12 +3,12 @@
   (:require [rems.context :as context]
             [rems.db.core :as db]
             [rems.db.catalogue :refer [get-localized-catalogue-item]]
-            [rems.util :refer [index-by]]
+            [rems.util :refer [index-by get-user-id]]
             [rems.auth.util :refer [throw-unauthorized]]))
 
 (defn get-approvals []
   (doall
-   (for [a (db/get-applications {:approver context/*user* :state "applied"})]
+   (for [a (db/get-applications {:approver (get-user-id) :state "applied"})]
      (assoc a :catalogue-item
             (get-in (get-localized-catalogue-item {:id (:catid a)})
                     [:localizations context/*lang*])))))
