d6b5dfe53be6ca8d7cf1e79f5105829e897744e5
Joel Kaasinen <joel.kaasinen@nitor.fi>
2017-04-20 10:08:43 +0300

    nicer test data
    - users and roles
    - realistic form

--- src/clj/rems/db/test_data.clj
+++ src/clj/rems/db/test_data.clj
@@ -1,30 +1,69 @@
 (ns rems.db.test-data
   "Populating the database with nice test data."
-  (:require [rems.db.core :as db]))
+  (:require [rems.db.core :as db]
+            [rems.db.roles :as roles]))
+
+(defn- create-users-and-roles! []
+  ;; users provided by the fake login
+  (db/add-user! {:user "developer" :userattrs nil})
+  (roles/add-role! "developer" :applicant)
+  (roles/add-role! "developer" :approver)
+  (db/add-user! {:user "alice" :userattrs nil})
+  (roles/add-role! "alice" :applicant)
+  (db/add-user! {:user "bob" :userattrs nil})
+  (roles/add-role! "bob" :approver)
+  ;; a user to own things
+  (db/add-user! {:user "owner" :userattrs nil}))
+
+(defn- create-basic-form! []
+  "Creates a bilingual form with all supported field types. Returns id of the form meta."
+  (let [meta (db/create-form-meta! {:title "metatitle" :user "owner"})
+        form-en (db/create-form! {:title "Yksinkertainen lomake" :user "owner"})
+        form-fi (db/create-form! {:title "Basic application" :user "owner"})
+
+        name-fi (db/create-form-item!
+                 {:title "Projektin nimi" :type "text" :inputprompt "Projekti"
+                  :optional false :user "owner" :value 0})
+        name-en (db/create-form-item!
+                 {:title "Project name" :type "text" :inputprompt "Project"
+                  :optional false :user "owner" :value 0})
+        purpose-fi (db/create-form-item!
+                    {:title "Projektin tarkoitus" :type "texta"
+                     :inputprompt "Projektin tarkoitus on ..." :optional false
+                     :user "owner" :value 0})
+        purpose-en (db/create-form-item!
+                    {:title "Purpose of the project" :type "texta"
+                     :inputprompt "The purpose of the project is to ..." :optional false
+                     :user "owner" :value 0})
+        duration-en (db/create-form-item!
+                     {:title "Duration of the project" :type "text"
+                      :inputprompt "YYYY-YYYY" :optional true
+                      :user "owner" :value 0})
+        duration-fi (db/create-form-item!
+                     {:title "Projektin kesto" :type "text"
+                      :inputprompt "YYYY-YYYY" :optional true
+                      :user "owner" :value 0})]
+    (db/link-form-meta! {:meta (:id meta) :form (:id form-en) :lang "en" :user "owner"})
+    (db/link-form-meta! {:meta (:id meta) :form (:id form-fi) :lang "fi" :user "owner"})
+
+    ;; link out of order for less predictable row ids
+    (db/link-form-item! {:form (:id form-en) :itemorder 3 :optional false :item (:id purpose-en) :user "owner"})
+    (db/link-form-item! {:form (:id form-en) :itemorder 2 :optional true :item (:id duration-en) :user "owner"})
+    (db/link-form-item! {:form (:id form-en) :itemorder 1 :optional false :item (:id name-en) :user "owner"})
+    (db/link-form-item! {:form (:id form-fi) :itemorder 1 :optional false :item (:id name-fi) :user "owner"})
+    (db/link-form-item! {:form (:id form-fi) :itemorder 3 :optional false :item (:id purpose-fi) :user "owner"})
+    (db/link-form-item! {:form (:id form-fi) :itemorder 2 :optional true :item (:id duration-fi) :user "owner"})
+    (:id meta)))
 
 (defn create-test-data! []
-  (let [meta (create-form-meta! {:title "metatitle" :user 0})
-        form-en (create-form! {:title "entitle" :user 0})
-        form-fi (create-form! {:title "fititle" :user 0})
-        item-c (create-form-item!
-                {:title "C" :type "text" :inputprompt "prompt" :user 0 :value 0})
-        item-a (create-form-item!
-                {:title "A" :type "text" :inputprompt "prompt" :user 0 :value 0})
-        item-b (create-form-item!
-                {:title "B" :type "text" :inputprompt "prompt" :user 0 :value 0})]
-    (link-form-meta! {:meta (:id meta) :form (:id form-en) :lang "en" :user 0})
-    (link-form-meta! {:meta (:id meta) :form (:id form-fi) :lang "fi" :user 0})
-    (link-form-item! {:form (:id form-en) :itemorder 2 :item (:id item-b) :user 0})
-    (link-form-item! {:form (:id form-en) :itemorder 1 :item (:id item-a) :user 0})
-    (link-form-item! {:form (:id form-en) :itemorder 3 :item (:id item-c) :user 0})
-    (link-form-item! {:form (:id form-fi) :itemorder 1 :item (:id item-a) :user 0})
-
-    (create-resource! {:id 1 :resid "http://urn.fi/urn:nbn:fi:lb-201403262" :prefix "nbn" :modifieruserid 1})
-    (create-catalogue-item! {:title "ELFA Corpus"
-                             :form (:id meta)
-                             :resid 1
-                             :wfid nil})
-    (create-catalogue-item! {:title "B"
-                             :form nil
-                             :resid nil
-                             :wfid nil})))
+  (create-users-and-roles!)
+  (let [meta (create-basic-form!)]
+    (db/create-resource! {:id 1 :resid "http://urn.fi/urn:nbn:fi:lb-201403262" :prefix "nbn" :modifieruserid 1})
+    (db/create-catalogue-item! {:title "ELFA Corpus"
+                                :form (:id meta)
+                                :resid 1
+                                :wfid nil})
+    (db/create-catalogue-item! {:title "B"
+                                :form nil
+                                :resid nil
+                                :wfid nil})))
