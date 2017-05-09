683efc3705cdd5f549e58acc3b5302b27dc26139
Markku Rontu <markku.rontu@iki.fi>
2017-05-09 17:40:29 +0300

    fix: localizations to fit available space

--- src/clj/rems/locales.clj
+++ src/clj/rems/locales.clj
@@ -55,7 +55,7 @@
                               :applied "Applied"
                               :approved "Approved"
                               :rejected "Rejected"
-                              :returned "Returned to applicant"
+                              :returned "Returned"
                               :unknown "Unknown"}
                      :empty "You don't have any applications."}
       :approvals {:application "Application"
@@ -125,7 +125,7 @@
                               :applied "Haettu"
                               :approved "Hyväksytty"
                               :rejected "Hylätty"
-                              :returned "Palautettu hakijalle"
+                              :returned "Palautettu"
                               :unknown "Tuntematon"}
                      :empty "Sinulla ei ole hakemuksia."}
       :approvals {:application "Hakemus"
