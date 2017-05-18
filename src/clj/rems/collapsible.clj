f58f7f489c8ddbb633dc1ea4486076ef99efc62a
Markku Rontu <markku.rontu@iki.fi>
2017-05-18 13:43:58 +0300

    feat: hide +/- when not openable

--- src/clj/rems/collapsible.clj
+++ src/clj/rems/collapsible.clj
@@ -2,11 +2,15 @@
   (:require [rems.guide :refer :all]))
 
 (defn- header
-  [id expanded title]
-  [:div.card-header.clickable {:onclick (str "$('#" id "').collapse('toggle')")}
-   [:a.card-title (merge {:data-toggle "collapse" :data-parent "#accordion" :href (str "#" id) :aria-expanded expanded :aria-controls id}
-                         (when-not expanded {:class "collapsed"}))
-    title]])
+  [id expanded title openable?]
+  [:div.card-header (when openable?
+                      {:class "clickable"
+                       :onclick (str "$('#" id "').collapse('toggle')")})
+   (if openable?
+     [:a.card-title (merge {:data-toggle "collapse" :data-parent "#accordion" :href (str "#" id) :aria-expanded expanded :aria-controls id}
+                           (when-not expanded {:class "collapsed"}))
+      title]
+     [:span.card-title title])])
 
 (defn- block [id expanded content]
   (let [classes (str "collapse" (when expanded " show"))]
@@ -14,17 +18,18 @@
      content]))
 
 (defn component [id expanded title content]
-   [:div.collapse-wrapper
-    (header id expanded title)
-    (block id expanded content)])
+  (let [openable? (not-empty content)]
+    [:div.collapse-wrapper
+     (header id expanded title openable?)
+     (block id expanded content)]))
 
 (defn guide
   []
   (list (example "Collapsible component expanded by default"
-           (list
-             [:div#accordion
-              (component "hello" true "Collapse expanded" [:p "I am content"])]))
+                 (list
+                  [:div#accordion
+                   (component "hello" true "Collapse expanded" [:p "I am content"])]))
         (example "Collapsible component closed by default"
-           (list
-             [:div#accordion
-              (component "hello2" false "Collapse minimized" [:p "I am content"])]))))
+                 (list
+                  [:div#accordion
+                   (component "hello2" false "Collapse minimized" [:p "I am content"])]))))
