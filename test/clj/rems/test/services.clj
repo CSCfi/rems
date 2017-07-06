0c48fd19b8e15d95f594ad66cc6516aef5d6ed34
Markku Rontu <markku.rontu@iki.fi>
2017-07-06 16:24:26 +0300

    fix: tests to use test data

--- test/clj/rems/test/services.clj
+++ test/clj/rems/test/services.clj
@@ -1,9 +1,26 @@
-(ns rems.test.services
+(ns ^:integration rems.test.services
   (:require [cheshire.core :refer [generate-string parse-stream]]
             [clojure.java.io]
             [clojure.test :refer :all]
+            [luminus-migrations.core :as migrations]
+            [mount.core :as mount]
+            [rems.config :refer [env]]
+            [rems.db.core :as db]
+            [rems.db.test-data :as test-data]
             [rems.handler :refer :all]
             [ring.mock.request :refer :all]))
+
+(use-fixtures
+  :once
+  (fn [f]
+    (mount/start
+     #'rems.config/env
+     #'rems.env/*db*)
+    (db/assert-test-database!)
+    (migrations/migrate ["reset"] (select-keys env [:database-url]))
+    (test-data/create-test-data!)
+    (f)
+    (mount/stop)))
 
 (defn authenticate [request api-key user-id]
   (-> request
@@ -22,41 +39,48 @@
   (let [api-key "42"
         user-id "alice"
         resource-id 2]
-    (testing "saving application"
+    (testing "saving"
       (let [response (-> (request :put (str "/api/application/" resource-id))
                          (authenticate api-key user-id)
                          (json {:operation "save"
-                                :items {2 "ensimmäinen"}})
+                                :items {2 "REST-Test"}})
                          app)
             cmd-response (read-body response)
             application-id (:id cmd-response)]
-        (println "application-id" application-id)
         (is (:success cmd-response))
+        (is (not (:errors cmd-response)))
         (is (= "draft" (:state cmd-response)))
         (is (not (:valid cmd-response)))
-        (is (= ["Field \"Additional Information\" is required."]
+        (is (= ["Field \"Purpose of the project\" is required."
+                "Field \"CC Attribution 4.0\" is required."
+                "Field \"License\" is required."]
                (:validation cmd-response)))
-        (testing "retrieving application"
+        (testing "retrieving"
           (let [response (-> (request :get (str "/api/application/" resource-id "/" application-id))
                              (authenticate api-key user-id)
                              app)
                 application (read-body response)]
-            (is (= 2 (:id application)))
+            (is (not (:errors application)))
+            (is (= application-id (:id (:application application))))
             (is (= "draft" (:state (:application application))))
-            (is (empty? (:licenses application)))
-            (is (= 2 (count (:items application))))
+            (is (= 2 (count (:licenses application))))
+            (is (= 3 (count (:items application))))
             ))
-        (testing "sending application"
+        (testing "sending"
           (let [response (-> (request :put (str "/api/application/" resource-id))
                              (authenticate api-key user-id)
                              (json {:operation "send"
                                     :application-id application-id
-                                    :items {2 "ensimmäinen"
-                                            8 "second"}})
+                                    :items {2 "REST-Test"
+                                            5 "2017-2018"
+                                            4 "The purpose is to test this REST service.}"}
+                                    :licenses {1 "approved" 2 "approved"}})
                              app)
                 cmd-response (read-body response)]
+            (is (:success cmd-response))
+            (is (not (:errors cmd-response)))
             (is (= application-id (:id cmd-response)))
             (is (= "applied" (:state cmd-response)))
-            (is (:success cmd-response))
             (is (:valid cmd-response))
+            (is (empty? (:validation cmd-response)))
             ))))))
