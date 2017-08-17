eeaa40dc47d7d1b6ffcc1bc0808b0b89fc8fb4c3
Joel Kaasinen <joel.kaasinen@nitor.fi>
2017-08-17 12:55:20 +0300

    adding missing rems.test.tempura requires
    this fixes cloverage

--- test/clj/rems/test/cart.clj
+++ test/clj/rems/test/cart.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer :all]
             [hiccup-find.core :refer :all]
             [rems.cart :as cart]
+            rems.test.tempura
             [ring.mock.request :refer :all]))
 
 (use-fixtures :once rems.test.tempura/fake-tempura-fixture)
--- test/clj/rems/test/catalogue.clj
+++ test/clj/rems/test/catalogue.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer :all]
             [hiccup-find.core :refer :all]
             [rems.catalogue :refer :all]
-            [rems.context :as context]))
+            [rems.context :as context]
+            rems.test.tempura))
 
 (use-fixtures :once rems.test.tempura/fake-tempura-fixture)
 
