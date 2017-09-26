6a697faae307bbf20f9eee3ee21b58a0b7d4b665
tjalkane <tjalkane@users.noreply.github.com>
2017-09-26 17:39:02 +0100

    fix definition names

--- test/clj/rems/test/locales.clj
+++ test/clj/rems/test/locales.clj
@@ -17,9 +17,9 @@
     (f)
     (mount/stop)))
 
-(def loc-fi (read-string (slurp (io/resource "translations/en-GB.edn"))))
+(def loc-en (read-string (slurp (io/resource "translations/en-GB.edn"))))
 
-(def loc-en (read-string (slurp (io/resource "translations/fi.edn"))))
+(def loc-fi (read-string (slurp (io/resource "translations/fi.edn"))))
 
 (defn map-structure
   "Recurse into map m and replace all leaves with true."
