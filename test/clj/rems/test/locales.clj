5f47eb76f998835a77e1285af9545c9322ea925e
tjalkane <tjalkane@users.noreply.github.com>
2017-09-26 17:26:59 +0100

    test that all applications events and states have localizations

--- test/clj/rems/test/locales.clj
+++ test/clj/rems/test/locales.clj
@@ -1,6 +1,25 @@
-(ns rems.test.locales
+(ns ^:integration rems.test.locales
   (:require [clojure.java.io :as io]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [luminus-migrations.core :as migrations]
+            [mount.core :as mount]
+            [rems.config :refer [env]]
+            [rems.db.core :as db]))
+
+(use-fixtures
+  :once
+  (fn [f]
+    (mount/start
+     #'rems.config/env
+     #'rems.env/*db*)
+    (db/assert-test-database!)
+    (migrations/migrate ["reset"] (select-keys env [:database-url]))
+    (f)
+    (mount/stop)))
+
+(def loc-fi (read-string (slurp (io/resource "translations/en-GB.edn"))))
+
+(def loc-en (read-string (slurp (io/resource "translations/fi.edn"))))
 
 (defn map-structure
   "Recurse into map m and replace all leaves with true."
@@ -9,5 +28,25 @@
     (reduce-kv (fn [m k v] (assoc m k (transform v))) {} m)))
 
 (deftest test-all-languages-defined
-  (is (= (map-structure (read-string (slurp (io/resource "translations/en-GB.edn"))))
-         (map-structure (read-string (slurp (io/resource "translations/fi.edn")))))))
+  (is (= (map-structure loc-en)
+         (map-structure loc-fi))))
+
+(deftest test-all-state-localizations
+  (is (= (-> (:states (:applications (:t loc-en)))
+             (dissoc :unknown)
+             (keys)
+             (sort))
+         (->> (rems.db.core/get-application-states)
+              (map :unnest)
+              (map keyword)
+              (sort)))))
+
+(deftest test-all-event-localizations
+  (is (= (-> (:events (:applications (:t loc-en)))
+             (dissoc :unknown)
+             (keys)
+             (sort))
+         (->> (rems.db.core/get-application-event-types)
+              (map :unnest)
+              (map keyword)
+              (sort)))))
