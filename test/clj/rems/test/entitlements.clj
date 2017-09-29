a3e2c37b1c45ef0178a44d4f268ad4a272040b24
Joel Kaasinen <joel.kaasinen@nitor.fi>
2017-09-29 11:25:13 +0300

    add catappid to entitlement export tests

--- test/clj/rems/test/entitlements.clj
+++ test/clj/rems/test/entitlements.clj
@@ -11,15 +11,17 @@
 (deftest test-get-entitlements-for-export
   (with-redefs [db/get-entitlements-for-export
                 (fn []
-                  [{:resid "res1" :userid "user1" :start (time/date-time 2001 10 11)}
-                   {:resid "res2" :userid "user2" :start (time/date-time 2002 10 11)}])]
+                  [{:resid "res1" :catappid 11 :userid "user1" :start (time/date-time 2001 10 11)}
+                   {:resid "res2" :catappid 12 :userid "user2" :start (time/date-time 2002 10 11)}])]
     (binding [context/*roles* #{:approver}]
       (let [lines (split-lines (#'entitlements/get-entitlements-for-export))]
         (is (= 3 (count lines)))
         (is (.contains (nth lines 1) "res1"))
         (is (.contains (nth lines 1) "2001"))
+        (is (.contains (nth lines 1) "11"))
         (is (.contains (nth lines 2) "res2"))
-        (is (.contains (nth lines 2) "2002"))))
+        (is (.contains (nth lines 2) "2002"))
+        (is (.contains (nth lines 2) "12"))))
     (binding [context/*roles* #{:applicant :reviewer}]
       (is (thrown? NotAuthorizedException
                    (#'entitlements/get-entitlements-for-export))))))
