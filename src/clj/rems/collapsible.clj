3c92283d983663419b7ad153f18a9577e6d5b140
Markku Rontu <markku.rontu@iki.fi>
2017-10-26 15:36:57 +0300

    feat: better collapse with content link

--- src/clj/rems/collapsible.clj
+++ src/clj/rems/collapsible.clj
@@ -1,27 +1,29 @@
 (ns rems.collapsible
-  (:require [rems.guide :refer :all]))
+  (:require [rems.guide :refer :all]
+            [rems.text :refer :all]))
 
 (defn- header
-  [id expanded title openable?]
-  [:div.card-header (when openable?
-                      {:class "clickable"
-                       :onclick (str "$('#" id "').collapse('toggle')")})
-   (if openable?
-     [:a.card-title (merge {:data-toggle "collapse" :href (str "#" id) :aria-expanded expanded :aria-controls id}
-                           (when-not expanded {:class "collapsed"}))
-      title]
-     [:span.card-title title])])
+  [id expanded title]
+  [:div.card-header
+   [:span.card-title title]])
 
 (defn- block [id expanded content]
-  (let [classes (str "collapse" (when expanded " show"))]
-    [:div.collapse-content {:id id :class classes}
-     content]))
+  [:div.collapse-content
+   [:div.collapse {:id id :class (when expanded "show")}
+    content]
+   [:div.collapse {:id (str id "more") :class (when-not expanded "show")}
+    [:a {:onclick (str "$('#" id "').collapse('show'), $('#" id "more').collapse('hide'), $('#" id "less').collapse('show')")}
+     (text :t.collapse/show-more)]]
+   [:div.collapse {:id (str id "less") :class (when expanded "show")}
+    [:a {:onclick (str "$('#" id "').collapse('hide'), $('#" id "more').collapse('show'), $('#" id "less').collapse('hide')")}
+     (text :t.collapse/show-less)]
+    ]])
 
 (defn component [id expanded title content]
-  (let [openable? (not-empty content)]
-    [:div.collapse-wrapper
-     (header id expanded title openable?)
-     (block id expanded content)]))
+  [:div.collapse-wrapper
+   (header id expanded title)
+   (when content
+     (block id expanded content))])
 
 (defn guide
   []
