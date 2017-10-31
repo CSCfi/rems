72938e3598028957f8129d090a283049585f615c
Markku Rontu <markku.rontu@iki.fi>
2017-10-31 13:46:04 +0200

    refactor: use id at top level
    id + "collapse" is the id for the collapsing div

--- src/clj/rems/collapsible.clj
+++ src/clj/rems/collapsible.clj
@@ -10,13 +10,13 @@
 (defn- show-more-button
   [id expanded]
   [:div.collapse.collapse-toggle {:id (str id "more") :class (when-not expanded "show")}
-   [:a.text-primary {:onclick (str "$('#" id "').collapse('show'), $('#" id "more').collapse('hide'), $('#" id "less').collapse('show')")}
+   [:a.text-primary {:onclick (str "$('#" id "collapse').collapse('show'), $('#" id "more').collapse('hide'), $('#" id "less').collapse('show')")}
     (text :t.collapse/show-more)]])
 
 (defn- show-less-button
   [id expanded]
   [:div.collapse.collapse-toggle {:id (str id "less") :class (when expanded "show")}
-   [:a.text-primary {:onclick (str "$('#" id "').collapse('hide'), $('#" id "more').collapse('show'), $('#" id "less').collapse('hide')")}
+   [:a.text-primary {:onclick (str "$('#" id "collapse').collapse('hide'), $('#" id "more').collapse('show'), $('#" id "less').collapse('hide')")}
     (text :t.collapse/show-less)]
    ])
 
@@ -25,12 +25,13 @@
    [:div content-always]
    (when-not (empty? content-hideable)
      (list
-      [:div.collapse {:id id :class (when expanded "show")} content-hideable]
+      [:div.collapse {:id (str id "collapse") :class (when expanded "show")} content-hideable]
       (show-more-button id expanded)
       (show-less-button id expanded)))])
 
 (defn component [{:keys [id class open? title always collapse]}]
-  [:div.collapse-wrapper {:class class}
+  [:div.collapse-wrapper {:id id
+                          :class class}
    (header title)
    (when (or always collapse)
      (block id open? always collapse))])
