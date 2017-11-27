d007f70c9b6e250fb4c29eeed32303cd71c90807
Markku Rontu <markku.rontu@iki.fi>
2017-11-27 10:49:06 +0200

    fix: down migration

--- resources/migrations/20171122134329-catalogue-item-state-changes.down.sql
+++ resources/migrations/20171122134329-catalogue-item-state-changes.down.sql
@@ -1,6 +1,4 @@
 ALTER TABLE catalogue_item_state DROP CONSTRAINT catalogue_item_state_pkey;
 --;;
-ALTER TABLE catalogue_item_state ADD COLUMN id;
---;;
-ALTER TABLE catalogue_item_state ADD PRIMARY KEY (id);
+ALTER TABLE catalogue_item_state ADD COLUMN id serial NOT NULL PRIMARY KEY;
 --;;
