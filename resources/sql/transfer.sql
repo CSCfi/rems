34504d8b40d5cb1a6e1cdbbc0b2d88e4cb1dfc73
Markku Rontu <markku.rontu@iki.fi>
2017-11-30 13:22:09 +0200

    feat: select latest row to update

--- resources/sql/transfer.sql
+++ resources/sql/transfer.sql
@@ -182,7 +182,11 @@ INSERT INTO public.catalogue_item_localization
 SELECT * FROM transfer.rms_catalogue_item_localization;
 
 UPDATE public.catalogue_item
-SET state = (SELECT CAST(cis.state::text AS item_state) FROM transfer.rms_catalogue_item_state cis WHERE catalogue_item.id = cis.catid);
+SET state = (SELECT CAST(cis.state::text AS item_state)
+             FROM transfer.rms_catalogue_item_state cis
+             WHERE catalogue_item.id = cis.catid
+             ORDER BY cis.start DESC
+             LIMIT 1);
 
 -- licenses
 
