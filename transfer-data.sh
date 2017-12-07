eae3f4c6f6792c7a9223efa515edf9fda101d18f
Markku Rontu <markku.rontu@iki.fi>
2017-12-07 19:43:16 +0200

    feat: rewrite as transfer-data

--- transfer-data.sh
+++ transfer-data.sh
@@ -9,19 +9,10 @@ sleep 30
 # Load dump
 docker run -i --link rems_mysql:mysql --rm mariadb mysql -hmysql -uroot rems_mysql < $1
 
-# Check contents of MariaDB
-#docker run -it --link rems_mysql:mysql --rm mariadb mysql -hmysql -uroot --execute="USE rems_mysql; SELECT * from rms_catalogue_item;"
-
-# Start mysql client
-#docker run -it --link rems_mysql:mysql --rm mariadb mysql -hmysql -uroot
-
-# Create target schema in Postgres
-#docker run -it --rm postgres psql -h $PGHOST -U postgres -c 'DROP SCHEMA IF EXISTS transfer CASCADE; CREATE SCHEMA transfer;' rems;
-
 # Load data from MariaDB into Postgres
 docker run --env-file=db.env -it --rm --link rems_mysql:mysql dimitri/pgloader pgloader --set "search_path='transfer'" --verbose mysql://root@rems_mysql/rems_mysql postgresql://$PGUSER@$PGHOST/$PGDATABASE
 
-docker run --env-file=db.env -i --rm postgres psql -h $PGHOST -U $PGUSER $PGDATABASE < resources/sql/transfer.sql
+docker run --env-file=db.env -i --rm postgres psql -h $PGHOST -U $PGUSER $PGDATABASE < resources/sql/transfer-data.sql
 
 # Stop (and remove) MariaDB
 docker stop rems_mysql
