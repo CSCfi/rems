f2402e36b11e72e36eb457e445bbd218db32aca6
Markku Rontu <markku.rontu@iki.fi>
2017-12-07 19:44:32 +0200

    feat: migrating users

--- dev_transfer.sh
+++ dev_transfer.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xeu
-# Usage: dev_transfer.sh rems_dump.sql
+# Usage: dev_transfer.sh rems_users_dump.sql rems_data_dump.sql
 rems_test_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' rems_test)
-PGHOST=$rems_test_ip PGUSER=rems PGDATABASE=rems ./transfer-db.sh $1
+PGHOST=$rems_test_ip PGUSER=rems PGDATABASE=rems ./transfer-users.sh $1
+PGHOST=$rems_test_ip PGUSER=rems PGDATABASE=rems ./transfer-data.sh $2
