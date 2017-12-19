cffd8d4db2cb60a2227bdbeabfaf7b0fe386198a
Markku Rontu <markku.rontu@iki.fi>
2017-12-19 17:58:21 +0200

    feat: markdown page support

--- src/clj/rems/home.clj
+++ src/clj/rems/home.clj
@@ -1,11 +1,17 @@
 (ns rems.home
-  (:require [compojure.core :refer [GET defroutes]]
+  (:require [clojure.java.io :as io]
+            [clojure.string :as s]
+            [compojure.core :refer [GET defroutes]]
+            [markdown.core :as md]
             [rems.auth.auth :as auth]
+            [rems.auth.util :as auth-util]
+            [rems.config :refer [env]]
             [rems.context :as context]
             [rems.css.styles :as styles]
             [rems.guide :refer :all]
             [rems.layout :as layout]
             [rems.text :refer [text]]
+            [rems.util :refer [index-by]]
             [ring.util.response :refer [content-type
                                         redirect
                                         response]]))
@@ -23,9 +29,20 @@
     (redirect "/landing_page")
     (layout/render "home" (auth/login-component))))
 
+(defn- find-allowed-markdown-file [filename]
+  (let [allowed-files (index-by [:file] (filter :file (:extra-pages env)))]
+    (when (contains? allowed-files filename)
+      (allowed-files filename))))
+
+(defn- markdown-page [filename]
+  (if-let [allowed-file (find-allowed-markdown-file filename)]
+    (layout/render filename (md/md-to-html-string (slurp (:file allowed-file))))
+    (auth-util/throw-unauthorized)))
+
 (defroutes home-routes
   (GET "/" [] (home-page))
   (GET "/about" [] (about-page))
+  (GET "/markdown/:filename" [filename] (markdown-page filename))
   (GET "/css/screen.css" [] (-> (styles/generate-css)
                                 (response)
                                 (content-type "text/css"))))
