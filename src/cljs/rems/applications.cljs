0143e93330c61488ef9b3f3e8384af73148db45c
Joel Kaasinen <joel.kaasinen@nitor.fi>
2018-04-10 13:46:54 +0300

    use rems.application-list in rems.applications

--- src/cljs/rems/applications.cljs
+++ src/cljs/rems/applications.cljs
@@ -2,6 +2,7 @@
   (:require [ajax.core :refer [GET]]
             [clojure.string :as string]
             [re-frame.core :as rf]
+            [rems.application-list :as application-list]
             [rems.text :refer [localize-state localize-time text]])
   (:require-macros [rems.guide-macros :refer [component-info example]]))
 
@@ -20,45 +21,26 @@
  (fn [db _]
    (::my-applications db)))
 
-(defn- applications-item [app]
-  (into [:tbody
-         [:tr.application
-          [:td {:data-th (text :t.applications/application)} (:id app)]
-          [:td {:data-th (text :t.applications/resource)} (string/join ", " (map :title (:catalogue-items app)))]
-          [:td {:data-th (text :t.applications/state)} (localize-state (:state app))]
-          [:td {:data-th (text :t.applications/created)} (localize-time (:start app))]
-          [:td [:a.btn.btn-primary
-                {:href (str "#/application/" (:id app))}
-                (text :t/applications.view)]]]]))
-
 (defn- applications
   [apps]
   (if (empty? apps)
     [:div.applications.alert.alert-success (text :t/applications.empty)]
-    [:table.rems-table.applications
-     (into [:tbody
-            [:tr
-             [:th (text :t.applications/application)]
-             [:th (text :t.applications/resource)]
-             [:th (text :t.applications/state)]
-             [:th (text :t.applications/created)]]])
-     (for [app apps]
-       (applications-item app))]))
+    [application-list/component :id apps]))
 
 (defn applications-page []
   (let [user @(rf/subscribe [:user])]
     (fetch-my-applications user))
   (let [apps @(rf/subscribe [::my-applications])]
-    (applications apps)))
+    [applications apps]))
 
 (defn guide []
   [:div
    (example "applications empty"
-            (applications []))
+            [applications []])
    (example "applications"
-            (applications
+            [applications
              [{:id 1 :catalogue-items [{:title "Draft application"}] :state "draft" :applicantuserid "alice"}
               {:id 2 :catalogue-items [{:title "Applied application"}] :state "applied" :applicantuserid "bob"}
               {:id 3 :catalogue-items [{:title "Approved application"}] :state "approved" :applicantuserid "charlie"}
               {:id 4 :catalogue-items [{:title "Rejected application"}] :state "rejected" :applicantuserid "david"}
-              {:id 5 :catalogue-items [{:title "Closed application"}] :state "closed" :applicantuserid "ernie"}]))])
+              {:id 5 :catalogue-items [{:title "Closed application"}] :state "closed" :applicantuserid "ernie"}]])])
