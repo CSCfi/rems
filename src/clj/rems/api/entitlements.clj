ca1897439376826cd1971849daf080235dd35948
Markku Rontu <markku.rontu@iki.fi>
2018-04-17 10:05:54 +0300

    feat: check-user in entitlement-api
    This is also checked inside the business method, but this is the
    standard API level check.

--- src/clj/rems/api/entitlements.clj
+++ src/clj/rems/api/entitlements.clj
@@ -1,10 +1,8 @@
 (ns rems.api.entitlements
   (:require [compojure.api.sweet :refer :all]
             [rems.api.schema :refer :all]
-            [rems.auth.util :refer [throw-unauthorized]]
-            [rems.context :as context]
+            [rems.api.util :refer [check-user]]
             [rems.db.entitlements :as entitlements]
-            [rems.util :refer [get-user-id]]
             [ring.util.http-response :refer :all]
             [schema.core :as s]))
 
@@ -20,4 +18,5 @@
       :query-params [{user :- (describe s/Str "return entitlements for this user (optional)") nil}
                      {resource :- (describe s/Str "return entitlements for this resource (optional)") nil}]
       :return GetEntitlementsResponse
+      (check-user)
       (ok (entitlements/get-entitlements-for-api user resource)))))
