5ce6eaaa4255a31736708c3e265eb1c21f7c2552
tjalkane <tjalkane@users.noreply.github.com>
2018-04-24 08:37:41 +0300

    add more test cases for actions api

--- test/clj/rems/test/api/actions.clj
+++ test/clj/rems/test/api/actions.clj
@@ -10,20 +10,37 @@
   fake-tempura-fixture
   api-fixture)
 
-(deftest actions-api-test
+(defn get-response-body [user-id]
   (let [api-key "42"
-        user-id "developer"]
-    (let [response (-> (request :get (str "/api/actions"))
-                       (authenticate api-key user-id)
-                       app)
-          body (read-body response)]
-      (testing "listing"
-        (is (:approver? body))
-        (is (= [2 8] (map :id (:approvals body))))
-        (is (= [2 2 3] (sort (map :id (mapcat :catalogue-items (:approvals body))))))
-        (is (= [3 4 5 7 9] (map :id (:handled-approvals body))))
-        (is (empty? (:reviews body)))
-        (is (empty? (:handled-reviews body)))))))
+        response (-> (request :get (str "/api/actions"))
+                     (authenticate api-key user-id)
+                     app)]
+    (read-body response)))
+
+(deftest actions-api-test
+  (testing "listing"
+    (let [body (get-response-body "developer")]
+      (is (:approver? body))
+      (is (= [2 8] (map :id (:approvals body))))
+      (is (= [2 2 3] (sort (map :id (mapcat :catalogue-items (:approvals body))))))
+      (is (= [3 4 5 7 9] (map :id (:handled-approvals body))))
+      (is (empty? (:reviews body)))
+      (is (empty? (:handled-reviews body)))))
+  (testing "listing as another approver"
+    (let [body (get-response-body "bob")]
+      (is (:approver? body))
+      (is (= [2 8] (map :id (:approvals body))))
+      (is (= [2 2 3] (sort (map :id (mapcat :catalogue-items (:approvals body))))))
+      (is (= [3 4 5 7] (map :id (:handled-approvals body))))
+      (is (empty? (:reviews body)))
+      (is (empty? (:handled-reviews body)))))
+  (testing "listing as a reviewer"
+    (let [body (get-response-body "carl")]
+      (is (:reviewer? body))
+      (is (empty? (:approvals body)))
+      (is (empty? (:handled-approvals body)))
+      (is (empty? (:reviews body)))
+      (is (= [9] (map :id (:handled-reviews body)))))))
 
 (deftest actions-api-security-test
   (testing "listing without authentication"
