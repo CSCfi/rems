23fde6ea5405c00aeb0482b7526f708d0133f288
Joel Kaasinen <joel.kaasinen@nitor.fi>
2018-04-25 09:26:35 +0300

    actions page: show last modified column for handled table

--- src/cljs/rems/application_list.cljs
+++ src/cljs/rems/application_list.cljs
@@ -12,27 +12,37 @@
 (defn- get-catalogue-items [app]
   (str/join ", " (map :title (:catalogue-items app))))
 
-(def ^:private +columns+
-  [{:name :id
-    :getter :id
-    :header #(text :t.actions/application)}
-   {:name :resource
-    :getter get-catalogue-items
-    :header #(text :t.actions/resource)}
-   {:name :applicant
-    :getter :applicantuserid
-    :header #(text :t.actions/applicant)}
-   {:name :state
-    :getter #(localize-state (:state %))
-    :header #(text :t.actions/state)}
-   {:name :created
-    :getter #(localize-time (:start %))
-    :header #(text :t.actions/created)}])
+(def +all-columns+
+  [:id :resource :applicant :state :created :handled])
 
-(defn- row [app]
+(def +default-columns+
+  [:id :resource :applicant :state :created])
+
+(def ^:private +columns+
+  {:id {:getter :id
+        :header #(text :t.actions/application)}
+   :resource {:getter get-catalogue-items
+              :header #(text :t.actions/resource)}
+   :applicant {:getter :applicantuserid
+               :header #(text :t.actions/applicant)}
+   :state {:getter #(localize-state (:state %))
+           :header #(text :t.actions/state)}
+   :created {:getter #(localize-time (:start %))
+             :header #(text :t.actions/created)}
+   :handled {:getter #(localize-time (:handled %))
+             ;; NB!:
+             :header #(text :t.actions/last-modified)}})
+
+(defn column-header [col]
+  ((get-in +columns+ [col :header])))
+
+(defn column-getter [col app]
+  ((get-in +columns+ [col :getter]) app))
+
+(defn- row [columns app]
   (into [:tr.action]
-        (concat (for [{:keys [header getter]} +columns+]
-                  [:td {:data-th (header)} (getter app)])
+        (concat (for [c columns]
+                  [:td {:data-th (column-header c)} (column-getter c app)])
                 ;; buttons to show could be parameterized
                 [[:td.commands (view-button app)]])))
 
@@ -46,23 +56,20 @@
     [old-column (flip old-order)]
     [new-column :asc]))
 
-(defn- table [[sort-column sort-order] set-sorting apps]
+(defn- table [columns [sort-column sort-order] set-sorting apps]
   [:table.rems-table.actions
    (into [:tbody
           (into [:tr]
-                (for [{:keys [name header]} +columns+]
+                (for [c columns]
                   [:th
-                   {:on-click #(set-sorting (change-sort sort-column sort-order name))}
-                   (header)
+                   {:on-click #(set-sorting (change-sort sort-column sort-order c))}
+                   (column-header c)
                    " "
-                   (when (= name sort-column) (sort-symbol sort-order))]))]
-         (map row apps))])
+                   (when (= c sort-column) (sort-symbol sort-order))]))]
+         (map (partial row columns) apps))])
 
 (defn- apply-sorting [[col order] apps]
-  (let [fun (get
-             (zipmap (map :name +columns+) (map :getter +columns+))
-             col)
-        sorted (sort-by fun apps)]
+  (let [sorted (sort-by #(column-getter col %) apps)]
     (case order
       :asc sorted
       :desc (reverse sorted))))
@@ -70,33 +77,35 @@
 (defn component
   "A table of applications.
 
+   columns should be a subsequence of +all-columns+, for instance +default-columns+.
+
    sorting should be a pair [column order] where
      - order is :asc or :desc
      - column is one of :id :applicant :resource :created :state
 
    set-sorting is a callback that is called with a new sorting when it changes"
-  [sorting set-sorting apps]
-  (table sorting set-sorting (apply-sorting sorting apps)))
+  [columns sorting set-sorting apps]
+  (table columns sorting set-sorting (apply-sorting sorting apps)))
 
 (def ^:private +example-applications+
   [{:id 1 :catalogue-items [{:title "Item 5"}] :state "draft" :applicantuserid "alice"
-    :start "1980-01-02T13:45:00.000Z"}
+    :start "1980-01-02T13:45:00.000Z" :handled "2017-01-01T01:01:01:001Z"}
    {:id 2 :catalogue-items [{:title "Item 3"}] :state "applied" :applicantuserid "bob"
-    :start "1971-02-03T23:59:00.000Z"}
+    :start "1971-02-03T23:59:00.000Z" :handled "2017-01-01T01:01:01:001Z"}
    {:id 3 :catalogue-items [{:title "Item 2"} {:title "Item 5"}] :state "approved" :applicantuserid "charlie"
-    :start "1980-01-01T01:01:00.000Z"}
+    :start "1980-01-01T01:01:00.000Z" :handled "2017-01-01T01:01:01:001Z"}
    {:id 4 :catalogue-items [{:title "Item 2"}] :state "rejected" :applicantuserid "david"
-    :start "1972-12-12T12:12:00.000Z"}
+    :start "1972-12-12T12:12:00.000Z" :handled "2017-01-01T01:01:01:001Z"}
    {:id 5 :catalogue-items [{:title "Item 2"}] :state "closed" :applicantuserid "ernie"
-    :start "1972-12-12T12:12:00.000Z"}])
+    :start "1972-12-12T12:12:00.000Z" :handled "2017-01-01T01:01:01:001Z"}])
 
 (defn guide
   []
   [:div
    (component-info component)
    (example "empty list"
-            [component [:id :asc] prn []])
+            [component +default-columns+ [:id :asc] prn []])
    (example "applications, default order"
-            [component [:id :asc] prn +example-applications+])
-   (example "applications, descending date"
-            [component [:created :desc] prn +example-applications+])])
+            [component +default-columns+ [:id :asc] prn +example-applications+])
+   (example "applications, descending date, all columns"
+            [component +all-columns+ [:created :desc] prn +example-applications+])])
