fdf3eadaacf6d8cd2d4d4fbaaccdb6ab39d886ba
Joel Kaasinen <joel.kaasinen@nitor.fi>
2018-04-25 14:15:07 +0300

    disable travis deploy

--- .travis-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xeu
-
-# Run as the after_success step of a travis build.
-
-if [[ ( "$TRAVIS_BRANCH" == "master" || "$TRAVIS_BRANCH" == "spa" ) && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-    cd ansible/
-    ansible-playbook -vv rems.yml
-fi
