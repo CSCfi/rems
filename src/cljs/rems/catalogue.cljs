e04d118f77ea71fe00f315c1c84751a2b32cbc21
Joel Kaasinen <joel.kaasinen@nitor.fi>
2018-05-17 15:57:04 +0300

    rems.catalogue: use generic table component

--- src/cljs/rems/catalogue.cljs
+++ src/cljs/rems/catalogue.cljs
@@ -6,22 +6,20 @@
             [rems.cart :as cart]
             [rems.db.catalogue :refer [urn-catalogue-item? get-catalogue-item-title disabled-catalogue-item?]]
             [rems.guide-functions]
+            [rems.table :as table]
             [rems.text :refer [text]]
             [rems.atoms :refer [external-link]])
   (:require-macros [rems.guide-macros :refer [component-info example]]))
 
 (rf/reg-event-db
- ::sort-by
- (fn [db [_ _]]
-   (let [sort-order (if (= (::sort-order db) :asc)
-                      :desc
-                      :asc)]
-     (assoc db ::sort-order sort-order))))
+ ::set-sort-order
+ (fn [db [_ order]]
+   (assoc db ::sort-order order)))
 
 (rf/reg-sub
  ::sort-order
  (fn [db _]
-   (get db ::sort-order :asc)))
+   (get db ::sort-order [:name :asc])))
 
 (rf/reg-event-db
  ::catalogue
@@ -33,35 +31,25 @@
  (fn [db _]
    (::catalogue db)))
 
-(defn catalogue-item
-  "Single catalogue item"
-  [item language]
-  (let [resid (:resid item)
-        title (get-catalogue-item-title item language)
-        component (if (urn-catalogue-item? item)
-                    [:a.catalogue-item-link {:href resid :target :_blank} title " " [external-link]]
-                    title)]
-    [:tr
-     [:td {:data-th (text :t.catalogue/header)} component]
-     [:td.commands {:data-th ""} [cart/add-to-cart-button item]]]))
+(defn- catalogue-item-title [item language]
+  (let [title (get-catalogue-item-title item language)]
+    (if (urn-catalogue-item? item)
+      [:a.catalogue-item-link {:href (:resid item) :target :_blank} title " " [external-link]]
+      [:span title])))
+
+(defn- catalogue-columns [lang]
+  {:name {:header #(text :t.catalogue/header)
+          :value #(catalogue-item-title % lang)
+          :sort-value #(get-catalogue-item-title % lang)}
+   :cart {:value (fn [i] [cart/add-to-cart-button i])
+          :sortable? false}})
 
 (defn- catalogue-list
-  "List of catalogue items"
   [items language sort-order]
-  (let [sorted (sort-by #(get-catalogue-item-title % language)
-                        (remove disabled-catalogue-item? items))
-        sorted-items (if (= sort-order :desc)
-                       (reverse sorted)
-                       sorted)]
-    [:table.rems-table.catalogue
-     (into [:tbody
-            [:tr
-             [:th {:on-click #(rf/dispatch [::sort-by])}
-              (str (text :t.catalogue/header) " ")
-              (atoms/sort-symbol sort-order)]
-             [:th ""]]]
-           (for [item sorted-items]
-                    [catalogue-item item language]))]))
+  (table/component (catalogue-columns language) [:name :cart]
+                   sort-order #(rf/dispatch [::set-sort-order %])
+                   :id
+                   items))
 
 (defn- fetch-catalogue []
   (GET "/api/catalogue/" {:handler #(rf/dispatch [::catalogue %])
@@ -79,30 +67,38 @@
 
 (defn guide []
   [:div
-   (component-info catalogue-item)
-   (example "catalogue-item"
+   (component-info catalogue-item-title)
+   (example "catalogue-item-title"
             [:table.rems-table
              [:tbody
-              [catalogue-item {:title "Item title"} nil]]])
-   (example "catalogue-item linked to urn.fi"
+              [:tr
+               [:td
+                [catalogue-item-title {:title "Item title"} nil]]]]])
+   (example "catalogue-item-title linked to urn.fi"
             [:table.rems-table
              [:tbody
-              [catalogue-item {:title "Item title" :resid "http://urn.fi/urn:nbn:fi:lb-201403262"} nil]]])
-   (example "catalogue-item in Finnish with localizations"
+              [:tr
+               [:td
+                [catalogue-item-title {:title "Item title" :resid "http://urn.fi/urn:nbn:fi:lb-201403262"} nil]]]]])
+   (example "catalogue-item-title in Finnish with localizations"
             [:table.rems-table
              [:tbody
-              [catalogue-item {:title "Not used when there are localizations" :localizations {:fi {:title "Suomenkielinen title"} :en {:title "English title"}}} :en]]])
-   (example "catalogue-item in English with localizations"
+              [:tr
+               [:td
+                [catalogue-item-title {:title "Not used when there are localizations" :localizations {:fi {:title "Suomenkielinen title"} :en {:title "English title"}}} :en]]]]])
+   (example "catalogue-item-title in English with localizations"
             [:table.rems-table
              [:tbody
-              [catalogue-item {:title "Not used when there are localizations" :localizations {:fi {:title "Suomenkielinen title"} :en {:title "English title"}}} :fi]]])
+              [:tr
+               [:td
+                [catalogue-item-title {:title "Not used when there are localizations" :localizations {:fi {:title "Suomenkielinen title"} :en {:title "English title"}}} :fi]]]]])
 
    (component-info catalogue-list)
    (example "catalogue-list empty"
-            [catalogue-list [] nil :asc])
+            [catalogue-list [] nil [:name :asc]])
    (example "catalogue-list with two items"
-            [catalogue-list [{:title "Item title"} {:title "Another title"}] nil :asc])
+            [catalogue-list [{:title "Item title"} {:title "Another title"}] nil [:name :asc]])
    (example "catalogue-list with two items in reverse order"
-            [catalogue-list [{:title "Item title"} {:title "Another title"}] nil :desc])
+            [catalogue-list [{:title "Item title"} {:title "Another title"}] nil [:name :desc]])
    (example "catalogue-list with three items, of which second is disabled"
-            [catalogue-list [{:title "Item 1"} {:title "Item 2 is disabled and should not be shown" :state "disabled"} {:title "Item 3"}] nil :asc])])
+            [catalogue-list [{:title "Item 1"} {:title "Item 2 is disabled and should not be shown" :state "disabled"} {:title "Item 3"}] nil [:name :asc]])])
