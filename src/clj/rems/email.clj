5909d822bbb04ca9007fcc21cb136be01c1215e5
tjalkane <tjalkane@users.noreply.github.com>
2018-05-29 16:54:57 +0300

    fix: show localization value instead of key in status change emails

--- src/clj/rems/email.clj
+++ src/clj/rems/email.clj
@@ -38,7 +38,7 @@
                           (get-username recipient-attrs)
                           app-id
                           (format-items items)
-                          (string/lower-case (localize-state state))
+                          (string/lower-case (text (localize-state state)))
                           (form-link app-id))))
 
 ;; TODO: send message localized according to recipient's preferences, when those are stored
