8816d62aa5d3d318c2cb264487dc458b6fa586e9
Joel Kaasinen <joel.kaasinen@nitor.fi>
2018-08-01 15:54:27 +0300

    rems.pdf: resources, licenses

--- src/clj/rems/pdf.clj
+++ src/clj/rems/pdf.clj
@@ -1,37 +1,62 @@
 (ns rems.pdf
   "Rendering applications as pdf"
   (:require [clj-pdf.core :refer :all]
+            [rems.context :as context]
             [rems.db.applications :as applications]
             [rems.text :refer [text localize-state with-language]]))
 
 (defn- render-header [form]
-  (let [state (get-in form [:application :state])]
+  (let [state (get-in form [:application :state])
+        user (:applicant-attributes form)
+        catalogue-items (:catalogue-items form)]
     (list
-     ;; TODO applied resources
-     ;; TODO applicant info
      ;; TODO events
      [:paragraph
       (text :t.applications/state)
-      (when state [:phrase ": " (text (localize-state state))])])))
+      (when state [:phrase ": " (text (localize-state state))])]
+     [:heading (text :t.applicant-info/applicant)]
+     [:paragraph (get user "eppn")]
+     [:paragraph (get user "mail")]
+     ;; TODO more fields?
+     [:heading (text :t.form/resources)]
+     (into
+      [:list]
+      (for [ci catalogue-items]
+        [:phrase
+         (get-in ci [:localizations context/*lang* :title])
+         " (" (:resid ci) ")"])))))
 
 (defn- render-field [field]
   (list
    [:heading (:title field)]
    [:paragraph (:value field)]))
 
-(defn- apply-localization [item language]
-  (merge item (get-in item [:localizations language])))
+(defn- apply-localization [item]
+  (merge item (get-in item [:localizations context/*lang*])))
 
 (defn- render-fields [form]
-  ;; TODO licenses
   (apply concat
          (for [field (:items form)]
-           (render-field (apply-localization field :en)))))
+           (render-field (apply-localization field)))))
+
+(defn- render-license [license]
+  ;; TODO nicer checkbox rendering
+  ;; TODO license text?
+  [:paragraph
+   (if (:approved license) "[x]" "[ ]")
+   (:title license)])
+
+(defn- render-licenses [form]
+  (into
+   [[:heading (text :t.form/licenses)]]
+   (for [license (:licenses form)]
+     (render-license (apply-localization license)))))
 
 (defn- render-form [form]
   [{}
    (render-header form)
-   (render-fields form)])
+   (render-fields form)
+   (render-licenses form)])
 
 (defn application-to-pdf [form out]
   (pdf (render-form form) out))
