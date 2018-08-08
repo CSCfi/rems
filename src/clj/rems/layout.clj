511c077833bdd35a07bbe02ca8bca3cf193ac15a
Esko Luontola <esko@luontola.fi>
2018-08-08 15:32:24 +0300

    feat: update the document language when language is changed

--- src/clj/rems/layout.clj
+++ src/clj/rems/layout.clj
@@ -23,7 +23,7 @@ window.rems = {
 
 (defn- page-template
   [content]
-  (html5 [:html {:lang "en"}                                ; TODO: parameterize
+  (html5 [:html {:lang "en"}
           [:head
            [:meta {:http-equiv "Content-Type" :content "text/html; charset=UTF-8"}]
            [:meta {:name "viewport" :content "width=device-width, initial-scale=1"}]
