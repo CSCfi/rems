921fd82c1c412a42548ebb8a1a33dacbfd53c802
Esko Luontola <esko@luontola.fi>
2018-08-22 13:18:57 +0300

    rename namespace for uniformity

--- test/cljs/rems/cljs_tests.cljs
+++ test/cljs/rems/cljs_tests.cljs
@@ -1,7 +1,7 @@
 (ns rems.cljs-tests
   (:require [doo.runner :refer-macros [doo-tests]]
-            [rems.test.administration.licenses]
+            [rems.test.administration.license]
             [rems.test.table]))
 
-(doo-tests 'rems.test.administration.licenses
+(doo-tests 'rems.test.administration.license
            'rems.test.table)
