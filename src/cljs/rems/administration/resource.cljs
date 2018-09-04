e69e5c4b5d365051ea7a43c0836aeee00aff04bb
Esko Luontola <esko@luontola.fi>
2018-09-04 17:59:09 +0300

    refactor to use shared form components and unify with other admin pages

--- src/cljs/rems/administration/resource.cljs
+++ src/cljs/rems/administration/resource.cljs
@@ -1,97 +1,120 @@
 (ns rems.administration.resource
   (:require [clojure.string :as str]
             [re-frame.core :as rf]
+            [rems.administration.components :refer [text-field]]
             [rems.autocomplete :as autocomplete]
             [rems.collapsible :as collapsible]
             [rems.text :refer [text localize-item]]
             [rems.util :refer [dispatch! fetch post!]]))
 
-(defn- fetch-licenses []
-  (fetch "/api/licenses?active=true"
-         {:handler #(do (rf/dispatch [::set-licenses %])
-                        (rf/dispatch [::set-selected-licenses #{}]))}))
+(defn- valid-request? [request]
+  (and (not (str/blank? (:prefix request)))
+       (not (str/blank? (:resid request)))))
 
-(defn- create-resource [prefix resid licenses]
-  (post! "/api/resources/create" {:params {:prefix prefix
-                                           :resid resid
-                                           :licenses (if licenses
-                                                       (map :id licenses)
-                                                       [])}
+(defn- build-request [form]
+  (let [request {:prefix (:prefix form)
+                 :resid (:resid form)
+                 :licenses (map :id (:licenses form))}]
+    (when (valid-request? request)
+      request)))
+
+(defn- create-resource [form]
+  (post! "/api/resources/create" {:params (build-request form)
                                   :handler (fn [resp]
                                              (dispatch! "#/administration"))}))
 
-(rf/reg-sub
- ::prefix
- (fn [db _]
-   (::prefix db)))
-
-(rf/reg-event-db
- ::set-prefix
- (fn [db [_ prefix]]
-   (assoc db ::prefix prefix)))
-
-(rf/reg-sub
- ::resid
- (fn [db _]
-   (::resid db)))
-
-(rf/reg-event-db
- ::set-resid
- (fn [db [_ resid]]
-   (assoc db ::resid resid)))
-
-(rf/reg-sub
- ::licenses
- (fn [db _]
-   (::licenses db)))
-
-(rf/reg-sub
- ::selected-licenses
- (fn [db _]
-  (::selected-licenses db)))
-
-(rf/reg-event-db
- ::set-licenses
- (fn [db [_ licenses]]
-   (assoc db ::licenses licenses)))
-
-(rf/reg-event-db
- ::set-selected-licenses
- (fn [db [_ licenses]]
-   (assoc db ::selected-licenses licenses)))
-
-(rf/reg-event-db
- ::add-selected-licenses
- (fn [db [_ license]]
-   (if (contains? (::selected-licenses db) license)
-     db
-     (update db ::selected-licenses conj license))))
-
-(rf/reg-event-db
- ::remove-selected-licenses
- (fn [db [_ license]]
-   (update db ::selected-licenses disj license)))
-
 (rf/reg-event-fx
- ::create-resource
- (fn [db [_ prefix resid licenses]]
-   (create-resource prefix resid licenses)
-   {}))
+  ::create-resource
+  (fn [db [_ request]]
+    (create-resource request)
+    {}))
 
 (rf/reg-event-db
- ::reset-create-resource
- (fn [db _]
-   (dissoc db ::prefix ::resid ::selected-licenses)))
+  ::reset-create-resource
+  (fn [db _]
+    (assoc db ::form {:licenses #{}})))
+
+(rf/reg-sub
+  ::form
+  (fn [db _]
+    (::form db)))
+
+(rf/reg-event-db
+  ::set-form-field
+  (fn [db [_ keys value]]
+    (assoc-in db (concat [::form] keys) value)))
+
+(rf/reg-sub
+  ::selected-licenses
+  (fn [db _]
+    (get-in db [::form :licenses])))
+
+(rf/reg-event-db
+  ::select-license
+  (fn [db [_ license]]
+    (update-in db [::form :licenses] conj license)))
+
+(rf/reg-event-db
+  ::deselect-license
+  (fn [db [_ license]]
+    (update-in db [::form :licenses] disj license)))
+
+
+; available licenses
+
+(rf/reg-sub
+  ::available-licenses
+  (fn [db _]
+    (::available-licenses db)))
+
+(rf/reg-event-db
+  ::set-available-licenses
+  (fn [db [_ licenses]]
+    (assoc db ::available-licenses licenses)))
+
+(defn- fetch-licenses []
+  (fetch "/api/licenses?active=true"
+         {:handler #(rf/dispatch [::set-available-licenses %])}))
+
 
 ;;;; UI ;;;;
 
+(def ^:private context {:get-form ::form
+                        :update-form ::set-form-field})
+
+(defn- resource-prefix-field []
+  [text-field context {:keys [:prefix]
+                       :label (text :t.create-resource/prefix)
+                       :placeholder (text :t.create-resource/prefix-placeholder)}])
+
+(defn- resource-id-field []
+  [text-field context {:keys [:resid]
+                       :label (text :t.create-resource/resid)
+                       :placeholder (text :t.create-resource/resid-placeholder)}])
+
+(defn- resource-licenses-field []
+  (let [available-licenses @(rf/subscribe [::available-licenses])
+        selected-licenses @(rf/subscribe [::selected-licenses])]
+    [:div.form-group
+     [:label (text :t.create-resource/licenses-selection)]
+     [autocomplete/component
+      {:value (->> selected-licenses
+                   (map localize-item)
+                   (sort-by :id))
+       :items (map localize-item available-licenses)
+       :value->text #(:title %2)
+       :item->key :id
+       :item->text :title
+       :item->value identity
+       :search-fields [:title]
+       :add-fn #(rf/dispatch [::select-license %])
+       :remove-fn #(rf/dispatch [::deselect-license %])}]]))
+
 (defn- save-resource-button []
-  (let [prefix @(rf/subscribe [::prefix])
-        resid @(rf/subscribe [::resid])
-        licenses @(rf/subscribe [::selected-licenses])]
+  (let [form @(rf/subscribe [::form])]
     [:button.btn.btn-primary
-     {:on-click #(rf/dispatch [::create-resource prefix resid licenses])
-      :disabled (not (and (not (str/blank? prefix)) (not (str/blank? resid))))}
+     {:on-click #(rf/dispatch [::create-resource form])
+      :disabled (not (build-request form))}
      (text :t.administration/save)]))
 
 (defn- cancel-button []
@@ -100,46 +123,17 @@
    (text :t.administration/cancel)])
 
 (defn create-resource-page []
+  ; FIXME: fetch available licenses the same way as fetch-form-items to fix https://github.com/CSCfi/rems/issues/646
   (fetch-licenses)
-  (let [prefix (rf/subscribe [::prefix])
-        resid (rf/subscribe [::resid])
-        licenses (rf/subscribe [::licenses])
-        selected-licenses (rf/subscribe [::selected-licenses])]
-    (fn []
-      [collapsible/component
-       {:id "create-resource"
-        :title (text :t.navigation/create-resource)
-        :always [:div
-                 [:div.form-group.field
-                  [:label {:for "prefix"} (text :t.create-resource/prefix)]
-                  [:input.form-control {:id "prefix"
-                                        :name "prefix"
-                                        :type :text
-                                        :placeholder (text :t.create-resource/prefix-placeholder)
-                                        :value @prefix
-                                        :on-change #(rf/dispatch [::set-prefix (.. % -target -value)])}]]
-                 [:div.form-group.field
-                  [:label {:for "resid"} (text :t.create-resource/resid)]
-                  [:input.form-control {:id "resid"
-                                        :name "resid"
-                                        :type :text
-                                        :placeholder (text :t.create-resource/resid-placeholder)
-                                        :value @resid
-                                        :on-change #(rf/dispatch [::set-resid (.. % -target -value)])}]]
-                 [:div.form-group
-                  [:label (text :t.create-resource/licenses-selection)]
-                  [autocomplete/component
-                   {:value (->> @selected-licenses
-                                (map localize-item)
-                                (sort-by :id))
-                    :items (map localize-item @licenses)
-                    :value->text #(:title %2)
-                    :item->key :id
-                    :item->text :title
-                    :item->value identity
-                    :search-fields [:title]
-                    :add-fn #(rf/dispatch [::add-selected-licenses %])
-                    :remove-fn #(rf/dispatch [::remove-selected-licenses %])}]]
-                 [:div.col.commands
-                  [cancel-button]
-                  [save-resource-button]]]}])))
+  (fn []
+    [collapsible/component
+     {:id "create-resource"
+      :title (text :t.navigation/create-resource)
+      :always [:div
+               [resource-prefix-field]
+               [resource-id-field]
+               [resource-licenses-field]
+
+               [:div.col.commands
+                [cancel-button]
+                [save-resource-button]]]}]))
--- test/cljs/rems/cljs_tests.cljs
+++ test/cljs/rems/cljs_tests.cljs
@@ -2,12 +2,14 @@
   (:require [doo.runner :refer-macros [doo-tests]]
             rems.test.administration.form
             rems.test.administration.license
+            rems.test.administration.resource
             rems.test.administration.workflow
             rems.test.table
             rems.test.util))
 
 (doo-tests 'rems.test.administration.form
            'rems.test.administration.license
+           'rems.test.administration.resource
            'rems.test.administration.workflow
            'rems.test.table
            'rems.test.util)
