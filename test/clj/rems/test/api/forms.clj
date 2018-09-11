2fdf47fe29d061efed7c41106ba73f72a90dced5
Esko Luontola <esko@luontola.fi>
2018-09-11 17:35:14 +0300

    API for creating forms

--- test/clj/rems/test/api/forms.clj
+++ test/clj/rems/test/api/forms.clj
@@ -4,12 +4,83 @@
             [rems.test.api :refer :all]
             [rems.test.tempura :refer [fake-tempura-fixture]]
             [rems.util :refer [index-by]]
-            [ring.mock.request :refer :all]))
+            [ring.mock.request :refer :all])
+  (:import (java.util UUID)))
 
 (use-fixtures
-  :once
-  fake-tempura-fixture
-  api-fixture)
+ :once
+ fake-tempura-fixture
+ api-fixture)
+
+(defn- get-draft-form [form-id]
+  ;; XXX: there is no simple API for reading the form items
+  (let [api-key "42"
+        user-id "owner"
+        catalogue-item-response (-> (request :post "/api/catalogue-items/create")
+                                    (authenticate api-key user-id)
+                                    (json-body {:title "tmp"
+                                                :form form-id
+                                                :resid 1
+                                                :wfid 1})
+                                    app)
+        catalogue-item (read-body catalogue-item-response)
+        draft-response (-> (request :get "/api/applications/draft" {:catalogue-items (:id catalogue-item)})
+                           (authenticate api-key user-id)
+                           app)
+        draft (read-body draft-response)]
+    (assert (response-is-ok? catalogue-item-response))
+    (assert (response-is-ok? draft-response))
+    draft))
+
+(deftest forms-api-test
+  (let [api-key "42"
+        user-id "owner"]
+    (testing "get"
+      (let [response (-> (request :get "/api/forms")
+                         (authenticate api-key user-id)
+                         app)
+            data (read-body response)]
+        (is (response-is-ok? response))
+        (is (coll-is-not-empty? data))
+        (is (= #{:id :prefix :title :start :end :active}
+               (set (keys (first data)))))))
+
+    (testing "create"
+      (let [command {:prefix "abc"
+                     :title (str "form title " (UUID/randomUUID))
+                     :items [{:title {:en "en title"
+                                      :fi "fi title"}
+                              :optional true
+                              :type "text"
+                              :input-prompt {:en "en prompt"
+                                             :fi "fi prompt"}}]}
+            response (-> (request :post "/api/forms/create")
+                         (authenticate api-key user-id)
+                         (json-body command)
+                         app)]
+        (is (response-is-ok? response))
+
+        (testing "and fetch"
+          (let [response (-> (request :get "/api/forms")
+                             (authenticate api-key user-id)
+                             app)
+                form (->> response
+                          read-body
+                          (filter #(= (:title %) (:title command)))
+                          first)]
+            (is (response-is-ok? response))
+            ;; TODO: create an API for reading full forms (will be needed latest for editing forms)
+            (is (= (select-keys command [:title :prefix])
+                   (select-keys form [:title :prefix])))
+            (is (= [{:optional true
+                     :type "text"
+                     :localizations {:en {:title "en title"
+                                          :inputprompt "en prompt"}
+                                     :fi {:title "fi title"
+                                          :inputprompt "fi prompt"}}}]
+                   (->> (get-draft-form (:id form))
+                        :items
+                        (map #(select-keys % [:optional :type :localizations])))))))))))
 
 (deftest forms-api-filtering-test
   (let [unfiltered-response (-> (request :get "/api/forms")
@@ -29,16 +100,36 @@
     (is (< (count filtered-data) (count unfiltered-data)))))
 
 (deftest forms-api-security-test
-  (testing "listing without authentication"
-    (let [response (-> (request :get (str "/api/forms"))
-                       app)
-          body (read-body response)]
-      (is (= 401 (:status response)))
-      (is (= "unauthorized" body))))
-  (testing "listing without owner role"
-    (let [response (-> (request :get (str "/api/forms"))
-                       (authenticate "42" "alice")
-                       app)
-          body (read-body response)]
-      (is (= 401 (:status response)))
-      (is (= "unauthorized" body)))))
+  (testing "without authentication"
+    (testing "list"
+      (let [response (-> (request :get (str "/api/forms"))
+                         app)
+            body (read-body response)]
+        (is (= 401 (:status response)))
+        (is (= "unauthorized" body))))
+    (testing "create"
+      (let [response (-> (request :post "/api/forms/create")
+                         (json-body {:prefix "abc"
+                                     :title "the title"
+                                     :items []})
+                         app)]
+        (is (= 403 (:status response)))
+        (is (= "<h1>Invalid anti-forgery token</h1>" (read-body response))))))
+
+  (testing "without owner role"
+    (testing "list"
+      (let [response (-> (request :get (str "/api/forms"))
+                         (authenticate "42" "alice")
+                         app)
+            body (read-body response)]
+        (is (= 401 (:status response)))
+        (is (= "unauthorized" body))))
+    (testing "create"
+      (let [response (-> (request :post "/api/forms/create")
+                         (authenticate "42" "alice")
+                         (json-body {:prefix "abc"
+                                     :title "the title"
+                                     :items []})
+                         app)]
+        (is (= 401 (:status response)))
+        (is (= "unauthorized" (read-body response)))))))
