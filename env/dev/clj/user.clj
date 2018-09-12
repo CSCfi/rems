c25d873a49abea9d416aad3caad06c57b5d6b904
Esko Luontola <esko@luontola.fi>
2018-09-12 15:03:31 +0300

    REPL helper to parse and pprint transit data

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -1,6 +1,11 @@
 (ns user
-  (:require [clojure.tools.namespace.repl :as repl]
-            [eftest.runner :as ef]))
+  (:require [clojure.pprint :refer [pprint]]
+            [clojure.tools.namespace.repl :as repl]
+            [eftest.runner :as ef]
+            [muuntaja.core :as muuntaja]
+            [rems.api :as api])
+  (:import (java.awt Toolkit)
+           (java.awt.datatransfer DataFlavor)))
 
 (defn reload []
   (repl/refresh)
@@ -14,3 +19,17 @@
 (defn run-all-tests []
   (reload)
   (ef/run-tests (ef/find-tests "test/clj") {:multithread? false}))
+
+(defn read-clipboard []
+  (-> (Toolkit/getDefaultToolkit)
+      (.getSystemClipboard)
+      (.getData DataFlavor/stringFlavor)))
+
+(defn decode-transit [data]
+  (muuntaja/decode api/muuntaja "application/transit+json" data))
+
+(defn pptransit
+  ([]
+   (pptransit (read-clipboard)))
+  ([data]
+   (pprint (decode-transit data))))
