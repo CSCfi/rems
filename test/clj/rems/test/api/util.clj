7f7d6bc7708c9b97d9151778595cf42f452c7ff7
Esko Luontola <esko@luontola.fi>
2018-10-25 10:11:12 +0300

    support for declarative roles check in routes

--- test/clj/rems/test/api/util.clj
+++ test/clj/rems/test/api/util.clj
@@ -1,7 +1,55 @@
 (ns rems.test.api.util
-  (:require [clojure.test :refer [deftest is]]
-            [rems.api.util :refer :all]))
+  (:require [clojure.test :refer :all]
+            [compojure.api.sweet :refer :all]
+            [rems.api.util :refer :all]
+            [rems.context :as context]
+            [ring.util.http-response :refer :all])
+  (:import (rems.auth NotAuthorizedException)))
 
 (deftest longify-keys-test
   (is (= {} (longify-keys nil)))
   (is (= {42 42} (longify-keys {:42 42}))))
+
+(deftest route-role-check-test
+  (testing "no roles required"
+    (let [route (GET "/foo" []
+                  :summary "Summary text"
+                  (ok {:success true}))]
+      (is (= {:status 200
+              :headers {}
+              :body {:success true}}
+             (route {:request-method :get
+                     :uri "/foo"})))))
+
+  (testing "role required"
+    (let [route (GET "/foo" []
+                  :summary "Summary text"
+                  :roles #{:approver}
+                  (ok {:success true}))]
+
+      (testing "but user doesn't have it"
+        (binding [context/*roles* #{:applicant}]
+          (is (thrown? NotAuthorizedException
+                       (route {:request-method :get
+                               :uri "/foo"})))))
+
+      (testing "and user has it"
+        (binding [context/*roles* #{:approver}]
+          (is (= {:status 200
+                  :headers {}
+                  :body {:success true}}
+                 (route {:request-method :get
+                         :uri "/foo"})))))))
+
+  (testing "required roles are added to summary documentation"
+    (let [route (GET "/foo" []
+                  :summary "Summary text"
+                  :roles #{:approver :reviewer}
+                  (ok {:success true}))]
+      (is (= "Summary text (roles: approver, reviewer)"
+             (get-in route [:info :public :summary])))))
+
+  (testing "summary documentation is required"
+    ; TODO: is there a way to test exceptions thrown from macros, so that we don't need to expose this private helper function?
+    (is (thrown-with-msg? IllegalArgumentException #"^\QRoute must have a :summary when using :roles\E$"
+                          (add-roles-documentation nil #{:some-role})))))
