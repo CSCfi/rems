9329305f60703b9fda8b220c7ffd09929d665784
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2018-11-23 16:31:45 +0200

    Read current revision from a different file
    This is that we can handle tags that contain -

--- src/cljs/rems/read_gitlog.clj
+++ src/cljs/rems/read_gitlog.clj
@@ -1,17 +1,24 @@
 (ns rems.read-gitlog
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.java.shell :as sh]))
+
+(def ^{:private true} version-description-file "git-describe.txt")
+(def ^{:private true} version-revision-file "git-revision.txt")
+(def ^{:private true} repo-url "https://github.com/CSCfi/rems/commit/")
+
+(defn- read-file [name]
+  (some-> name
+          io/resource
+          slurp
+          str/trim))
 
 (defmacro read-current-version []
   (try
-    (let [full (-> "git-describe.txt"
-                   io/resource
-                   slurp
-                   str/trim)
-          [tag commits-since commit dirty] (str/split full #"-")]
-      {:full full
-       :tag tag
-       :commits-since commits-since
-       :commit commit
-       :dirty dirty})
+    (let [version (read-file version-description-file)
+          revision (read-file version-revision-file)]
+      (when (and version revision)
+        {:version version
+         :revision revision
+         :repo-url repo-url}))
     (catch java.io.IOException _ "")))
