ed720e88daee5b1aed53c0170df49407c1d0f08a
Markku Rontu <markku.rontu@iki.fi>
2018-11-28 10:44:08 +0200

    refactor: extract lorem ipsum to guide utils

--- src/cljs/rems/application.cljs
+++ src/cljs/rems/application.cljs
@@ -5,6 +5,7 @@
             [rems.autocomplete :as autocomplete]
             [rems.collapsible :as collapsible]
             [rems.db.catalogue :refer [get-catalogue-item-title]]
+            [rems.guide-utils :refer [lipsum lipsum-short lipsum-paragraphs]]
             [rems.modal :as modal]
             [rems.phase :refer [phases]]
             [rems.spinner :as spinner]
@@ -928,19 +929,6 @@
 
 
 ;;;; Guide
-
-(def ^:private lipsum
-  (str "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod "
-       "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim "
-       "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex "
-       "ea commodo consequat. Duis aute irure dolor in reprehenderit in "
-       "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur "
-       "sint occaecat cupidatat non proident, sunt in culpa qui officia "
-       "deserunt mollit anim id est laborum."))
-
-(def ^:private lipsum-short "Lorem ipsum dolor sit amet")
-
-(def ^:private lipsum-paragraphs "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula malesuada gravida. Nulla in massa eget quam porttitor consequat id egestas urna. Aliquam non pharetra dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed quis ante at nunc convallis aliquet at quis ligula. Aliquam accumsan consectetur risus. Quisque semper turpis a erat dapibus iaculis.\n\nCras sit amet laoreet lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus vestibulum a metus in laoreet. Phasellus eleifend eget dui vitae tincidunt. Aenean eu sapien sed nibh viverra facilisis in ac nulla. Integer quis odio eu sapien porta interdum in eu nulla. Sed sodales efficitur diam, vel iaculis ante bibendum vel. Praesent pretium ut lorem sit amet viverra. Etiam luctus nisi eget pharetra rutrum.\n\n")
 
 (defn guide []
   [:div
---
+++ src/cljs/rems/guide_utils.cljs
@@ -0,0 +1,15 @@
+(ns rems.guide-utils)
+
+(def lipsum
+  (str "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod "
+       "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim "
+       "veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex "
+       "ea commodo consequat. Duis aute irure dolor in reprehenderit in "
+       "voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur "
+       "sint occaecat cupidatat non proident, sunt in culpa qui officia "
+       "deserunt mollit anim id est laborum."))
+
+(def lipsum-short "Lorem ipsum dolor sit amet")
+
+(def lipsum-paragraphs
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vehicula malesuada gravida. Nulla in massa eget quam porttitor consequat id egestas urna. Aliquam non pharetra dolor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed quis ante at nunc convallis aliquet at quis ligula. Aliquam accumsan consectetur risus. Quisque semper turpis a erat dapibus iaculis.\n\nCras sit amet laoreet lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus vestibulum a metus in laoreet. Phasellus eleifend eget dui vitae tincidunt. Aenean eu sapien sed nibh viverra facilisis in ac nulla. Integer quis odio eu sapien porta interdum in eu nulla. Sed sodales efficitur diam, vel iaculis ante bibendum vel. Praesent pretium ut lorem sit amet viverra. Etiam luctus nisi eget pharetra rutrum.\n\n")
