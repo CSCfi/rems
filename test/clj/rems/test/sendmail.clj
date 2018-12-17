fdd1fa4efc844e184734536af6c92928b8c01ea9
Markku Rontu <markku.rontu@iki.fi>
2018-12-17 16:55:45 +0200

    refactor: use better names
    - uid is approver
    - uid2 is reviewer
    - extract "test-user" to applicant

--- test/clj/rems/test/sendmail.clj
+++ test/clj/rems/test/sendmail.clj
@@ -44,35 +44,36 @@
 
 (deftest test-sending-email
   (let [common-name "Test User"
-        applicant-attrs {"eppn" "test-user" "mail" "invalid-addr" "commonName" common-name}]
+        applicant "test-user"
+        applicant-attrs {"eppn" applicant "mail" "invalid-addr" "commonName" common-name}]
     (with-redefs [catalogue/cached {:localizations (catalogue/load-catalogue-item-localizations!)}]
       (binding [context/*root-path* "localhost:3000"]
-        (let [uid "approver"
-              uid2 "reviewer"
+        (let [approver "approver"
+              reviewer "reviewer"
               wfid1 (:id (db/create-workflow! {:organization "abc" :owneruserid "workflow-owner" :modifieruserid "workflow-owner" :title "" :fnlround 1}))
               wfid2 (:id (db/create-workflow! {:organization "abc" :owneruserid "workflow-owner" :modifieruserid "workflow-owner" :title "" :fnlround 0}))
               wfid3 (:id (db/create-workflow! {:organization "abc" :owneruserid "workflow-owner" :modifieruserid "workflow-owner" :title "" :fnlround 1}))
               wfid4 (:id (db/create-workflow! {:organization "abc" :owneruserid "workflow-owner" :modifieruserid "workflow-owner" :title "" :fnlround 1}))
-              _ (actors/add-reviewer! wfid1 uid2 0)
-              _ (actors/add-approver! wfid1 uid 1)
-              _ (actors/add-approver! wfid2 uid 0)
-              _ (actors/add-approver! wfid3 uid2 0)
-              _ (actors/add-approver! wfid3 uid 0)
-              _ (actors/add-approver! wfid3 uid 1)
-              _ (actors/add-approver! wfid4 uid 0)
-              _ (actors/add-approver! wfid4 uid2 0)
-              _ (actors/add-approver! wfid4 uid 1)
-              _ (actors/add-approver! wfid4 uid2 1)
+              _ (actors/add-reviewer! wfid1 reviewer 0)
+              _ (actors/add-approver! wfid1 approver 1)
+              _ (actors/add-approver! wfid2 approver 0)
+              _ (actors/add-approver! wfid3 reviewer 0)
+              _ (actors/add-approver! wfid3 approver 0)
+              _ (actors/add-approver! wfid3 approver 1)
+              _ (actors/add-approver! wfid4 approver 0)
+              _ (actors/add-approver! wfid4 reviewer 0)
+              _ (actors/add-approver! wfid4 approver 1)
+              _ (actors/add-approver! wfid4 reviewer 1)
               item1 (:id (db/create-catalogue-item! {:title "item" :form nil :resid nil :wfid wfid1}))
               item2 (:id (db/create-catalogue-item! {:title "item2" :form nil :resid nil :wfid wfid2}))
               item3 (:id (db/create-catalogue-item! {:title "item3" :form nil :resid nil :wfid wfid3}))
               item4 (:id (db/create-catalogue-item! {:title "item4" :form nil :resid nil :wfid wfid4}))
-              app1 (applications/create-new-draft "test-user" wfid1)
-              app2 (applications/create-new-draft "test-user" wfid2)
-              app3 (applications/create-new-draft "test-user" wfid2)
-              app4 (applications/create-new-draft "test-user" wfid2)
-              app5 (applications/create-new-draft "test-user" wfid3)
-              app6 (applications/create-new-draft "test-user" wfid4)
+              app1 (applications/create-new-draft applicant wfid1)
+              app2 (applications/create-new-draft applicant wfid2)
+              app3 (applications/create-new-draft applicant wfid2)
+              app4 (applications/create-new-draft applicant wfid2)
+              app5 (applications/create-new-draft applicant wfid3)
+              app6 (applications/create-new-draft applicant wfid4)
               _ (db/add-application-item! {:application app1 :item item1})
               _ (db/add-application-item! {:application app2 :item item2})
               _ (db/add-application-item! {:application app3 :item item2})
@@ -83,17 +84,17 @@
               items2 (rems.db.applications/get-catalogue-items-by-application-id app2)
               items3 (rems.db.applications/get-catalogue-items-by-application-id app3)
               items4 (rems.db.applications/get-catalogue-items-by-application-id app4)
-              approver-attrs {"eppn" "approver" "mail" "appr-invalid" "commonName" "App Rover"}
-              approver (db/add-user! {:user uid :userattrs (generate-string approver-attrs)})
+              approver-attrs {"eppn" approver "mail" "appr-invalid" "commonName" "App Rover"}
+              _ (db/add-user! {:user approver :userattrs (generate-string approver-attrs)})
               reviewer-attrs {"eppn" "reviewer" "mail" "rev-invalid" "commonName" "Rev Iwer"}
-              reviewer (db/add-user! {:user uid2 :userattrs (generate-string reviewer-attrs)})
-              _ (db/add-user! {:user "test-user" :userattrs (generate-string applicant-attrs)})
+              _ (db/add-user! {:user reviewer :userattrs (generate-string reviewer-attrs)})
+              _ (db/add-user! {:user applicant :userattrs (generate-string applicant-attrs)})
               outside-attrs {"eppn" "outside-reviewer" "mail" "out-invalid" "commonName" "Out Sider"}
               _ (db/add-user! {:user "outside-reviewer" :userattrs (generate-string outside-attrs)})]
           (testing "Applicant and reviewer should receive an email about the new application"
             (conjure/mocking [email/confirm-application-creation
                               email/review-request]
-                             (applications/submit-application "test-user" app1)
+                             (applications/submit-application applicant app1)
                              (conjure/verify-called-once-with-args email/confirm-application-creation
                                                                    app1
                                                                    items1)
@@ -104,7 +105,7 @@
                                                                    items1)))
           (testing "Approver gets notified after review round"
             (conjure/mocking [email/approval-request]
-                             (applications/review-application "reviewer" app1 0 "")
+                             (applications/review-application reviewer app1 0 "")
                              (conjure/verify-called-once-with-args email/approval-request
                                                                    approver-attrs
                                                                    common-name
@@ -112,18 +113,18 @@
                                                                    items1)))
           (testing "Applicant gets notified when application is approved"
             (conjure/mocking [email/status-change-alert]
-                             (applications/approve-application "approver" app1 1 "")
+                             (applications/approve-application approver app1 1 "")
                              (conjure/verify-called-once-with-args email/status-change-alert
                                                                    applicant-attrs
                                                                    app1
                                                                    items1
                                                                    "approved")))
           (conjure/mocking [email/send-mail]
-                           (applications/submit-application "test-user" app2)
-                           (applications/submit-application "test-user" app3))
+                           (applications/submit-application applicant app2)
+                           (applications/submit-application applicant app3))
           (testing "Applicant gets notified when application is rejected"
             (conjure/mocking [email/status-change-alert]
-                             (applications/reject-application "approver" app2 0 "")
+                             (applications/reject-application approver app2 0 "")
                              (conjure/verify-called-once-with-args email/status-change-alert
                                                                    applicant-attrs
                                                                    app2
@@ -131,7 +132,7 @@
                                                                    "rejected")))
           (testing "Applicant gets notified when application is returned to him/her"
             (conjure/mocking [email/status-change-alert]
-                             (applications/return-application "approver" app3 0 "")
+                             (applications/return-application approver app3 0 "")
                              (conjure/verify-called-once-with-args email/status-change-alert
                                                                    applicant-attrs
                                                                    app3
@@ -139,22 +140,22 @@
                                                                    "returned")))
           (testing "Emails should not be sent when actions fail"
             (conjure/mocking [email/send-mail]
-                             (is (thrown? NotAuthorizedException (applications/approve-application "test-user" app4 1 ""))
+                             (is (thrown? NotAuthorizedException (applications/approve-application applicant app4 1 ""))
                                  "Approval should fail")
-                             (is (thrown? NotAuthorizedException (applications/reject-application "test-user" app4 1 ""))
+                             (is (thrown? NotAuthorizedException (applications/reject-application applicant app4 1 ""))
                                  "Rejection should fail")
-                             (is (thrown? NotAuthorizedException (applications/review-application "test-user" app4 1 ""))
+                             (is (thrown? NotAuthorizedException (applications/review-application applicant app4 1 ""))
                                  "Review should fail")
-                             (is (thrown? NotAuthorizedException (applications/return-application "test-user" app4 1 ""))
+                             (is (thrown? NotAuthorizedException (applications/return-application applicant app4 1 ""))
                                  "Return should fail")
-                             (is (thrown? NotAuthorizedException (applications/close-application "test-user" app4 2 ""))
+                             (is (thrown? NotAuthorizedException (applications/close-application applicant app4 2 ""))
                                  "closing should fail")
-                             (is (thrown? NotAuthorizedException (applications/withdraw-application "test-user" app1 2 ""))
+                             (is (thrown? NotAuthorizedException (applications/withdraw-application applicant app1 2 ""))
                                  "withdraw should fail")
                              (conjure/verify-call-times-for email/send-mail 0)))
           (testing "Applicant is notified of closed application"
             (conjure/mocking [email/status-change-alert]
-                             (applications/close-application "approver" app1 1 "")
+                             (applications/close-application approver app1 1 "")
                              (conjure/verify-called-once-with-args email/status-change-alert
                                                                    applicant-attrs
                                                                    app1
@@ -162,8 +163,8 @@
                                                                    "closed")))
           (testing "Applicant is notified of withdrawn application"
             (conjure/mocking [email/status-change-alert]
-                             (applications/submit-application "test-user" app4)
-                             (applications/withdraw-application "test-user" app4 0 "")
+                             (applications/submit-application applicant app4)
+                             (applications/withdraw-application applicant app4 0 "")
                              (conjure/verify-called-once-with-args email/status-change-alert
                                                                    applicant-attrs
                                                                    app4
@@ -172,37 +173,37 @@
           (testing "3rd party reviewer is notified after a review request"
             (conjure/mocking [email/review-request
                               email/status-change-alert]
-                             (applications/submit-application "test-user" app4)
-                             (applications/send-review-request "approver" app4 0 "" uid2)
-                             (applications/send-review-request "approver" app4 0 "" uid2)
+                             (applications/submit-application applicant app4)
+                             (applications/send-review-request approver app4 0 "" reviewer)
+                             (applications/send-review-request approver app4 0 "" reviewer)
                              (conjure/verify-called-once-with-args email/review-request
                                                                    reviewer-attrs
                                                                    common-name
                                                                    app4
                                                                    items4)
                              (conjure/verify-call-times-for email/review-request 1)
-                             (applications/perform-third-party-review "reviewer" app4 0 "")))
+                             (applications/perform-third-party-review reviewer app4 0 "")))
           (testing "Actors are notified when their attention is no longer required"
             (conjure/mocking [email/action-not-needed]
-                             (applications/submit-application "test-user" app5)
-                             (applications/approve-application "approver" app5 0 "")
+                             (applications/submit-application applicant app5)
+                             (applications/approve-application approver app5 0 "")
                              ;; NB: reviewer is here also approver
                              (conjure/verify-called-once-with-args email/action-not-needed
                                                                    reviewer-attrs
                                                                    common-name
                                                                    app5))
             (conjure/mocking [email/action-not-needed]
-                             (applications/send-review-request "approver" app5 1 "" uid2)
-                             (applications/approve-application "approver" app5 1 "")
+                             (applications/send-review-request approver app5 1 "" reviewer)
+                             (applications/approve-application approver app5 1 "")
                              (conjure/verify-called-once-with-args email/action-not-needed
                                                                    reviewer-attrs
                                                                    common-name
                                                                    app5)))
           (testing "Multiple rounds with lazy actors"
             (conjure/mocking [email/action-not-needed]
-                             (applications/submit-application "test-user" app6)
-                             (applications/send-review-request "approver" app6 0 "" "outside-reviewer")
-                             (applications/approve-application "approver" app6 0 "")
+                             (applications/submit-application applicant app6)
+                             (applications/send-review-request approver app6 0 "" "outside-reviewer")
+                             (applications/approve-application approver app6 0 "")
                              (conjure/verify-call-times-for email/action-not-needed 2)
                              (conjure/verify-nth-call-args-for 1
                                                                email/action-not-needed
@@ -215,9 +216,9 @@
                                                                common-name
                                                                app6))
             (conjure/mocking [email/action-not-needed]
-                             (applications/send-review-request "approver" app6 1 "" "outside-reviewer")
+                             (applications/send-review-request approver app6 1 "" "outside-reviewer")
                              (applications/perform-third-party-review "outside-reviewer" app6 1 "")
-                             (applications/approve-application "approver" app6 1 "")
+                             (applications/approve-application approver app6 1 "")
                              (conjure/verify-called-once-with-args email/action-not-needed
                                                                    reviewer-attrs
                                                                    common-name
