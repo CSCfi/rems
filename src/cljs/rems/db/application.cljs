0717058f5158e32f945d22167e85c6c2fc163383
Markku Rontu <markku.rontu@iki.fi>
2019-01-15 21:58:19 +0200

    fix: remove anonymous fn

--- src/cljs/rems/db/application.cljs
+++ src/cljs/rems/db/application.cljs
@@ -1,8 +1,8 @@
 (ns rems.db.application)
 
 (defn draft? [state]
-  #(contains? #{"draft" "returned" "withdrawn"
-                :rems.workflow.dynamic/draft :rems.workflow.dynamic/returned
-                ;; TODO add dynamic withdrawn state
-                }
-              state))
+  (contains? #{"draft" "returned" "withdrawn"
+               :rems.workflow.dynamic/draft :rems.workflow.dynamic/returned
+               ;; TODO add dynamic withdrawn state
+               }
+             state))
