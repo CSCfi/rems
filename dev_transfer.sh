696245d05c74d705d6bfe18f1fd4dccce704e565
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-01-21 11:13:25 +0200

    remove support for transferring REMS1 data
    - scripts
    - test data
    - CI jobs

--- dev_transfer.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -xeu
-# Usage: dev_transfer.sh rems_users_dump.sql rems_data_dump.sql
-rems_test_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' rems_test)
-PGHOST=$rems_test_ip PGUSER=rems PGDATABASE=rems ./transfer-users.sh $1
-PGHOST=$rems_test_ip PGUSER=rems PGDATABASE=rems ./transfer-data.sh $2
--- resources/sql/init.sql
+++ resources/sql/init.sql
@@ -5,7 +5,3 @@ CREATE DATABASE rems_test OWNER rems_test;
 
 CREATE USER rems;
 CREATE DATABASE rems OWNER rems;
-
-\connect rems
-DROP SCHEMA IF EXISTS transfer CASCADE;
-CREATE SCHEMA transfer AUTHORIZATION rems;
