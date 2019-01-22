15ab1c3719d0192216c0922bb2396eee073546d3
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-01-22 15:53:26 +0200

    more robust 20180913105209-resource-owner.up migration

--- resources/migrations/20180913105209-resource-owner.up.sql
+++ resources/migrations/20180913105209-resource-owner.up.sql
@@ -1,2 +1,8 @@
 alter table resource
-  add column ownerUserId varchar(255) NOT NULL;
+  add column ownerUserId varchar(255);
+--;;
+update resource
+  set ownerUserId = modifierUserId;
+--;;
+alter table resource
+  alter column ownerUserId set not null;
