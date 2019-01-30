d50558acc83f12f70fe11ed0ef1dd2d8992ba46a
Markku Rontu <markku.rontu@iki.fi>
2019-01-30 09:39:48 +0200

    fix: allow values >= 0

--- src/clj/rems/api/forms.clj
+++ src/clj/rems/api/forms.clj
@@ -20,7 +20,7 @@
   {:title {s/Keyword s/Str}
    :optional s/Bool
    :type (s/enum "attachment" "date" "description" "label" "multiselect" "option" "text" "texta")
-   (s/optional-key :maxlength) (s/maybe (s/constrained s/Int (complement neg?)))
+   (s/optional-key :maxlength) (s/maybe (s/constrained s/Int (partial <= 0)))
    (s/optional-key :options) [{:key s/Str
                                :label {s/Keyword s/Str}}]
    (s/optional-key :input-prompt) {s/Keyword s/Str}})
