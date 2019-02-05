ac1b1d9c6d3d146e898f57ef85864d237884d74f
Markku Rontu <markku.rontu@iki.fi>
2019-02-05 14:00:32 +0200

    feat: support attachment license in resource licenses

--- src/cljs/rems/administration/resource.cljs
+++ src/cljs/rems/administration/resource.cljs
@@ -3,7 +3,7 @@
             [re-frame.core :as rf]
             [rems.administration.administration :refer [administration-navigator-container]]
             [rems.administration.components :refer [inline-info-field]]
-            [rems.atoms :refer [external-link info-field readonly-checkbox]]
+            [rems.atoms :refer [external-link readonly-checkbox]]
             [rems.collapsible :as collapsible]
             [rems.common-util :refer [andstr]]
             [rems.spinner :as spinner]
@@ -42,6 +42,12 @@
    {:href "/#/administration/create-resource"}
    (text :t.administration/create-resource)])
 
+(defn attachment-link [id title]
+  [:a.btn.btn-secondary.mr-2
+   {:href (str "api/licenses/attachments/" id)
+    :target :_new}
+   title " " [external-link]])
+
 (defn license-view [license language]
   (into [:div.form-item
          [:h4 (text-format :t.administration/license-field (get-in license [:localizations language :title]))]
@@ -60,6 +66,15 @@
                     [inline-info-field (str (text :t.create-license/external-link)
                                             " "
                                             (str/upper-case (name langcode))) [:a {:target :_blank :href (:textcontent localization)} (:textcontent localization) " " [external-link]]]))
+                (when (= "attachment" (:licensetype license))
+                  (for [[langcode localization] (:localizations license)]
+                    (when (:attachment-id localization)
+                      [inline-info-field
+                       (str (text :t.create-license/license-attachment)
+                            " "
+                            (str/upper-case (name langcode)))
+                       [attachment-link (:attachment-id localization) (:title localization)]
+                       {:no-box? true}])))
                 [[inline-info-field (text :t.administration/start) (localize-time (:start license))]
                  [inline-info-field (text :t.administration/end) (localize-time (:end license))]])))
 
