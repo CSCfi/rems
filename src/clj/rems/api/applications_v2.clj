4560703ee63074baf329fa6e7a04f0d7b90fbc05
Esko Luontola <esko@luontola.fi>
2019-02-09 02:18:14 +0200

    refactor

--- src/clj/rems/api/applications_v2.clj
+++ src/clj/rems/api/applications_v2.clj
@@ -16,88 +16,106 @@
 ;;; user permissions
 
 (defn- set-permissions
-  "Updates user and role specific permissions for the application.
-   In `permission-map` the key is the user or role, and the value is
-   a list of permissions to set for that user or role.
-
-   Roles are keywords and user IDs are strings. See `user-permissions` for
-   the supported roles. User specific permissions can be used e.g. to give
-   specific users commenting access or to give non-applicant members read-only
-   access to the application.
+  "Sets permissions for the application. Use `set-role-permissions` or
+  `set-user-permissions` instead of calling this function directly.
+  
+   In `permission-map`, the key is the subject (user or role), and the value
+   is a list of permissions to set for that subject.
 
    There is a difference between an empty list of permissions and nil.
-   Empty list of permissions means that the user has read-only access to
-   the application, whereas nil means that the user cannot even see the
-   application.
+   Empty list means that the user has read-only access to the application,
+   whereas nil means that the user has no access to the application.
 
-   The permissions can be keywords signifying the commands that the user
-   is allowed to run, but they could also be used to signify whether the user
-   can see all events and comments from the reviewers (e.g. `:see-everything`)."
-  [application permission-map]
+   The permissions must be keywords. They may represent commands that the user
+   is allowed to run, or they may be used to specify whether the user can see
+   all events and comments from the reviewers (e.g. `:see-everything`)."
+  [application category permission-map]
   (reduce (fn [application [subject permissions]]
-            (let [category (cond
-                             (keyword? subject) :permissions/by-role
-                             (string? subject) :permissions/by-user)]
-              (if (nil? permissions)
-                (update application category dissoc subject)
-                (assoc-in application [category subject] (set permissions)))))
+            (if (nil? permissions)
+              (update application category dissoc subject)
+              (assoc-in application [category subject] (set permissions))))
           application
           permission-map))
+
+(defn- set-role-permissions
+  "Sets role specific permissions for the application.
+
+   Users will be mapped to roles based on application state.
+   The supported roles are defined in `user-permissions`.
+   
+   The keys in `permission-map` are the role names as keywords.
+   See `set-permissions` for details on `permission-map`."
+  [application permission-map]
+  (assert (every? keyword? (keys permission-map)))
+  (set-permissions application :permissions/by-role permission-map))
+
+(defn- set-user-permissions
+  "Sets user specific permissions for the application.
+  
+   User specific permissions can be used e.g. to give specific users
+   commenting access or to give non-applicant members read-only
+   access to the application.
+
+   The keys in `permission-map` are the user IDs as strings.
+   See `set-permissions` for details on `permission-map`."
+  [application permission-map]
+  (assert (every? string? (keys permission-map)))
+  (set-permissions application :permissions/by-user permission-map))
 
 (deftest test-set-permissions
   (testing "role-specific permissions"
     (is (= {:permissions/by-role {:role #{:foo :bar}}}
            (-> {}
-               (set-permissions {:role [:foo :bar]}))))
+               (set-role-permissions {:role [:foo :bar]}))))
     (testing "updating"
       (is (= {:permissions/by-role {:role #{:gazonk}}}
              (-> {}
-                 (set-permissions {:role [:foo :bar]})
-                 (set-permissions {:role [:gazonk]})))))
+                 (set-role-permissions {:role [:foo :bar]})
+                 (set-role-permissions {:role [:gazonk]})))))
     (testing "removing (read-only access)"
       (is (= {:permissions/by-role {:role #{}}}
              (-> {}
-                 (set-permissions {:role [:foo :bar]})
-                 (set-permissions {:role []})))))
+                 (set-role-permissions {:role [:foo :bar]})
+                 (set-role-permissions {:role []})))))
     (testing "removing (no access)"
       (is (= {:permissions/by-role {}}
              (-> {}
-                 (set-permissions {:role [:foo :bar]})
-                 (set-permissions {:role nil}))))))
+                 (set-role-permissions {:role [:foo :bar]})
+                 (set-role-permissions {:role nil}))))))
 
   (testing "user-specific permissions"
     (is (= {:permissions/by-user {"user" #{:foo :bar}}}
            (-> {}
-               (set-permissions {"user" [:foo :bar]}))))
+               (set-user-permissions {"user" [:foo :bar]}))))
     (testing "updating"
       (is (= {:permissions/by-user {"user" #{:gazonk}}}
              (-> {}
-                 (set-permissions {"user" [:foo :bar]})
-                 (set-permissions {"user" [:gazonk]})))))
+                 (set-user-permissions {"user" [:foo :bar]})
+                 (set-user-permissions {"user" [:gazonk]})))))
     (testing "removing (read-only access)"
       (is (= {:permissions/by-user {"user" #{}}}
              (-> {}
-                 (set-permissions {"user" [:foo :bar]})
-                 (set-permissions {"user" []})))))
+                 (set-user-permissions {"user" [:foo :bar]})
+                 (set-user-permissions {"user" []})))))
     (testing "removing (no access)"
       (is (= {:permissions/by-user {}}
              (-> {}
-                 (set-permissions {"user" [:foo :bar]})
-                 (set-permissions {"user" nil}))))))
+                 (set-user-permissions {"user" [:foo :bar]})
+                 (set-user-permissions {"user" nil}))))))
 
   (testing "can set permissions for multiple roles/users"
     (is (= {:permissions/by-role {:role-1 #{:foo}
                                   :role-2 #{:bar}}}
            (-> {}
-               (set-permissions {:role-1 [:foo]
-                                 :role-2 [:bar]})))))
+               (set-role-permissions {:role-1 [:foo]
+                                      :role-2 [:bar]})))))
   (testing "does not alter unrelated roles/users"
     (is (= {:permissions/by-role {:unrelated #{:foo}
                                   :role #{:gazonk}}}
            (-> {}
-               (set-permissions {:unrelated [:foo]
-                                 :role [:bar]})
-               (set-permissions {:role [:gazonk]}))))))
+               (set-role-permissions {:unrelated [:foo]
+                                      :role [:bar]})
+               (set-role-permissions {:role [:gazonk]}))))))
 
 (defn- user-permissions
   "Returns the specified user's permissions to this application.
@@ -179,9 +197,9 @@
              ;; TODO: or would :workflow.dynamic/state be more appropriate?
              :workflow/state :rems.workflow.dynamic/draft ; TODO: other workflows
              :workflow.dynamic/handlers (:workflow.dynamic/handlers event))
-      (set-permissions {:applicant #{::dynamic/add-member
-                                     ::dynamic/save-draft
-                                     ::dynamic/submit}})))
+      (set-role-permissions {:applicant #{::dynamic/add-member
+                                          ::dynamic/save-draft
+                                          ::dynamic/submit}})))
 
 (defn- set-accepted-licences [licenses accepted-licenses]
   (map (fn [license]
@@ -206,12 +224,12 @@
   [application event]
   (-> application
       (assoc :workflow/state ::dynamic/submitted)
-      (set-permissions {:applicant #{::dynamic/add-member}
-                        :handler #{::dynamic/approve
-                                   ::dynamic/reject
-                                   ::dynamic/return
-                                   ::dynamic/request-decision
-                                   ::dynamic/request-comment}})))
+      (set-role-permissions {:applicant #{::dynamic/add-member}
+                             :handler #{::dynamic/approve
+                                        ::dynamic/reject
+                                        ::dynamic/return
+                                        ::dynamic/request-decision
+                                        ::dynamic/request-comment}})))
 
 (defmethod application-view-specific :application.event/returned
   [application event]
