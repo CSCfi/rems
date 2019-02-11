20363b6bef2940d415dbbda79870208ba3b64aed
Leena Wooller <leena.wooller@csc.fi>
2019-02-11 15:52:48 +0200

    Override translation value, WIP

--- src/clj/rems/locales.clj
+++ src/clj/rems/locales.clj
@@ -5,7 +5,12 @@
             [rems.config :refer [env]])
   (:import (java.io FileNotFoundException)))
 
-(defn- load-translation [language translations-directory]
+(defn- deep-merge [a & maps]
+  (if (map? a)
+    (apply merge-with deep-merge a maps)
+    (apply merge-with deep-merge maps)))
+
+(defn- load-translation [language translations-directory extra-translations-directory]
   (let [file (when translations-directory
                (io/file translations-directory (str (name language) ".edn")))
         resource-path (str "translations/" (name language) ".edn")
@@ -14,14 +19,17 @@
                       (and file (.exists file)) file
                       resource resource
                       :else (throw (FileNotFoundException.
-                                    (if file
-                                      (str "translations for " language " language could not be found in " file " file or " resource-path " resource")
-                                      (str "translations for " language " language could not be found in " resource-path " resource and " :translations-directory " was not set")))))]
-    {language (read-string (slurp translation))}))
+                                     (if file
+                                       (str "translations for " language " language could not be found in " file " file or " resource-path " resource")
+                                       (str "translations for " language " language could not be found in " resource-path " resource and " :translations-directory " was not set")))))
+        result (deep-merge {language (read-string (slurp translation))}
+                           {language {:t {:administration {:catalogue-items "Kettupossu items"}}}})] ;;TODO read from extra-translations-directory
+    (clojure.pprint/pprint result)
+    result))
 
-(defn load-translations [{:keys [languages translations-directory]}]
+(defn load-translations [{:keys [languages translations-directory extra-translations-directory]}]
   (->> languages
-       (map #(load-translation % translations-directory))
+       (map #(load-translation % translations-directory extra-translations-directory))
        (apply merge)))
 
 (defstate translations :start (load-translations env))
