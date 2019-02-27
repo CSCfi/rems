e3fe6bd8ed0a1782818e96b69eff7993e6b7b115
Markku Rontu <markku.rontu@iki.fi>
2019-02-27 14:27:24 +0200

    fix: add missing event handlers

--- src/cljs/rems/application.cljs
+++ src/cljs/rems/application.cljs
@@ -170,6 +170,8 @@
       :target :_new}
      "PDF " (external-link)]))
 
+(rf/reg-event-db ::set-field (fn [db [_ id value]] (assoc-in db [::edit-application :items id :value] value)))
+(rf/reg-event-db ::set-license (fn [db [_ id value]] (assoc-in db [::edit-application :licenses id] value)))
 (defn- set-field-value
   [id]
   (fn [event]
