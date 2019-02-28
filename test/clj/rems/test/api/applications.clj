6d5a1a47ab1d661396085af80d5f83ff747649e0
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-02-28 14:48:47 +0200

    remove applications-api-command-test, port relevant parts to dynamic

--- test/clj/rems/test/api/applications.clj
+++ test/clj/rems/test/api/applications.clj
@@ -33,86 +33,6 @@
           (is (= "application/transit+json; charset=utf-8" (get-in response [:headers "Content-Type"])))
           (is (= 7 (count data))))))))
 
-#_(deftest applications-api-command-test
-  (let [api-key "42"
-        user-id "alice"
-        another-user "alice_smith"
-        catid 2]
-    (let [response (-> (request :post (str "/api/applications/save"))
-                       (authenticate api-key user-id)
-                       (json-body {:command "save"
-                                   :catalogue-items [catid]
-                                   :items {1 "REST-Test"}})
-                       app)
-          cmd-response (read-body response)
-          application-id (:id cmd-response)]
-
-      (testing "saving"
-        (is (:success cmd-response))
-        (is (not (:errors cmd-response)))
-        (is (= "draft" (:state cmd-response)))
-        (is (not (:valid cmd-response)))
-        (is (= [{:type "t.form.validation/required" :field-id 2}
-                {:type "t.form.validation/required" :license-id 1}
-                {:type "t.form.validation/required" :license-id 2}]
-               (:validation cmd-response))))
-      (testing "retrieving"
-        (let [response (-> (request :get (str "/api/applications/" application-id))
-                           (authenticate api-key user-id)
-                           app)
-              application (read-body response)]
-          (is (not (:errors application)))
-          (is (= application-id (:id (:application application))))
-          (is (= "draft" (:state (:application application))))
-          (is (= 2 (count (:licenses application))))
-          (is (< 6 (count (:items application))))))
-      (testing "retrieving as other user"
-        (let [response (-> (request :get (str "/api/applications/" application-id))
-                           (authenticate api-key another-user)
-                           app)]
-          (is (response-is-forbidden? response))))
-      (testing "saving as other user"
-        (let [response (-> (request :post (str "/api/applications/save"))
-                           (authenticate api-key another-user)
-                           (json-body {:command "save"
-                                       :application-id application-id
-                                       :items {1 "REST-Test"}})
-                           app)]
-          (is (response-is-forbidden? response))))
-      (testing "submitting"
-        (let [response (-> (request :post (str "/api/applications/save"))
-                           (authenticate api-key user-id)
-                           (json-body {:command "submit"
-                                       :application-id application-id
-                                       :items {1 "REST-Test"
-                                               2 "2017-2018"
-                                               3 "The purpose is to test this REST service.}"}
-                                       :licenses {1 "approved" 2 "approved"}})
-                           app)
-              cmd-response (read-body response)]
-          (is (:success cmd-response))
-          (is (not (:errors cmd-response)))
-          (is (= application-id (:id cmd-response)))
-          (is (= "applied" (:state cmd-response)))
-          (is (:valid cmd-response))
-          (is (empty? (:validation cmd-response)))))
-      (testing "approving"
-        (let [response (-> (request :post (str "/api/applications/judge"))
-                           (authenticate api-key "developer")
-                           (json-body {:command "approve"
-                                       :application-id application-id
-                                       :round 0
-                                       :comment "msg"})
-                           app)
-              cmd-response (read-body response)
-              application-response (-> (request :get (str "/api/applications/" application-id))
-                                       (authenticate api-key user-id)
-                                       app)
-              application (:application (read-body application-response))]
-          (is (:success cmd-response))
-          (is (= "approved" (:state application)))
-          (is (= [nil nil "msg"] (map :comment (:events application)))))))))
-
 #_(deftest application-validation-test
   (let [api-key "42"
         user-id "alice"
@@ -809,6 +729,19 @@
           (is (= "workflow/dynamic" (get-in saved [:application :workflow :type])))
           (is (= "rems.workflow.dynamic/draft" (get-in saved [:application :state])))
           (is (= "dynamic test" (get-in saved [:items 0 :value])))))
+      (testing "getting application as other user is forbidden"
+        (is (response-is-forbidden?
+             (-> (request :get (str "/api/applications/" application-id))
+                 (authenticate api-key "bob")
+                 app))))
+      (testing "saving fields as other user is forbidden"
+        (is (response-is-forbidden?
+             (-> (request :post "/api/applications/save")
+                 (authenticate api-key "bob")
+                 (json-body {:command "save"
+                             :application-id application-id
+                             :items {1 "xxxxx"}})
+                 app))))
       (testing "can't submit with missing required fields"
         (is (= {:success false :errors [{:type "t.form.validation/required" :field-id 2}
                                         {:type "t.form.validation/required" :license-id 1}
