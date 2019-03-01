1fc102cab91803845e081dcd9212462a27199160
Esko Luontola <esko@luontola.fi>
2019-03-01 11:23:39 +0200

    refactor: extract migrate-application!

--- test/clj/rems/test/migrations/convert_to_dynamic_applications.clj
+++ test/clj/rems/test/migrations/convert_to_dynamic_applications.clj
@@ -4,7 +4,9 @@
             [clojure.test :refer :all]
             [rems.db.applications :as applications]
             [rems.db.core :as db :refer [*db*]]
+            [rems.db.roles :as roles]
             [rems.db.test-data :as test-data]
+            [rems.db.users :as users]
             [rems.db.workflow :as workflow]
             [rems.migrations.convert-to-dynamic-applications :refer :all]
             [rems.test.api :refer [api-fixture]]))
@@ -13,33 +15,23 @@
   :once
   api-fixture)
 
-(deftest test-migration
-  (let [applications (applications/get-applications-impl-batch "whatever" {})
-        application (->> applications
-                         (filter #(= 1 (:id %)))
-                         (first))
-        dynamic-workflows (->> (workflow/get-workflows {})
-                               (filter #(= "workflow/dynamic" (get-in % [:workflow :type]))))
-        new-workflow (first dynamic-workflows)]
-    (assert application)
-    (assert (= 1 (count dynamic-workflows)))
-
-    ;(prn *db*)
+(defn migrate-application! [application-id workflow-id]
+  (let [read-user (->> (users/get-all-users)
+                       (map (fn [user] (assoc user :roles (roles/get-roles (:eppn user)))))
+                       (filter (fn [user] (contains? (:roles user) :approver)))
+                       first
+                       :eppn)
+        form (applications/get-form-for read-user application-id)
+        application (:application form)
+        workflow (workflow/get-workflow workflow-id)]
+    (assert (= "workflow/dynamic" (get-in workflow [:workflow :type])))
 
     (jdbc/with-db-connection [conn *db*]
       ;; use the dynamic workflow
-      (jdbc/execute! conn ["update catalogue_item set wfid = ?" (:id new-workflow)])
-      (jdbc/execute! conn ["update catalogue_item_application set wfid = ? where id = ?" (:id new-workflow) (:id application)])
+      (jdbc/execute! conn ["update catalogue_item_application set wfid = ? where id = ?" (:id workflow) (:id application)])
       ;; delete old events
       (jdbc/execute! conn ["delete from application_event where appid = ?" (:id application)]))
 
-    (println "--- before ---")
-    (pprint application)
-    (applications/add-application-created-event! {:application-id (:id application)
-                                                  :catalogue-item-ids (->> (:catalogue-items application)
-                                                                           (map :id))
-                                                  :time (:start application)
-                                                  :actor (:applicantuserid application)})
     ;; TODO: migrate "save" or actually just the form values
     ;; TODO: migrate "apply"
     ;; TODO: migrate "approve"
@@ -52,15 +44,32 @@
     ;; TODO: migrate "withdraw"
     ;; TODO: migrate "close"
 
+    (applications/add-application-created-event! {:application-id (:id application)
+                                                  :catalogue-item-ids (->> (:catalogue-items application)
+                                                                           (map :id))
+                                                  :time (:start application)
+                                                  :actor (:applicantuserid application)})))
 
+(deftest test-migration
+  (let [applications (applications/get-applications-impl-batch "whatever" {})
+        application (->> applications
+                         (filter #(= 1 (:id %)))
+                         (first))
+        dynamic-workflows (->> (workflow/get-workflows {})
+                               (filter #(= "workflow/dynamic" (get-in % [:workflow :type]))))
+        new-workflow (first dynamic-workflows)]
+    (assert application)
+    (assert (= 1 (count dynamic-workflows)))
+
+    (jdbc/with-db-connection [conn *db*]
+      ;; use the dynamic workflow
+      (jdbc/execute! conn ["update catalogue_item set wfid = ?" (:id new-workflow)]))
+
+    (println "--- before ---")
+    (pprint application)
+    (migrate-application! (:id application) (:id new-workflow))
     (println "--- after ---")
-    (pprint (applications/get-application-state (:id application)))
-
-
-
-    (prn (count applications)))
-
-
+    (pprint (applications/get-application-state (:id application))))
 
   (let [application (applications/get-application-state 1)]
     (is (= {:id 1
