f277edfa40b7617fbe79b9e5a070bb47a4d964be
Esko Luontola <esko@luontola.fi>
2019-03-18 16:15:27 +0200

    add feature toggles

--- src/clj/rems/api/actions.clj
+++ src/clj/rems/api/actions.clj
@@ -3,6 +3,7 @@
             [rems.api.applications-v2 :refer [get-open-reviews-v2 get-handled-reviews-v2]]
             [rems.api.schema :refer :all]
             [rems.api.util]
+            [rems.config :refer [env]]
             [rems.db.applications :as applications]
             [rems.util :refer [getx-user-id]]
             [ring.util.http-response :refer :all]
@@ -49,10 +50,12 @@
       :summary "Lists applications which the user needs to review"
       :roles #{:handler :commenter :decider :past-commenter :past-decider}
       :return s/Any ;; TODO: add schema
-      (ok (get-open-reviews-v2 (getx-user-id))))
+      (when (:dev env) ; TODO: remove feature toggle
+        (ok (get-open-reviews-v2 (getx-user-id)))))
 
     (GET "/handled" []
       :summary "Lists applications which the user has already reviewed"
       :roles #{:handler :commenter :decider :past-commenter :past-decider}
       :return s/Any ;; TODO: add schema
-      (ok (get-handled-reviews-v2 (getx-user-id))))))
+      (when (:dev env) ; TODO: remove feature toggle
+        (ok (get-handled-reviews-v2 (getx-user-id)))))))
