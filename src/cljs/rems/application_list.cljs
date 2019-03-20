1e15a4354fae2d3ec9c6bdf18849a2fb89c860a6
Esko Luontola <esko@luontola.fi>
2019-03-20 15:47:58 +0200

    fix link

--- src/cljs/rems/application_list.cljs
+++ src/cljs/rems/application_list.cljs
@@ -8,7 +8,7 @@
 
 (defn- view-button [app]
   [:a.btn.btn-primary
-   {:href (str "#/application/" (:id app))}
+   {:href (str "#/application/" (:application/id app))}
    (text :t.applications/view)])
 
 (defn- format-catalogue-items [app]
