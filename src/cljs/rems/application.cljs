6fb6c94d90b3341b0273e8845ff2470a47a0b79d
Esko Luontola <esko@luontola.fi>
2019-03-20 13:40:24 +0200

    application page: use v2 API through v1 compatibility layer

--- src/cljs/rems/application.cljs
+++ src/cljs/rems/application.cljs
@@ -87,7 +87,7 @@
 (rf/reg-fx
  ::fetch-application
  (fn [id]
-   (fetch (str "/api/applications/" id)
+   (fetch (str "/api/v2/applications/" id "/migration")
           {:handler #(rf/dispatch [::fetch-application-result %])})))
 
 (rf/reg-event-db
