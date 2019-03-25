2e423dfb2f6bc35452e78d00ec7755ac5ef08275
Esko Luontola <esko@luontola.fi>
2019-03-25 16:44:16 +0200

    refactor: add test descriptions

--- test/clj/rems/test_form_validation.clj
+++ test/clj/rems/test_form_validation.clj
@@ -3,44 +3,45 @@
             [rems.form-validation :refer [validate]]))
 
 (deftest test-validate
-  (is (= :valid (validate
-                 {:items [{:title "A"
-                           :optional true
-                           :value nil}
-                          {:title "B"
-                           :optional false
-                           :value "xyz"}
-                          {:title "C"
-                           :optional false
-                           :value "1"}]})))
+  (testing "all fields filled & all licenses accepted"
+    (is (= :valid (validate
+                   {:items [{:title "A"
+                             :optional false
+                             :value "a"}]
+                    :licenses [{:title "LGPL"
+                                :approved true}]}))))
 
-  (is (= [{:type :t.form.validation/required :license-id 123}]
-         (validate
-          {:items [{:title "A"
-                    :optional false
-                    :value "a"}]
-           :licenses [{:id 123
-                       :title "LGPL"}]})))
+  (testing "optional fields"
+    (is (= :valid (validate
+                   {:items [{:title "A"
+                             :optional true
+                             :value nil}
+                            {:title "B"
+                             :optional true
+                             :value ""}]}))))
 
-  (is (= :valid (validate
-                 {:items [{:title "A"
-                           :optional false
-                           :value "a"}]
-                  :licenses [{:title "LGPL"
-                              :approved true}]})))
+  (testing "error: license required"
+    (is (= [{:type :t.form.validation/required :license-id 123}]
+           (validate
+            {:items [{:title "A"
+                      :optional false
+                      :value "a"}]
+             :licenses [{:id 123
+                         :title "LGPL"}]}))))
 
-  (is (= [{:type :t.form.validation/required :field-id 2}
-          {:type :t.form.validation/required :field-id 3}]
-         (validate
-          {:items [{:id 1
-                    :localizations {:en {:title "A"}}
-                    :optional true
-                    :value nil}
-                   {:id 2
-                    :localizations {:en {:title "B"}}
-                    :optional false
-                    :value ""}
-                   {:id 3
-                    :localizations {:en {:title "C"}}
-                    :optional false
-                    :value nil}]}))))
+  (testing "error: field required"
+    (is (= [{:type :t.form.validation/required :field-id 2}
+            {:type :t.form.validation/required :field-id 3}]
+           (validate
+            {:items [{:id 1
+                      :localizations {:en {:title "A"}}
+                      :optional true
+                      :value nil}
+                     {:id 2
+                      :localizations {:en {:title "B"}}
+                      :optional false
+                      :value ""}
+                     {:id 3
+                      :localizations {:en {:title "C"}}
+                      :optional false
+                      :value nil}]})))))
