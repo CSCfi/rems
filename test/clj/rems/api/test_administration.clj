fb12fe54fb0a9d44d75d9bbadbee0974fd405095
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-04-02 13:19:44 +0300

    rems.api.test-administration: extract api-call helper

--- test/clj/rems/api/test_administration.clj
+++ test/clj/rems/api/test_administration.clj
@@ -2,103 +2,66 @@
   "Tests for invariants across administration APIs."
   (:require [clojure.test :refer :all]
             [rems.handler :refer [handler]]
-            [rems.api.testing :refer :all]
-            [ring.mock.request :refer :all]))
+            [rems.api.testing :refer :all]))
 
 (use-fixtures
   :once
   api-fixture)
 
-(deftest test-archiving
+(def api-key "42")
+(def user-id "owner")
+
+
+(deftest test-archiving-disabling
   (let [api-key "42"
         user-id "owner"
-        resource-id (-> (request :post "/api/resources/create")
-                        (authenticate api-key user-id)
-                        (json-body {:resid "test" :organization "abc" :licenses []})
-                        handler
-                        read-ok-body
-                        :id)
-        form-id (-> (request :post "/api/forms/create")
-                    (authenticate api-key user-id)
-                    (json-body {:organization "abc" :title "form update test"
-                                :items []})
-                    handler
-                    read-ok-body
-                    :id)
-        workflow-id (-> (request :post "/api/workflows/create")
-                        (json-body {:organization "abc" :title "dynamic workflow"
-                                    :type :dynamic :handlers [user-id]})
-                   (authenticate api-key user-id)
-                   handler
-                   read-ok-body
-                   :id)
-        catalogue-id (-> (request :post "/api/catalogue-items/create")
-                         (authenticate api-key "owner")
-                         (json-body {:title "test-item-title"
+        resource-id (:id (api-call :post "/api/resources/create" {:resid "test" :organization "abc" :licenses []}
+                                   api-key user-id))
+        form-id (:id (api-call :post "/api/forms/create" {:organization "abc" :title "form update test" :items []}
+                               api-key user-id))
+        workflow-id (:id (api-call :post "/api/workflows/create"
+                                   {:organization "abc" :title "dynamic workflow"
+                                    :type :dynamic :handlers [user-id]}
+                                   api-key user-id))
+        catalogue-id (:id (api-call :post "/api/catalogue-items/create"
+                                    {:title "test-item-title"
                                      :form form-id
                                      :resid resource-id
-                                     :wfid workflow-id})
-                         handler
-                         read-ok-body
-                         :id)]
+                                     :wfid workflow-id}
+                                    api-key user-id))]
     (is resource-id)
     (is form-id)
     (is workflow-id)
     (is catalogue-id)
 
     (testing "can't archive resource if it is part of an active catalogue item"
-      (let [resp (-> (request :put "/api/resources/update")
-                     (authenticate api-key user-id)
-                     (json-body {:id resource-id :enabled true :archived true})
-                     handler
-                     read-ok-body)]
+      (let [resp (api-call :put "/api/resources/update" {:id resource-id :enabled true :archived true}
+                           api-key user-id)]
         (is (false? (:success resp)))
         (is (= [{:type "t.administration.errors/resource-in-use" :catalogue-items [catalogue-id]}]
                (:errors resp)))))
     (testing "can't archive a form that's in use"
-      (let [resp (-> (request :put "/api/forms/update")
-                     (authenticate api-key user-id)
-                     (json-body {:id form-id :enabled true :archived true})
-                     handler
-                     read-ok-body)]
+      (let [resp (api-call :put "/api/forms/update" {:id form-id :enabled true :archived true}
+                           api-key user-id)]
         (is (false? (:success resp)))
         (is (= [{:type "t.administration.errors/form-in-use" :catalogue-items [catalogue-id]}]
                (:errors resp)))))
     (testing "can't archive a workflow that's in use"
-      (let [resp (-> (request :put "/api/workflows/update")
-                     (authenticate api-key user-id)
-                     (json-body {:id workflow-id :enabled true :archived true})
-                     handler
-                     read-ok-body)]
+      (let [resp (api-call :put "/api/workflows/update" {:id workflow-id :enabled true :archived true}
+                           api-key user-id)]
         (is (false? (:success resp)))
         (is (= [{:type "t.administration.errors/workflow-in-use" :catalogue-items [catalogue-id]}]
                (:errors resp)))))
 
     (testing "can archive a catalogue item"
-      (is (-> (request :put "/api/catalogue-items/update")
-              (authenticate api-key user-id)
-              (json-body {:id catalogue-id :enabled true :archived true})
-              handler
-              read-ok-body
-              :success)))
+      (is (:success (api-call :put "/api/catalogue-items/update" {:id catalogue-id :enabled true :archived true}
+                              api-key user-id))))
     (testing "can archive a resource that's not in use"
-      (is (-> (request :put "/api/resources/update")
-              (authenticate api-key user-id)
-              (json-body {:id resource-id :enabled true :archived true})
-              handler
-              read-ok-body
-              :success)))
+      (is (:success (api-call :put "/api/resources/update" {:id resource-id :enabled true :archived true}
+                              api-key user-id))))
     (testing "can archive a form that's not in use"
-      (is (-> (request :put "/api/forms/update")
-              (authenticate api-key user-id)
-              (json-body {:id form-id :enabled true :archived true})
-              handler
-              read-ok-body
-              :success)))
+      (is (:success (api-call :put "/api/forms/update" {:id form-id :enabled true :archived true}
+                              api-key user-id))))
     (testing "can archive a workflow that's not in use"
-      (is (-> (request :put "/api/workflows/update")
-              (authenticate api-key user-id)
-              (json-body {:id workflow-id :enabled true :archived true})
-              handler
-              read-ok-body
-              :success)))))
+      (is (:success (api-call :put "/api/workflows/update" {:id workflow-id :enabled true :archived true}
+                              api-key user-id))))))
