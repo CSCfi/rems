5dede1bc4bafe07aba7e972414fbe851c13f86b3
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-04-05 13:02:31 +0300

    rems.actions.return-action: use new command endpoints

--- src/cljs/rems/actions/return_action.cljs
+++ src/cljs/rems/actions/return_action.cljs
@@ -17,9 +17,8 @@
  ::send-return
  (fn [_ [_ {:keys [application-id comment on-finished]}]]
    (status-modal/common-pending-handler! (text :t.actions/return))
-   (post! "/api/applications/command"
+   (post! "/api/applications/command/return"
           {:params {:application-id application-id
-                    :type :application.command/return
                     :comment comment}
            :handler (partial status-modal/common-success-handler! on-finished)
            :error-handler status-modal/common-error-handler!})
