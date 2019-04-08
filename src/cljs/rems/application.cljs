a04cf55a5ad9a109bc25a698c9e9e9b1365fca8b
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-04-08 09:52:10 +0300

    rems.application: show disabled warning for expired items

--- src/cljs/rems/application.cljs
+++ src/cljs/rems/application.cljs
@@ -44,6 +44,7 @@
   (when (in-processing? application)
     (when-some [resources (->> (:application/resources application)
                                (filter #(or (not (:catalogue-item/enabled %))
+                                            (not (:catalogue-item/active %))
                                             (:catalogue-item/archived %)))
                                seq)]
       [:div.alert.alert-danger
