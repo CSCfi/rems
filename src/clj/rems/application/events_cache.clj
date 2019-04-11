4908ea53b3e690aa5e4743785763ebbb477065e0
Esko Luontola <esko@luontola.fi>
2019-04-11 18:34:45 +0300

    documentation

--- src/clj/rems/application/events_cache.clj
+++ src/clj/rems/application/events_cache.clj
@@ -5,12 +5,16 @@
   (atom {:last-processed-event-id 0
          :state nil}))
 
-(defn refresh! [cache f]
+(defn refresh!
+  "Refreshes the cache with new events and returns the latest state.
+   `update-fn` should be a function which takes as parameters the previously
+   cached state and a list of new events, and returns the updated state."
+  [cache update-fn]
   (let [cached @cache
         new-events (applications/get-dynamic-application-events-since (:last-processed-event-id cached))]
     (if (empty? new-events)
       (:state cached)
-      (let [updated {:state (f (:state cached) new-events)
+      (let [updated {:state (update-fn (:state cached) new-events)
                      :last-processed-event-id (:event/id (last new-events))}]
         ;; with concurrent requests, only one of them will update the cache
         (compare-and-set! cache cached updated)
