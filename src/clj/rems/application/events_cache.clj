138eb6d493da7fe04047d36f1212b95e65a1a181
Esko Luontola <esko@luontola.fi>
2019-04-11 19:41:55 +0300

    guard against a cache which is not started

--- src/clj/rems/application/events_cache.clj
+++ src/clj/rems/application/events_cache.clj
@@ -1,21 +1,27 @@
 (ns rems.application.events-cache
-  (:require [rems.db.applications :as applications]))
+  (:require [rems.db.applications :as applications])
+  (:import [clojure.lang Atom]))
+
+(def ^:private empty-cache
+  {:last-processed-event-id 0
+   :state nil})
 
 (defn new []
-  (atom {:last-processed-event-id 0
-         :state nil}))
+  (atom empty-cache))
 
 (defn refresh!
   "Refreshes the cache with new events and returns the latest state.
    `update-fn` should be a function which takes as parameters the previously
    cached state and a list of new events, and returns the updated state."
   [cache update-fn]
-  (let [cached @cache
+  (let [cache-enabled? (instance? Atom cache)
+        cached (if cache-enabled? @cache empty-cache)
         new-events (applications/get-dynamic-application-events-since (:last-processed-event-id cached))]
     (if (empty? new-events)
       (:state cached)
       (let [updated {:state (update-fn (:state cached) new-events)
                      :last-processed-event-id (:event/id (last new-events))}]
         ;; with concurrent requests, only one of them will update the cache
-        (compare-and-set! cache cached updated)
+        (when cache-enabled?
+          (compare-and-set! cache cached updated))
         (:state updated)))))
--- test/clj/rems/application/test_events_cache.clj
+++ test/clj/rems/application/test_events_cache.clj
@@ -1,6 +1,7 @@
 (ns ^:integration rems.application.test-events-cache
   (:require [clj-time.core :as time]
             [clojure.test :refer :all]
+            [mount.core :as mount]
             [rems.application.events-cache :as events-cache]
             [rems.db.applications :as applications]
             [rems.db.core :as db]
@@ -62,4 +63,14 @@
                                         (is (= [expected-event]
                                                (->> events
                                                     (map #(dissoc % :event/id)))))
-                                        "second result"))))))))
+                                        "second result"))))))
+
+    (testing "cache disabled"
+      (let [update-fn (fn [state events]
+                        (is (nil? state))
+                        (is (= 2 (count events)))
+                        "non-cached result")]
+        (is (= "non-cached result"
+               (events-cache/refresh! nil update-fn)
+               ;; in case the cache uses mount and somebody forgets to start it
+               (events-cache/refresh! (mount/->NotStartedState "foo") update-fn)))))))
