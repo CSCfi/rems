da6c4b51861bf7406bed0362abd5a62318c785b3
Esko Luontola <esko@luontola.fi>
2019-04-11 14:57:56 +0300

    cache dynamic roles projection: request time 800ms -> 20ms for all routes

--- src/clj/rems/db/dynamic_roles.clj
+++ src/clj/rems/db/dynamic_roles.clj
@@ -1,21 +1,26 @@
 (ns rems.db.dynamic-roles
   (:require [clojure.set :as set]
             [clojure.test :refer [deftest is testing]]
+            [clojure.tools.logging :as log]
+            [mount.core :as mount]
             [rems.application.model :as model]
             [rems.db.applications :as applications]
             [rems.permissions :as permissions]))
 
-;; TODO: make private after removing the hack which uses this
-(defn permissions-of-all-applications [applications event]
+(defn- permissions-of-all-applications [applications event]
   (if-let [app-id (:application/id event)] ; old style events don't have :application/id
     (update applications app-id model/calculate-permissions event)
     applications))
 
-(defn- roles-from-all-applications [user events]
-  (->> (reduce permissions-of-all-applications nil events)
-       vals
+(defn- roles-of-user [user applications-by-id]
+  (->> applications-by-id
+       (vals)
        (map #(permissions/user-roles % user))
        (apply set/union)))
+
+(defn- roles-from-all-applications [user events]
+  (->> (reduce permissions-of-all-applications nil events)
+       (roles-of-user user)))
 
 (deftest test-roles-from-all-applications
   (let [events [{:event/type :application.event/created
@@ -30,7 +35,18 @@
     (is (= #{:applicant :handler}
            (roles-from-all-applications "applicant-and-handler" events)))))
 
+(mount/defstate roles-cache
+  :start (atom {:last-processed-event-id 0
+                :applications nil}))
+
 (defn get-roles [user]
-  ;; TODO: caching?
-  (let [events (applications/get-dynamic-application-events-since 0)]
-    (roles-from-all-applications user events)))
+  (let [cache @roles-cache
+        events (applications/get-dynamic-application-events-since (:last-processed-event-id cache))
+        applications (reduce permissions-of-all-applications (:applications cache) events)]
+    (when-let [event-id (:event/id (last events))]
+      (when (compare-and-set! roles-cache
+                              cache
+                              {:last-processed-event-id event-id
+                               :applications applications})
+        (log/info "Updated roles-cache from" (:last-processed-event-id cache) "to" event-id)))
+    (roles-of-user user applications)))
