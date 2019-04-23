aa985eef5a41cbf86e77c53932e14086fb54b40c
Esko Luontola <esko@luontola.fi>
2019-04-23 16:42:19 +0300

    refactor

--- test/clj/rems/db/test_transactions.clj
+++ test/clj/rems/db/test_transactions.clj
@@ -10,8 +10,9 @@
             [rems.db.testing :refer [test-db-fixture rollback-db-fixture test-data-fixture]]
             [rems.db.users :as users]
             [rems.db.workflow :as workflow])
-  (:import [java.util UUID]
-           [java.util.concurrent Executors Future TimeUnit]
+  (:import [java.sql SQLException]
+           [java.util UUID]
+           [java.util.concurrent Executors Future TimeUnit ExecutorService]
            [org.postgresql.util PSQLException]))
 
 (use-fixtures
@@ -52,88 +53,91 @@
                                            "The transaction might succeed if retried")
     :else (transaction-conflict? (.getCause e))))
 
-(defn- retry-transaction [f]
-  (while (and (not (.isInterrupted (Thread/currentThread)))
-              (not (try
-                     (f)
-                     true
-                     (catch Exception e
-                       (if (transaction-conflict? e)
-                         false
-                         (throw e))))))))
+(defn- sample-until-interrupted [f]
+  (loop [results []]
+    (if (.isInterrupted (Thread/currentThread))
+      results
+      (recur (try
+               (conj results (f))
+               (catch InterruptedException _
+                 (.interrupt (Thread/currentThread))
+                 results)
+               ;; XXX: HikariPool.getConnection wraps InterruptedException into SQLException
+               (catch SQLException e
+                 (if (instance? InterruptedException (.getCause e))
+                   (do (.interrupt (Thread/currentThread))
+                       results)
+                   (throw e))))))))
+
+(defn- submit-all [^ExecutorService thread-pool tasks]
+  (doall (for [task tasks]
+           (.submit thread-pool ^Callable task))))
 
 (deftest test-event-publishing-consistency
-  (let [written-events-count 50
-        concurrent-writers-count 10
-        timeout-seconds 30
+  (let [test-duration-millis 2000
+        applications-count 5
+        writers-per-application 5
+        concurrent-readers 5
         user-id (create-dummy-user)
-        app-ids (vec (for [_ (range concurrent-writers-count)]
+        app-ids (vec (for [_ (range applications-count)]
                        (create-dummy-application user-id)))
-        writers-pool (Executors/newFixedThreadPool concurrent-writers-count)
-        readers-pool (Executors/newCachedThreadPool)
-        write-event (fn []
-                      (retry-transaction
-                       (fn []
-                         ;; Note: currently these tests pass only with serializable isolation
-                         (conman/with-transaction [db/*db* {:isolation :serializable}]
-                           (applications/command!
-                            {:type :application.command/save-draft
-                             :time (time/now)
-                             :actor user-id
-                             :application-id (rand-nth app-ids)
-                             :field-values []})))))
+        write-event (fn [app-id]
+                      (try
+                        ;; Note: currently these tests pass only with serializable isolation
+                        (conman/with-transaction [db/*db* {:isolation :serializable}]
+                          (applications/command!
+                           {:type :application.command/save-draft
+                            :time (time/now)
+                            :actor user-id
+                            :application-id app-id
+                            :field-values []}))
+                        (catch Exception e
+                          (if (transaction-conflict? e)
+                            ::transaction-conflict
+                            (throw e)))))
         read-app-events (fn [app-id]
                           (conman/with-transaction [db/*db* {:isolation :read-committed
                                                              :read-only? true}]
-                            (applications/get-application-events app-id)))
+                            {::app-id app-id
+                             ::events (applications/get-application-events app-id)}))
         read-all-events (fn []
                           (conman/with-transaction [db/*db* {:isolation :read-committed
                                                              :read-only? true}]
-                            (applications/get-all-events-since 0)))
-        read-app-events-repeatedly (fn [app-id]
-                                     (let [results (atom [])]
-                                       (while (not (.isTerminated writers-pool))
-                                         (swap! results conj {::app-id app-id
-                                                              ::events (read-app-events app-id)}))
-                                       @results))
-        read-all-events-repeatedly (fn []
-                                     (let [results (atom [])]
-                                       (while (not (.isTerminated writers-pool))
-                                         (swap! results conj {::events (read-all-events)}))
-                                       @results))
-        app-readers (doall (for [app-id app-ids]
-                             (.submit readers-pool ^Callable (fn [] (read-app-events-repeatedly app-id)))))
-        all-readers (doall (for [_ (range concurrent-writers-count)]
-                             (.submit readers-pool ^Callable read-all-events-repeatedly)))
-        writers (doall (for [_ (range written-events-count)]
-                         (.submit writers-pool ^Callable write-event)))]
-    (.shutdown writers-pool)
-    (.awaitTermination writers-pool timeout-seconds TimeUnit/SECONDS)
-    (.shutdown readers-pool)
-    (.awaitTermination readers-pool timeout-seconds TimeUnit/SECONDS)
+                            {::events (applications/get-all-events-since 0)}))
+        thread-pool (Executors/newCachedThreadPool)
+        app-events-readers (submit-all thread-pool (for [app-id app-ids]
+                                                     (fn [] (sample-until-interrupted
+                                                             (fn [] (read-app-events app-id))))))
+        all-events-readers (submit-all thread-pool (for [_ (range concurrent-readers)]
+                                                     (fn [] (sample-until-interrupted
+                                                             (fn [] (read-all-events))))))
+        writers (submit-all thread-pool (for [app-id app-ids
+                                              _ (range writers-per-application)]
+                                          (fn [] (sample-until-interrupted
+                                                  (fn [] (write-event app-id))))))]
+    (Thread/sleep test-duration-millis)
+    (doto thread-pool
+      (.shutdownNow)
+      (.awaitTermination 30 TimeUnit/SECONDS))
 
-    (prn 'app-ids app-ids)
-    (let [writer-results (map #(.get ^Future %) writers)
-          app-reader-results (flatten (map #(.get ^Future %) app-readers))
-          all-reader-results (flatten (map #(.get ^Future %) all-readers))
-          final-events (read-all-events)
-          final-events-by-app-id (into {} (for [app-id app-ids]
-                                            [app-id (read-app-events app-id)]))]
-      (prn 'writer-results (count writer-results))
-      (prn 'app-reader-results (count app-reader-results))
-      (prn 'all-reader-results (count all-reader-results))
+    (let [writer-results (->> (flatten (map #(.get ^Future %) writers))
+                              (remove #{::transaction-conflict}))
+          app-events-reader-results (flatten (map #(.get ^Future %) app-events-readers))
+          all-events-reader-results (flatten (map #(.get ^Future %) all-events-readers))
+          final-events (applications/get-all-events-since 0)
+          final-events-by-app-id (group-by :application/id final-events)]
 
       (testing "all commands succeeded"
-        (is (= written-events-count (count writer-results)))
-        (is (every? nil? writer-results)))
+        (is (not (empty? writer-results)))
+        (is (every? nil? writer-results))) ; successful commands return nil
 
       (testing "all events were written"
-        (is (= (+ (count app-ids) ; number of created events
-                  written-events-count)
-               (count (applications/get-all-events-since 0)))))
+        (is (= (+ (count app-ids) ; one application created event per application
+                  (count writer-results))
+               (count final-events))))
 
       (testing "event IDs are observed in monotonically increasing order, within one application"
-        (doseq [observed app-reader-results]
+        (doseq [observed app-events-reader-results]
           (let [final-events (get final-events-by-app-id (::app-id observed))]
             (is (= (->> final-events
                         (take (count (::events observed)))
@@ -142,15 +146,15 @@
                         (map :event/id)))))))
 
       (testing "event IDs are observed in monotonically increasing order, globally"
-        (doseq [observed all-reader-results]
+        (doseq [observed all-events-reader-results]
           (is (= (->> final-events
                       (take (count (::events observed)))
                       (map :event/id))
                  (->> (::events observed)
-                      (map :event/id)))))
+                      (map :event/id))))))
 
-        ;; TODO?
-        #_(testing "there are not gaps in event IDs"
-            (is (every? (fn [[a b]] (= (inc a) b))
-                        (->> (map :event/id final-events)
-                             (partition 2 1)))))))))
+      ;; TODO?
+      #_(testing "there are not gaps in event IDs"
+          (is (every? (fn [[a b]] (= (inc a) b))
+                      (->> (map :event/id final-events)
+                           (partition 2 1))))))))
