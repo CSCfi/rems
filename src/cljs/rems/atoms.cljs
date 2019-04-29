e5fed5625fefa96cb94062951470b1d9abe397de
Esko Luontola <esko@luontola.fi>
2019-04-29 16:54:55 +0300

    refactor: extract rems.atoms/more-options

--- src/cljs/rems/atoms.cljs
+++ src/cljs/rems/atoms.cljs
@@ -7,6 +7,9 @@
 (defn external-link []
   [:i {:class "fa fa-external-link-alt"}])
 
+(defn more-options []
+  [:i {:class "fa fa-ellipsis-h"}])
+
 (defn link-to [opts uri title]
   [:a (merge opts {:href uri}) title])
 
