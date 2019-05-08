f8cf84ff44d7995028874c31d41489bd61e4b279
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-05-08 12:52:35 +0300

    rems.form-validation: remove unused code

--- src/clj/rems/form_validation.clj
+++ src/clj/rems/form_validation.clj
@@ -16,26 +16,3 @@
        (map validate-field)
        (remove nil?)
        (seq)))
-
-(defn- validate-license
-  [license]
-  (when-not (:approved license)
-    {:license-id (:id license)
-     :type :t.form.validation/required}))
-
-(defn validate-licenses [licenses]
-  (->> (sort-by :id licenses)
-       (map validate-license)
-       (remove nil?)
-       (seq)))
-
-(defn validate
-  "Validates a filled in form from (see rems.db.form/get-form).
-
-   Returns either :valid or a sequence of validation errors."
-  [form]
-  (let [messages (vec (concat (validate-fields (:items form))
-                              (validate-licenses (:licenses form))))]
-    (if (empty? messages)
-      :valid
-      messages)))
--- test/clj/rems/test_form_validation.clj
+++ test/clj/rems/test_form_validation.clj
@@ -1,59 +1,45 @@
 (ns rems.test-form-validation
   (:require [clojure.test :refer :all]
-            [rems.form-validation :refer [validate]]))
+            [rems.form-validation :refer [validate-fields]]))
 
-(deftest test-validate
-  (testing "all fields filled & all licenses accepted"
-    (is (= :valid (validate
-                   {:items [{:title "A"
-                             :optional false
-                             :value "a"}]
-                    :licenses [{:title "LGPL"
-                                :approved true}]}))))
+(deftest test-validate-fields
+  (testing "all fields filled"
+    (is (nil? (validate-fields [{:title "A"
+                                 :optional false
+                                 :value "a"}]))))
+
 
   (testing "optional fields"
-    (is (= :valid (validate
-                   {:items [{:title "A"
-                             :optional true
-                             :value nil}
-                            {:title "B"
-                             :optional true
-                             :value ""}]}))))
-
-  (testing "error: license required"
-    (is (= [{:type :t.form.validation/required :license-id 123}]
-           (validate
-            {:items [{:title "A"
-                      :optional false
-                      :value "a"}]
-             :licenses [{:id 123
-                         :title "LGPL"}]}))))
+    (is (nil? (validate-fields [{:title "A"
+                                 :optional true
+                                 :value nil}
+                                {:title "B"
+                                 :optional true
+                                 :value ""}]))))
 
   (testing "error: field required"
     (is (= [{:type :t.form.validation/required :field-id 2}
             {:type :t.form.validation/required :field-id 3}]
-           (validate
-            {:items [{:id 1
-                      :localizations {:en {:title "A"}}
-                      :optional true
-                      :value nil}
-                     {:id 2
-                      :localizations {:en {:title "B"}}
-                      :optional false
-                      :value ""}
-                     {:id 3
-                      :localizations {:en {:title "C"}}
-                      :optional false
-                      :value nil}]}))))
+           (validate-fields [{:id 1
+                              :localizations {:en {:title "A"}}
+                              :optional true
+                              :value nil}
+                             {:id 2
+                              :localizations {:en {:title "B"}}
+                              :optional false
+                              :value ""}
+                             {:id 3
+                              :localizations {:en {:title "C"}}
+                              :optional false
+                              :value nil}]))))
 
   (testing "error: field input too long"
     (is (= [{:type :t.form.validation/toolong :field-id 2}]
-           (validate
-            {:items [{:id 1
-                      :localizations {:en {:title "A"}}
-                      :maxlength 5
-                      :value "abcde"}
-                     {:id 2
-                      :localizations {:en {:title "B"}}
-                      :maxlength 5
-                      :value "abcdef"}]})))))
+           (validate-fields [{:id 1
+                              :localizations {:en {:title "A"}}
+                              :maxlength 5
+                              :value "abcde"}
+                             {:id 2
+                              :localizations {:en {:title "B"}}
+                              :maxlength 5
+                              :value "abcdef"}])))))
