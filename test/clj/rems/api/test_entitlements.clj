7912fcdfcf762c6fd536250f10e545e5730d11b1
Leena Wooller <leena.wooller@csc.fi>
2019-05-15 11:24:28 +0300

    Add more test cases to entitlements api.

--- test/clj/rems/api/test_entitlements.clj
+++ test/clj/rems/api/test_entitlements.clj
@@ -27,6 +27,54 @@
                      read-body)]
         (is (= 2 (count data)))))
 
+    (testing "all as an developer"
+      (let [data (-> (request :get "/api/entitlements")
+                     (authenticate api-key "developer")
+                     handler
+                     read-body)]
+        (is (= 2 (count data)))
+        ;; sanity check the data
+        (is (= {:resource "urn:nbn:fi:lb-201403262"
+                :application-id 12
+                :mail "alice@example.com"}
+               (-> data first (dissoc :start))))
+        (is (= {:resource "urn:nbn:fi:lb-201403262"
+                :application-id 19
+                :mail "developer@example.com"}
+               (-> data second (dissoc :start))))))
+
+    (testing "all as an owner"
+      (let [data (-> (request :get "/api/entitlements")
+                     (authenticate api-key "owner")
+                     handler
+                     read-body)]
+        (is (= 2 (count data)))
+        ;; sanity check the data
+        (is (= {:resource "urn:nbn:fi:lb-201403262"
+                :application-id 12
+                :mail "alice@example.com"}
+               (-> data first (dissoc :start))))
+        (is (= {:resource "urn:nbn:fi:lb-201403262"
+                :application-id 19
+                :mail "developer@example.com"}
+               (-> data second (dissoc :start))))))
+
+    (testing "all as a reporter"
+      (let [data (-> (request :get "/api/entitlements")
+                     (authenticate api-key "reporter")
+                     handler
+                     read-body)]
+        (is (= 2 (count data)))
+        ;; sanity check the data
+        (is (= {:resource "urn:nbn:fi:lb-201403262"
+                :application-id 12
+                :mail "alice@example.com"}
+               (-> data first (dissoc :start))))
+        (is (= {:resource "urn:nbn:fi:lb-201403262"
+                :application-id 19
+                :mail "developer@example.com"}
+               (-> data second (dissoc :start))))))
+
     (testing "just for alice as a developer"
       (let [data (-> (request :get "/api/entitlements?user=alice")
                      (authenticate api-key "developer")
