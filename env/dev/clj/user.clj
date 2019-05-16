712407c108de0bf2b0441f4770b7d931656c9b1b
Tomi Hukkalainen <tomi.hukkalainen@nitor.com>
2019-05-16 11:46:49 +0300

    Remove test helpers from user ns
    Couldn't get kaocha working with gen-class, aot and repl/refresh

--- env/dev/clj/user.clj
+++ env/dev/clj/user.clj
@@ -1,21 +1,12 @@
 (ns user
   (:require [clojure.pprint :refer [pprint]]
-            [clojure.tools.namespace.repl :as repl]
-            [kaocha.repl :as k]))
+            [clojure.tools.namespace.repl :as repl]))
 
 (defn reload []
   ((resolve 'rems.standalone/stop-app))
   (repl/refresh-all)
   ((resolve 'rems.standalone/start-app))
   ((resolve 'rems.validate/validate)))
-
-(defn run-tests [& namespaces]
-  (repl/refresh)
-  (apply k/run namespaces))
-
-(defn run-all-tests []
-  (repl/refresh)
-  (k/run))
 
 (defn pptransit []
   ;; XXX: The user namespace is loaded before AOT happens, so our custom exceptions won't exist yet then,
