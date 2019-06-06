d2ab59e024199bc7c50a6ceeffc949a69162b05d
Markku Rontu <markku.rontu@iki.fi>
2019-06-06 21:39:25 +0300

    feat: improved login style and text

--- src/cljs/rems/auth/shibboleth.cljs
+++ src/cljs/rems/auth/shibboleth.cljs
@@ -8,7 +8,7 @@
   [:div
    [document-title (text :t.login/title)]
    [:p (text :t.login/text)]
-   [:div.col-md6
+   [:div
     [atoms/link-to nil
                    (nav/url-dest "/Shibboleth.sso/Login")
                    [atoms/image {:class "login-btn" :alt "Haka"} "/img/haka-logo.jpg"]]]
