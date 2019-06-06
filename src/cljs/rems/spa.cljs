9cc689264c08840b7c18f0a8e5e830a7ab68222e
Markku Rontu <markku.rontu@iki.fi>
2019-06-06 18:48:18 +0300

    style: center login page content

--- src/cljs/rems/spa.cljs
+++ src/cljs/rems/spa.cljs
@@ -193,10 +193,10 @@
       [catalogue-page])
     [:div
      [:div.row.justify-content-center
-      [:div.col-md-6
+      [:div.col-md-6.row.justify-content-center
        (text :t.login/intro)]]
      [:div.row.justify-content-center
-      [:div.col-md-6
+      [:div.col-md-6.row.justify-content-center
        [auth/login-component]]]]))
 
 (defn unauthorized-page []
