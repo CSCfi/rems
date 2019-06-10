68312a708d26f7a82733adb7500428e1f0213927
Esko Luontola <esko@luontola.fi>
2019-06-10 13:33:11 +0300

    refactor: naming

--- src/cljs/rems/focus.cljs
+++ src/cljs/rems/focus.cljs
@@ -1,15 +1,15 @@
 (ns rems.focus)
 
-(def ^:private element-atom-to-focus (atom nil))
+(def ^:private element-atom-to-be-focused (atom nil))
 
 (defn- grab-focus []
-  (when-let [element-atom @element-atom-to-focus]
+  (when-let [element-atom @element-atom-to-be-focused]
     (when-let [element @element-atom]
       (.focus element)
-      (reset! element-atom-to-focus nil))))
+      (reset! element-atom-to-be-focused nil))))
 
 (defn set-focus [element-atom]
-  (reset! element-atom-to-focus element-atom)
+  (reset! element-atom-to-be-focused element-atom)
   (grab-focus))
 
 (defn ref-changed [element-atom element]
