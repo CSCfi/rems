4d45c886bb6f643160686d38931baf088cf5152b
Esko Luontola <esko@luontola.fi>
2019-08-02 17:29:04 +0300

    fix duplicate element IDs

--- src/cljs/rems/actions/remark.cljs
+++ src/cljs/rems/actions/remark.cljs
@@ -41,7 +41,7 @@
   [{:keys [comment on-set-comment public on-set-public on-send]}]
   [action-form-view action-form-id
    (text :t.actions/remark)
-   [[button-wrapper {:id "comment"
+   [[button-wrapper {:id action-form-id
                      :text (text :t.actions/remark)
                      :class "btn-primary"
                      :on-click on-send}]]
@@ -50,14 +50,15 @@
                      :label (text :t.form/add-remark)
                      :comment comment
                      :on-comment on-set-comment}]
-    [:div.form-check
-     [:input.form-check-input {:type "checkbox"
-                               :id "public"
-                               :name "public"
-                               :value public
-                               :on-change #(on-set-public (.. % -target -checked))}]
-     [:label.form-check-label {:for "public"}
-      (text :t.actions/remark-public)]]]])
+    (let [id (str "public-" action-form-id)]
+      [:div.form-check
+       [:input.form-check-input {:type "checkbox"
+                                 :id id
+                                 :name id
+                                 :value public
+                                 :on-change #(on-set-public (.. % -target -checked))}]
+       [:label.form-check-label {:for id}
+        (text :t.actions/remark-public)]])]])
 
 (defn remark-form [application-id on-finished]
   [remark-view {:comment @(rf/subscribe [::comment])
