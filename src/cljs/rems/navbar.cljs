300360cda808524ec07717089aa1e2d2ca2f10f5
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@csc.fi>
2019-08-05 13:45:03 +0300

    Improve alignment of navbar with main-content
    - Add id for container class div element so max-width can be set for
      that instead of its child elements where it does not do anything,
      making it analogous to div element for main-content.
    
    - Change class from container to container-fluid, the difference being
      that container-fluid is supposed to change size continuously when
      the browser window is stretched, whereas container changes size only
      at certain points. It is now similar to main-content element.
    
    - After these changes, right edge of navbar is now aligned with right
      edge of main-content.
    
    - Left edge of navbar is almost aligned with left edge of main-content,
      but the link texts in navbar have a separate padding-left and padding-
      right (coming from .nav-link CSS in bootstrap), causing there to be
      a remaining slight offset.

--- src/cljs/rems/navbar.cljs
+++ src/cljs/rems/navbar.cljs
@@ -77,7 +77,7 @@
     [:div.fixed-top
      [skip-navigation]
      [:div.navbar-top-bar [:div.navbar-top-left] [:div.navbar-top-right]]
-     [:div.container
+     [:div.container-fluid {:id "navbar"}
       [navbar-normal page-id identity]
       [navbar-small page-id identity]]
      [:div.navbar-bottom-bar]]))
