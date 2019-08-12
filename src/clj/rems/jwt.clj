02cf92529d0b05200e3876656e9c51c6a8047374
Esko Luontola <esko@luontola.fi>
2019-08-12 15:29:53 +0300

    JWT validator using Auth0 libraries
    Based on re-licensed code from https://github.com/luontola/territory-bro

---
+++ src/clj/rems/jwt.clj
@@ -0,0 +1,47 @@
+(ns rems.jwt
+  (:require [clojure.data.json :as json]
+            [mount.core :as mount]
+            [rems.config :refer [env]]
+            [rems.util :refer [getx]])
+  (:import [com.auth0.jwk JwkProviderBuilder JwkProvider]
+           [com.auth0.jwt JWT JWTVerifier$BaseVerification]
+           [com.auth0.jwt.algorithms Algorithm]
+           [com.auth0.jwt.interfaces Clock]
+           [java.nio.charset StandardCharsets]
+           [java.time Instant]
+           [java.util Base64 Date]))
+
+(mount/defstate ^:dynamic ^JwkProvider jwk-provider
+  :start (-> (JwkProviderBuilder. ^String (getx env :auth0-domain))
+             (.build)))
+
+(defn- fetch-public-key [^String jwt]
+  (let [key-id (.getKeyId (JWT/decode jwt))]
+    (.getPublicKey (.get jwk-provider key-id))))
+
+(defn- decode-base64url [^String base64-str]
+  (-> (Base64/getUrlDecoder)
+      (.decode base64-str)
+      (String. StandardCharsets/UTF_8)))
+
+(defn validate [^String jwt env]
+  (let [public-key (fetch-public-key jwt)
+        algorithm (Algorithm/RSA256 public-key nil)
+        clock (reify Clock
+                (getToday [_]
+                  (Date/from ((getx env :now)))))
+        verifier (-> (JWT/require algorithm)
+                     (.withIssuer (into-array String [(getx env :jwt-issuer)]))
+                     (.withAudience (into-array String [(getx env :jwt-audience)]))
+                     (->> ^JWTVerifier$BaseVerification (cast JWTVerifier$BaseVerification))
+                     (.build clock))]
+    (-> (.verify verifier jwt)
+        (.getPayload)
+        (decode-base64url)
+        (json/read-str :key-fn keyword))))
+
+(defn expired?
+  ([jwt]
+   (expired? jwt (Instant/now)))
+  ([jwt ^Instant now]
+   (< (:exp jwt) (.getEpochSecond now))))
