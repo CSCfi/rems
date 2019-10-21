c7f4135d503976bd529592dafcaf99a0b95b9934
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-10-21 16:02:04 +0300

    feat: add email column to blacklist

--- src/cljs/rems/administration/blacklist.cljs
+++ src/cljs/rems/administration/blacklist.cljs
@@ -30,7 +30,8 @@
    (for [{:keys [resource user]} rows]
      {:key (str "blacklist-" resource (:userid user))
       :resource {:value resource}
-      :user {:value (rems.application-util/get-member-name user)}})))
+      :user {:value (rems.application-util/get-member-name user)}
+      :email {:value (:email user)}})))
 
 (rf/reg-event-db
  ::fetch-result
@@ -47,7 +48,9 @@
                     :columns [{:key :resource
                                :title (text :t.administration/resource)}
                               {:key :user
-                               :title (text :t.administration/user)}]
+                               :title (text :t.administration/user)}
+                              {:key :email
+                               :title (text :t.applicant-info/email)}]
                     :rows [::blacklist]
                     :default-sort-column :resource}]
     [:div.mt-3
