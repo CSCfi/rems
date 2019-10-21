2667b86d09dfb8f8cee05c5a7acc7f4080a97ed2
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-10-21 16:00:10 +0300

    feat: remove blacklist filtering
    let's just rely on table search for now

--- src/cljs/rems/administration/blacklist.cljs
+++ src/cljs/rems/administration/blacklist.cljs
@@ -3,7 +3,6 @@
             [rems.administration.administration :refer [administration-navigator-container]]
             [rems.application-util]
             [rems.atoms :as atoms]
-            [rems.dropdown :as dropdown]
             [rems.flash-message :as flash-message]
             [rems.spinner :as spinner]
             [rems.table :as table]
@@ -14,7 +13,6 @@
  ::enter-page
  (fn [{:keys [db]}]
    {:dispatch-n [[::fetch {}]
-                 [::fetch-resources]
                  [:rems.table/reset]]}))
 
 (rf/reg-event-fx
@@ -44,32 +42,6 @@
 (rf/reg-sub ::blacklist (fn [db _] (::blacklist db)))
 (rf/reg-sub ::loading? (fn [db _] (::loading? db)))
 
-(rf/reg-event-fx
- ::fetch-resources
- (fn [_ _]
-   (fetch "/api/resources"
-          {:url-params {:disabled true
-                        :archived true}
-           :handler #(rf/dispatch [::fetch-resources-result %])
-           :error-handler (flash-message/default-error-handler :top "Fetch resources")})
-   {}))
-
-(rf/reg-event-db
- ::fetch-resources-result
- (fn [db [_ resources]]
-   (assoc db ::resources resources)))
-
-(rf/reg-sub ::resources (fn [db _] (::resources db)))
-
-(rf/reg-event-fx
- ::set-resource-filter
- (fn [{:keys [db]} [_ id]]
-   {:db (assoc db ::resource-filter id)
-    :dispatch [::fetch (if id
-                         {:resource id}
-                         {})]}))
-(rf/reg-sub ::resource-filter (fn [db _] (::resource-filter db)))
-
 (defn- blacklist [rows]
   (let [table-spec {:id ::blacklist
                     :columns [{:key :resource
@@ -82,22 +54,6 @@
      [table/search table-spec]
      [table/table table-spec]]))
 
-(defn- filter-resource-field []
-  (let [resources @(rf/subscribe [::resources])
-        selected-resource-id @(rf/subscribe [::resource-filter])
-        item-selected? #(= (:resid %) selected-resource-id)
-        id "blacklist-filter-resource"]
-    [:div.form-group
-     [:label {:for id} (text :t.administration/filter-by-resource)]
-     [dropdown/dropdown
-      {:id id
-       :clearable? true
-       :items resources
-       :item-key :resid
-       :item-label :resid
-       :item-selected? item-selected?
-       :on-change #(rf/dispatch [::set-resource-filter (:resid %)])}]]))
-
 (defn blacklist-page []
   [:div
    [administration-navigator-container]
@@ -105,6 +61,4 @@
    [flash-message/component :top]
    (if @(rf/subscribe [::loading?])
      [spinner/big]
-     [:<>
-      [filter-resource-field]
-      [blacklist @(rf/subscribe [::blacklist])]])])
+     [blacklist @(rf/subscribe [::blacklist])])])
