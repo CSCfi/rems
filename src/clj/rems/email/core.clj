5d70e699517c744abce65ea1214b81fa141bee36
Esko Luontola <esko@luontola.fi>
2019-11-11 18:47:58 +0200

    extract rems.email.template from rems.email.core

--- src/clj/rems/email/core.clj
+++ src/clj/rems/email/core.clj
@@ -1,223 +1,30 @@
 (ns rems.email.core
   "Sending emails based on application events."
   (:require [clj-time.core :as time]
-            [clojure.string :as str]
             [clojure.test :refer :all]
             [clojure.tools.logging :as log]
             [mount.core :as mount]
             [postal.core :as postal]
-            [rems.application-util :as application-util]
             [rems.application.model]
             [rems.config :refer [env]]
-            [rems.context :as context]
             [rems.db.applications :as applications]
-            [rems.db.user-settings :as user-settings]
             [rems.db.users :as users]
             [rems.email.outbox :as email-outbox]
-            [rems.scheduler :as scheduler]
-            [rems.text :refer [text text-format with-language]]
-            [rems.util :as util])
+            [rems.email.template :as template]
+            [rems.scheduler :as scheduler])
   (:import [javax.mail.internet InternetAddress]
            [org.joda.time Duration Period]))
 
-;;; Mapping events to emails
-
-;; TODO list of resources?
-;; TODO use real name when addressing user?
-
-;; move this to a util namespace if its needed somewhere else
-(defn- link-to-application [application-id]
-  (str (:public-url env) "application/" application-id))
-
-(defn- invitation-link [token]
-  (str (:public-url env) "accept-invitation?token=" token))
-
-(defn- format-application-for-email [application]
-  (str
-   (case (util/getx env :application-id-column)
-     :external-id (:application/external-id application)
-     :id (:application/id application))
-   (when-not (empty? (:application/description application))
-     (str ", \"" (:application/description application) "\""))))
-
-(defn- resources-for-email [application]
-  (->> (:application/resources application)
-       (map #(get-in % [:catalogue-item/title context/*lang*]))
-       (str/join ", ")))
-
-(defn- handlers [application]
-  (get-in application [:application/workflow :workflow.dynamic/handlers]))
-
-(defn- other-handlers [event application]
-  (filter #(not= (:userid %) (:event/actor event)) (handlers application)))
-
-(defmulti ^:private event-to-emails-impl
-  (fn [event _application] (:event/type event)))
-
-(defmethod event-to-emails-impl :default [_event _application]
-  [])
-
-(defn- emails-to-recipients [recipients event application subject-text body-text]
-  (vec
-   (for [recipient recipients]
-     (with-language (:language (user-settings/get-user-settings (:userid recipient)))
-       (fn []
-         {:to-user (:userid recipient)
-          :subject (text-format subject-text
-                                (application-util/get-member-name recipient)
-                                (application-util/get-member-name (:event/actor-attributes event))
-                                (format-application-for-email application)
-                                (application-util/get-applicant-name application)
-                                (resources-for-email application)
-                                (link-to-application (:application/id event)))
-          :body (str
-                 (text-format body-text
-                              (application-util/get-member-name recipient)
-                              (application-util/get-member-name (:event/actor-attributes event))
-                              (format-application-for-email application)
-                              (application-util/get-applicant-name application)
-                              (resources-for-email application)
-                              (link-to-application (:application/id event)))
-                 (text :t.email/regards)
-                 (text :t.email/footer))})))))
-
-(defmethod event-to-emails-impl :application.event/approved [event application]
-  (concat (emails-to-recipients (application-util/applicant-and-members application)
-                                event application
-                                :t.email.application-approved/subject-to-applicant
-                                :t.email.application-approved/message-to-applicant)
-          (emails-to-recipients (other-handlers event application)
-                                event application
-                                :t.email.application-approved/subject-to-handler
-                                :t.email.application-approved/message-to-handler)))
-
-(defmethod event-to-emails-impl :application.event/rejected [event application]
-  (concat (emails-to-recipients (application-util/applicant-and-members application)
-                                event application
-                                :t.email.application-rejected/subject-to-applicant
-                                :t.email.application-rejected/message-to-applicant)
-          (emails-to-recipients (other-handlers event application)
-                                event application
-                                :t.email.application-rejected/subject-to-handler
-                                :t.email.application-rejected/message-to-handler)))
-
-(defmethod event-to-emails-impl :application.event/revoked [event application]
-  (concat (emails-to-recipients (application-util/applicant-and-members application)
-                                event application
-                                :t.email.application-revoked/subject-to-applicant
-                                :t.email.application-revoked/message-to-applicant)
-          (emails-to-recipients (other-handlers event application)
-                                event application
-                                :t.email.application-revoked/subject-to-handler
-                                :t.email.application-revoked/message-to-handler)))
-
-(defmethod event-to-emails-impl :application.event/closed [event application]
-  (concat (emails-to-recipients (application-util/applicant-and-members application)
-                                event application
-                                :t.email.application-closed/subject-to-applicant
-                                :t.email.application-closed/message-to-applicant)
-          (emails-to-recipients (other-handlers event application)
-                                event application
-                                :t.email.application-closed/subject-to-handler
-                                :t.email.application-closed/message-to-handler)))
-
-(defmethod event-to-emails-impl :application.event/returned [event application]
-  (concat (emails-to-recipients [(:application/applicant application)]
-                                event application
-                                :t.email.application-returned/subject-to-applicant
-                                :t.email.application-returned/message-to-applicant)
-          (emails-to-recipients (other-handlers event application)
-                                event application
-                                :t.email.application-returned/subject-to-handler
-                                :t.email.application-returned/message-to-handler)))
-
-(defmethod event-to-emails-impl :application.event/licenses-added [event application]
-  (emails-to-recipients (application-util/applicant-and-members application)
-                        event application
-                        :t.email.application-licenses-added/subject
-                        :t.email.application-licenses-added/message))
-
-(defmethod event-to-emails-impl :application.event/submitted [event application]
-  (if (= (:event/time event)
-         (:application/first-submitted application))
-    (emails-to-recipients (handlers application)
-                          event application
-                          :t.email.application-submitted/subject
-                          :t.email.application-submitted/message)
-    (emails-to-recipients (handlers application)
-                          event application
-                          :t.email.application-resubmitted/subject
-                          :t.email.application-resubmitted/message)))
-
-(defmethod event-to-emails-impl :application.event/comment-requested [event application]
-  (emails-to-recipients (:application/commenters event)
-                        event application
-                        :t.email.comment-requested/subject
-                        :t.email.comment-requested/message))
-
-(defmethod event-to-emails-impl :application.event/commented [event application]
-  (emails-to-recipients (handlers application)
-                        event application
-                        :t.email.commented/subject
-                        :t.email.commented/message))
-
-(defmethod event-to-emails-impl :application.event/remarked [event application]
-  (emails-to-recipients (handlers application)
-                        event application
-                        :t.email.remarked/subject
-                        :t.email.remarked/message))
-
-(defmethod event-to-emails-impl :application.event/decided [event application]
-  (emails-to-recipients (handlers application)
-                        event application
-                        :t.email.decided/subject
-                        :t.email.decided/message))
-
-(defmethod event-to-emails-impl :application.event/decision-requested [event application]
-  (emails-to-recipients (:application/deciders event)
-                        event application
-                        :t.email.decision-requested/subject
-                        :t.email.decision-requested/message))
-
-(defmethod event-to-emails-impl :application.event/member-added [event application]
-  ;; TODO email to applicant? email to handler?
-  (emails-to-recipients [(:application/member event)]
-                        event application
-                        :t.email.member-added/subject
-                        :t.email.member-added/message))
-
-(defmethod event-to-emails-impl :application.event/member-invited [event application]
-  (with-language (:default-language env)
-    (fn []
-      [{:to (:email (:application/member event))
-        :subject (text-format :t.email.member-invited/subject
-                              (:name (:application/member event))
-                              (application-util/get-applicant-name application)
-                              (format-application-for-email application)
-                              (invitation-link (:invitation/token event)))
-        :body (str
-               (text-format :t.email.member-invited/message
-                            (:name (:application/member event))
-                            (application-util/get-applicant-name application)
-                            (format-application-for-email application)
-                            (invitation-link (:invitation/token event)))
-               (text :t.email/regards)
-               (text :t.email/footer))}])))
-
-;; TODO member-joined?
-
-(defn event-to-emails [event]
+(defn- event-to-emails [event]
   (when-let [app-id (:application/id event)]
-    (event-to-emails-impl (rems.application.model/enrich-event event users/get-user (constantly nil))
-                          (applications/get-unrestricted-application app-id))))
+    (template/event-to-emails (rems.application.model/enrich-event event users/get-user (constantly nil))
+                              (applications/get-unrestricted-application app-id))))
 
 (defn generate-emails! [new-events]
   (doseq [event new-events
           email (event-to-emails event)]
     (email-outbox/put! {:email email
                         :deadline (-> (time/now) (.plus ^Period (:email-retry-period env)))})))
-
-;;; Generic poller infrastructure
 
 ;;; Email poller
 
---
+++ src/clj/rems/email/template.clj
@@ -0,0 +1,195 @@
+(ns rems.email.template
+  (:require [clojure.string :as str]
+            [rems.application-util :as application-util]
+            [rems.application.model]
+            [rems.config :refer [env]]
+            [rems.context :as context]
+            [rems.db.user-settings :as user-settings]
+            [rems.text :refer [text text-format with-language]]
+            [rems.util :as util]))
+
+;;; Mapping events to emails
+
+;; TODO list of resources?
+;; TODO use real name when addressing user?
+
+;; move this to a util namespace if its needed somewhere else
+(defn- link-to-application [application-id]
+  (str (:public-url env) "application/" application-id))
+
+(defn- invitation-link [token]
+  (str (:public-url env) "accept-invitation?token=" token))
+
+(defn- format-application-for-email [application]
+  (str
+   (case (util/getx env :application-id-column)
+     :external-id (:application/external-id application)
+     :id (:application/id application))
+   (when-not (empty? (:application/description application))
+     (str ", \"" (:application/description application) "\""))))
+
+(defn- resources-for-email [application]
+  (->> (:application/resources application)
+       (map #(get-in % [:catalogue-item/title context/*lang*]))
+       (str/join ", ")))
+
+(defn- handlers [application]
+  (get-in application [:application/workflow :workflow.dynamic/handlers]))
+
+(defn- other-handlers [event application]
+  (filter #(not= (:userid %) (:event/actor event)) (handlers application)))
+
+(defmulti event-to-emails
+  (fn [event _application] (:event/type event)))
+
+(defmethod event-to-emails :default [_event _application]
+  [])
+
+(defn- emails-to-recipients [recipients event application subject-text body-text]
+  (vec
+   (for [recipient recipients]
+     (with-language (:language (user-settings/get-user-settings (:userid recipient)))
+       (fn []
+         {:to-user (:userid recipient)
+          :subject (text-format subject-text
+                                (application-util/get-member-name recipient)
+                                (application-util/get-member-name (:event/actor-attributes event))
+                                (format-application-for-email application)
+                                (application-util/get-applicant-name application)
+                                (resources-for-email application)
+                                (link-to-application (:application/id event)))
+          :body (str
+                 (text-format body-text
+                              (application-util/get-member-name recipient)
+                              (application-util/get-member-name (:event/actor-attributes event))
+                              (format-application-for-email application)
+                              (application-util/get-applicant-name application)
+                              (resources-for-email application)
+                              (link-to-application (:application/id event)))
+                 (text :t.email/regards)
+                 (text :t.email/footer))})))))
+
+(defmethod event-to-emails :application.event/approved [event application]
+  (concat (emails-to-recipients (application-util/applicant-and-members application)
+                                event application
+                                :t.email.application-approved/subject-to-applicant
+                                :t.email.application-approved/message-to-applicant)
+          (emails-to-recipients (other-handlers event application)
+                                event application
+                                :t.email.application-approved/subject-to-handler
+                                :t.email.application-approved/message-to-handler)))
+
+(defmethod event-to-emails :application.event/rejected [event application]
+  (concat (emails-to-recipients (application-util/applicant-and-members application)
+                                event application
+                                :t.email.application-rejected/subject-to-applicant
+                                :t.email.application-rejected/message-to-applicant)
+          (emails-to-recipients (other-handlers event application)
+                                event application
+                                :t.email.application-rejected/subject-to-handler
+                                :t.email.application-rejected/message-to-handler)))
+
+(defmethod event-to-emails :application.event/revoked [event application]
+  (concat (emails-to-recipients (application-util/applicant-and-members application)
+                                event application
+                                :t.email.application-revoked/subject-to-applicant
+                                :t.email.application-revoked/message-to-applicant)
+          (emails-to-recipients (other-handlers event application)
+                                event application
+                                :t.email.application-revoked/subject-to-handler
+                                :t.email.application-revoked/message-to-handler)))
+
+(defmethod event-to-emails :application.event/closed [event application]
+  (concat (emails-to-recipients (application-util/applicant-and-members application)
+                                event application
+                                :t.email.application-closed/subject-to-applicant
+                                :t.email.application-closed/message-to-applicant)
+          (emails-to-recipients (other-handlers event application)
+                                event application
+                                :t.email.application-closed/subject-to-handler
+                                :t.email.application-closed/message-to-handler)))
+
+(defmethod event-to-emails :application.event/returned [event application]
+  (concat (emails-to-recipients [(:application/applicant application)]
+                                event application
+                                :t.email.application-returned/subject-to-applicant
+                                :t.email.application-returned/message-to-applicant)
+          (emails-to-recipients (other-handlers event application)
+                                event application
+                                :t.email.application-returned/subject-to-handler
+                                :t.email.application-returned/message-to-handler)))
+
+(defmethod event-to-emails :application.event/licenses-added [event application]
+  (emails-to-recipients (application-util/applicant-and-members application)
+                        event application
+                        :t.email.application-licenses-added/subject
+                        :t.email.application-licenses-added/message))
+
+(defmethod event-to-emails :application.event/submitted [event application]
+  (if (= (:event/time event)
+         (:application/first-submitted application))
+    (emails-to-recipients (handlers application)
+                          event application
+                          :t.email.application-submitted/subject
+                          :t.email.application-submitted/message)
+    (emails-to-recipients (handlers application)
+                          event application
+                          :t.email.application-resubmitted/subject
+                          :t.email.application-resubmitted/message)))
+
+(defmethod event-to-emails :application.event/comment-requested [event application]
+  (emails-to-recipients (:application/commenters event)
+                        event application
+                        :t.email.comment-requested/subject
+                        :t.email.comment-requested/message))
+
+(defmethod event-to-emails :application.event/commented [event application]
+  (emails-to-recipients (handlers application)
+                        event application
+                        :t.email.commented/subject
+                        :t.email.commented/message))
+
+(defmethod event-to-emails :application.event/remarked [event application]
+  (emails-to-recipients (handlers application)
+                        event application
+                        :t.email.remarked/subject
+                        :t.email.remarked/message))
+
+(defmethod event-to-emails :application.event/decided [event application]
+  (emails-to-recipients (handlers application)
+                        event application
+                        :t.email.decided/subject
+                        :t.email.decided/message))
+
+(defmethod event-to-emails :application.event/decision-requested [event application]
+  (emails-to-recipients (:application/deciders event)
+                        event application
+                        :t.email.decision-requested/subject
+                        :t.email.decision-requested/message))
+
+(defmethod event-to-emails :application.event/member-added [event application]
+  ;; TODO email to applicant? email to handler?
+  (emails-to-recipients [(:application/member event)]
+                        event application
+                        :t.email.member-added/subject
+                        :t.email.member-added/message))
+
+(defmethod event-to-emails :application.event/member-invited [event application]
+  (with-language (:default-language env)
+    (fn []
+      [{:to (:email (:application/member event))
+        :subject (text-format :t.email.member-invited/subject
+                              (:name (:application/member event))
+                              (application-util/get-applicant-name application)
+                              (format-application-for-email application)
+                              (invitation-link (:invitation/token event)))
+        :body (str
+               (text-format :t.email.member-invited/message
+                            (:name (:application/member event))
+                            (application-util/get-applicant-name application)
+                            (format-application-for-email application)
+                            (invitation-link (:invitation/token event)))
+               (text :t.email/regards)
+               (text :t.email/footer))}])))
+
+;; TODO member-joined?
