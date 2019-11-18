26b31b270599db734016554178d2eb10657981cd
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-11-18 15:32:34 +0200

    refactor: factor out on-element-appear, focus-and-ensure-visible

--- src/cljs/rems/focus.cljs
+++ src/cljs/rems/focus.cljs
@@ -2,6 +2,17 @@
   "Focuses an HTML element as soon as it exists."
   (:require [rems.util :refer [visibility-ratio]]))
 
+(defn on-element-appear
+  "Do something when an element appears."
+  ([selector f]
+   (on-element-appear selector f 100))
+  ([selector f tries]
+   (when (pos? tries)
+     (if-let [element (.querySelector js/document selector)]
+       (f element)
+       (js/setTimeout #(on-element-appear selector f (dec tries))
+                      10)))))
+
 (defn- scroll-below-navigation-menu
   "Scrolls an element into view if it's behind the navigation menu."
   [element]
@@ -11,18 +22,14 @@
       (when (< element-top navbar-bottom)
         (.scrollBy js/window 0 (- element-top navbar-bottom))))))
 
+(defn focus-and-ensure-visible [element]
+  (.setAttribute element "tabindex" "-1")
+  ;; Focusing the element scrolls it into the viewport, but
+  ;; it can still be hidden behind the navigation menu.
+  (.focus element)
+  (scroll-below-navigation-menu element))
+
 (defn focus-element-async
   "Focus an element when it appears."
-  ([selector]
-   (focus-element-async selector 100))
-  ([selector tries]
-   (when (pos? tries)
-     (if-let [element (.querySelector js/document selector)]
-       (do
-         (.setAttribute element "tabindex" "-1")
-         ;; Focusing the element scrolls it into the viewport, but
-         ;; it can still be hidden behind the navigation menu.
-         (.focus element)
-         (scroll-below-navigation-menu element))
-       (js/setTimeout #(focus-element-async selector (dec tries))
-                      10)))))
+  [selector]
+  (on-element-appear selector focus-and-ensure-visible))
