4b0caf187f5b78b79cc56300e0e3e6760fb510e1
Esko Luontola <esko@luontola.fi>
2019-11-27 15:37:29 +0200

    extract rems.application.workflow1 namespace

---
+++ src/clj/rems/application/workflow1.clj
@@ -0,0 +1,170 @@
+(ns rems.application.workflow1
+  (:require [rems.permissions :as permissions]))
+
+(defmulti calculate-permissions
+  (fn [_application event] (:event/type event)))
+
+(defmethod calculate-permissions :default
+  [application _event]
+  application)
+
+(def ^:private submittable-application-commands
+  #{:application.command/save-draft
+    :application.command/submit
+    :application.command/close
+    :application.command/remove-member
+    :application.command/invite-member
+    :application.command/uninvite-member
+    :application.command/accept-licenses
+    :application.command/change-resources
+    :application.command/copy-as-new})
+
+(def ^:private non-submittable-application-commands
+  #{:application.command/remove-member
+    :application.command/uninvite-member
+    :application.command/accept-licenses
+    :application.command/copy-as-new})
+
+(def ^:private handler-all-commands
+  #{:application.command/remark
+    :application.command/add-licenses
+    :application.command/add-member
+    :application.command/change-resources
+    :application.command/remove-member
+    :application.command/invite-member
+    :application.command/uninvite-member
+    :application.command/request-comment
+    :application.command/request-decision
+    :application.command/return
+    :application.command/approve
+    :application.command/reject
+    :application.command/close})
+
+(def ^:private handler-returned-commands
+  (disj handler-all-commands
+        :application.command/return
+        :application.command/approve
+        :application.command/reject
+        :application.command/request-decision))
+
+(def ^:private created-permissions
+  {:applicant submittable-application-commands
+   :member #{:application.command/accept-licenses
+             :application.command/copy-as-new}
+   :reporter #{:see-everything}
+   ;; member before accepting an invitation
+   :everyone-else #{:application.command/accept-invitation}})
+
+(def ^:private submitted-permissions
+  {:applicant non-submittable-application-commands
+   :handler (conj handler-all-commands :see-everything)
+   :commenter #{:see-everything
+                :application.command/remark
+                :application.command/comment}
+   :past-commenter #{:see-everything
+                     :application.command/remark}
+   :decider #{:see-everything
+              :application.command/remark
+              :application.command/decide}
+   :past-decider #{:see-everything
+                   :application.command/remark}})
+
+(def ^:private returned-permissions
+  {:applicant submittable-application-commands
+   :handler (conj handler-returned-commands :see-everything)})
+
+(def ^:private approved-permissions
+  {:applicant non-submittable-application-commands
+   :handler #{:see-everything
+              :application.command/remark
+              :application.command/add-member
+              :application.command/change-resources
+              :application.command/remove-member
+              :application.command/invite-member
+              :application.command/uninvite-member
+              :application.command/close
+              :application.command/revoke}})
+
+(def ^:private closed-permissions
+  {:applicant #{:application.command/copy-as-new}
+   :member #{:application.command/copy-as-new}
+   :handler #{:see-everything
+              :application.command/remark}
+   :commenter #{:see-everything}
+   :past-commenter #{:see-everything}
+   :decider #{:see-everything}
+   :past-decider #{:see-everything}
+   :everyone-else #{}})
+
+(defmethod calculate-permissions :application.event/created
+  [application event]
+  (-> application
+      (permissions/give-role-to-users :applicant [(:event/actor event)])
+      (permissions/update-role-permissions created-permissions)))
+
+(defmethod calculate-permissions :application.event/member-added
+  [application event]
+  (-> application
+      (permissions/give-role-to-users :member [(get-in event [:application/member :userid])])))
+
+(defmethod calculate-permissions :application.event/member-joined
+  [application event]
+  (-> application
+      (permissions/give-role-to-users :member [(:event/actor event)])))
+
+(defmethod calculate-permissions :application.event/member-removed
+  [application event]
+  (-> application
+      (permissions/remove-role-from-user :member (get-in event [:application/member :userid]))))
+
+(defmethod calculate-permissions :application.event/submitted
+  [application _event]
+  (-> application
+      (permissions/update-role-permissions submitted-permissions)))
+
+(defmethod calculate-permissions :application.event/returned
+  [application _event]
+  (-> application
+      (permissions/update-role-permissions returned-permissions)))
+
+(defmethod calculate-permissions :application.event/comment-requested
+  [application event]
+  (-> application
+      (permissions/give-role-to-users :commenter (:application/commenters event))))
+
+(defmethod calculate-permissions :application.event/commented
+  [application event]
+  (-> application
+      (permissions/remove-role-from-user :commenter (:event/actor event))
+      (permissions/give-role-to-users :past-commenter [(:event/actor event)]))) ; allow to still view the application
+
+(defmethod calculate-permissions :application.event/decision-requested
+  [application event]
+  (-> application
+      (permissions/give-role-to-users :decider (:application/deciders event))))
+
+(defmethod calculate-permissions :application.event/decided
+  [application event]
+  (-> application
+      (permissions/remove-role-from-user :decider (:event/actor event))
+      (permissions/give-role-to-users :past-decider [(:event/actor event)]))) ; allow to still view the application
+
+(defmethod calculate-permissions :application.event/approved
+  [application _event]
+  (-> application
+      (permissions/update-role-permissions approved-permissions)))
+
+(defmethod calculate-permissions :application.event/rejected
+  [application _event]
+  (-> application
+      (permissions/update-role-permissions closed-permissions)))
+
+(defmethod calculate-permissions :application.event/closed
+  [application _event]
+  (-> application
+      (permissions/update-role-permissions closed-permissions)))
+
+(defmethod calculate-permissions :application.event/revoked
+  [application _event]
+  (-> application
+      (permissions/update-role-permissions closed-permissions)))
--- test/clj/rems/application/test_model.clj
+++ test/clj/rems/application/test_model.clj
@@ -1067,56 +1067,6 @@
            (model/enrich-answers {:rems.application.model/previous-submitted-answers {1 "a" 2 "b"}
                                   :rems.application.model/submitted-answers {1 "aa" 2 "bb"}})))))
 
-;;;; Tests for permissions
-
-(deftest test-calculate-permissions
-  (testing "commenter may comment only once"
-    (let [requested (reduce model/calculate-permissions nil [{:event/type :application.event/created
-                                                              :event/actor "applicant"}
-                                                             {:event/type :application.event/submitted
-                                                              :event/actor "applicant"}
-                                                             {:event/type :application.event/comment-requested
-                                                              :event/actor "handler"
-                                                              :application/commenters ["commenter1" "commenter2"]}])
-          commented (reduce model/calculate-permissions requested [{:event/type :application.event/commented
-                                                                    :event/actor "commenter1"}])]
-      (is (= #{:see-everything :application.command/comment :application.command/remark}
-             (permissions/user-permissions requested "commenter1")))
-      (is (= #{:see-everything :application.command/remark}
-             (permissions/user-permissions commented "commenter1")))
-      (is (= #{:see-everything :application.command/comment :application.command/remark}
-             (permissions/user-permissions commented "commenter2")))))
-
-  (testing "decider may decide only once"
-    (let [requested (reduce model/calculate-permissions nil [{:event/type :application.event/created
-                                                              :event/actor "applicant"}
-                                                             {:event/type :application.event/submitted
-                                                              :event/actor "applicant"}
-                                                             {:event/type :application.event/decision-requested
-                                                              :event/actor "handler"
-                                                              :application/deciders ["decider"]}])
-          decided (reduce model/calculate-permissions requested [{:event/type :application.event/decided
-                                                                  :event/actor "decider"}])]
-      (is (= #{:see-everything :application.command/decide :application.command/remark}
-             (permissions/user-permissions requested "decider")))
-      (is (= #{:see-everything :application.command/remark}
-             (permissions/user-permissions decided "decider")))))
-
-  (testing "everyone can accept invitation"
-    (let [created (reduce model/calculate-permissions nil [{:event/type :application.event/created
-                                                            :event/actor "applicant"}])]
-      (is (contains? (permissions/user-permissions created "joe")
-                     :application.command/accept-invitation))))
-  (testing "nobody can accept invitation for closed application"
-    (let [closed (reduce model/calculate-permissions nil [{:event/type :application.event/created
-                                                           :event/actor "applicant"}
-                                                          {:event/type :application.event/closed
-                                                           :event/actor "applicant"}])]
-      (is (not (contains? (permissions/user-permissions closed "joe")
-                          :application.command/accept-invitation)))
-      (is (not (contains? (permissions/user-permissions closed "applicant")
-                          :application.command/accept-invitation))))))
-
 (deftest test-apply-user-permissions
   (let [application (-> (model/application-view nil {:event/type :application.event/created
                                                      :event/actor "applicant"})
