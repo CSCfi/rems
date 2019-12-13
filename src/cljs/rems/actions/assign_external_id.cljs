45a3b729edc98c2ac47f8194e1d8abb24d296b4d
Joel Kaasinen <joel.kaasinen@nitor.fi>
2019-12-13 17:38:50 +0200

    fix: reset external id field in frontend
    also, fix some copy-paste errors...

--- src/cljs/rems/actions/assign_external_id.cljs
+++ src/cljs/rems/actions/assign_external_id.cljs
@@ -1,7 +1,8 @@
 (ns rems.actions.assign-external-id
   (:require [clojure.string :as str]
             [re-frame.core :as rf]
-            [rems.actions.action :refer [action-button action-form-view action-comment button-wrapper collapse-action-form]]
+            [rems.actions.action :refer [action-button action-form-view button-wrapper collapse-action-form]]
+            [rems.atoms :refer [textarea]]
             [rems.flash-message :as flash-message]
             [rems.text :refer [text]]
             [rems.util :refer [post!]]))
@@ -9,7 +10,7 @@
 (rf/reg-event-fx
  ::open-form
  (fn [{:keys [db]} _]
-   {:db (assoc db ::comment "")}))
+   {:db (assoc db ::external-id "")}))
 
 (rf/reg-sub ::external-id (fn [db _] (::external-id db)))
 (rf/reg-event-db ::set-external-id (fn [db [_ value]] (assoc db ::external-id value)))
@@ -41,14 +42,19 @@
   [{:keys [external-id on-set-external-id on-send]}]
   [action-form-view action-form-id
    (text :t.actions/assign-external-id)
-   [[button-wrapper {:id "review-button"
+   [[button-wrapper {:id "assign-external-id-button"
                      :text (text :t.actions/assign-external-id)
                      :class "btn-primary"
                      :on-click on-send}]]
-   [action-comment {:id action-form-id
-                    :label (text :t.actions/assign-external-id-info)
-                    :comment external-id
-                    :on-comment on-set-external-id}]])
+   (let [id (str action-form-id "-field")]
+     [:div.form-group
+      [:label {:for id}
+       (text :t.actions/assign-external-id-info)]
+      [:input.form-control {:type :text
+                            :id id
+                            :name id
+                            :value external-id
+                            :on-change #(on-set-external-id (.. % -target -value))}]])])
 
 (defn assign-external-id-form [application-id on-finished]
   (let [external-id @(rf/subscribe [::external-id])]
