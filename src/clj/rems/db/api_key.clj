d7296f4a37c0857fe41ac1783049cc3574ad0a88
Olli-Pekka Kahilakoski <olli-pekka.kahilakoski@csc.fi>
2020-01-14 10:40:54 +0200

    Distinguish +roles+ from +all-roles+, use keywords in +all-roles+

--- src/clj/rems/db/api_key.clj
+++ src/clj/rems/db/api_key.clj
@@ -2,7 +2,8 @@
   (:require [rems.db.core :as db]
             [rems.json :as json]))
 
-(def +all-roles+ ["applicant" "decider" "handler" "logged-in" "owner" "past-reviewer" "reporter" "reviewer"])
+(def +all-roles+ #{:applicant :decider :handler :logged-in
+                   :owner :past-reviewer :reporter :reviewer})
 
 (defn valid? [key]
   (not (nil? (db/get-api-key {:apikey key}))))
