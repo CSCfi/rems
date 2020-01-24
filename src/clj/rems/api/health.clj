7c8e4097415ba344c2e50bfdd76cd41ecc2c23f5
Joel Kaasinen <joel.kaasinen@nitor.fi>
2020-01-24 14:47:10 +0200

    feat: add version information to health api

--- src/clj/rems/api/health.clj
+++ src/clj/rems/api/health.clj
@@ -1,18 +1,22 @@
 (ns rems.api.health
   (:require [compojure.api.sweet :refer :all]
             [rems.db.events :as events]
+            [rems.read-gitlog]
             [ring.util.http-response :refer :all]
             [schema.core :as s])
   (:import org.joda.time.DateTime))
 
 (s/defschema Health
   {:healthy s/Bool
+   :version (s/maybe {:version s/Str
+                      :revision s/Str})
    :latest-event (s/maybe DateTime)})
 
 (defn- health []
   (let [latest (events/get-latest-event)]
     ;; unhealthy statuses are currently reported via exceptions, i.e. non-200 status codes
     {:healthy true
+     :version (rems.read-gitlog/read-current-version)
      :latest-event (:event/time latest)}))
 
 (def health-api
