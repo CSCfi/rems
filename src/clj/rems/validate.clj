811f6ae220c1e6ae8c94549bc687c5cb036b157b
Joel Kaasinen <joel.kaasinen@nitor.fi>
2020-01-31 13:58:07 +0200

    feat: warn about unrecognized organizations in db

--- src/clj/rems/validate.clj
+++ src/clj/rems/validate.clj
@@ -6,8 +6,11 @@
             [rems.application.events :as events]
             [rems.common.form :as common-form]
             [rems.config :refer [env]]
+            [rems.db.catalogue :as catalogue]
+            [rems.db.core :as db]
             [rems.db.events :as events-db]
             [rems.db.form :as form]
+            [rems.db.licenses :as licenses]
             [schema.core :as s]))
 
 (def ^:private validate-form-template
@@ -18,11 +21,31 @@
     (validate-form-template template)
     (assert (nil? (common-form/validate-form-template template (:languages env))))))
 
+(defn- valid-organization? [org]
+  (contains? (set (:organizations env)) org))
+
+(defn validate-organizations []
+  ;; only warning for now
+  (doseq [form (form/get-form-templates {})]
+    (when-not (valid-organization? (:form/organization form))
+      (log/warn "Unrecognized organization in form: " (pr-str form))))
+  ;; rems.db.resource/get-resources requires a user to be set (for forbidden-organization?)
+  (doseq [resource (db/get-resources {})]
+    (when-not (valid-organization? (:organization resource))
+      (log/warn "Unrecognized organization in resource: " (pr-str resource))))
+  (doseq [license (licenses/get-all-licenses {})]
+    (when-not (valid-organization? (:organization license))
+      (log/warn "Unrecognized organization in license: " (pr-str license))))
+  (doseq [item (catalogue/get-localized-catalogue-items)]
+    (when-not (valid-organization? (:organization item))
+      (log/warn "Unrecognized organization in catalogue item: " (pr-str item)))))
+
 (defn validate []
   (log/info "Validating data")
   (try
     (validate-forms)
     (events/validate-events (events-db/get-all-events-since 0))
+    (validate-organizations)
     (log/info "Validations passed")
     (catch Throwable t
       (log/error t "Validations failed" (with-out-str (when-let [data (ex-data t)]
