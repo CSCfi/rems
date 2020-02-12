409a5f83391cf558f709c92546376adf67b53496
Leena Wooller <leena.wooller@csc.fi>
2020-02-12 13:42:05 +0200

    Test for permissions api.

---
+++ test/clj/rems/api/test_permissions.clj
@@ -0,0 +1,33 @@
+(ns ^:integration rems.api.test-permissions
+  (:require [clojure.test :refer :all]
+            [rems.api.testing :refer :all]
+            [rems.handler :refer [handler]]
+            [ring.mock.request :refer :all]))
+
+(use-fixtures
+  :once
+  api-fixture)
+
+(deftest permissions-test
+  (testing "listing without authentication"
+    (let [response (-> (request :get (str "/api/permissions/userx"))
+                       handler)
+          body (read-body response)]
+      (is (= "unauthorized" body))))
+
+  (testing "all"
+    (let [api-key "42"
+          data (-> (request :get "/api/permissions/alice")
+                   (authenticate api-key "developer")
+                   handler
+                   read-ok-body)]
+      (is (= 1 (count data)))
+      (is (= {:ga4gh_visa_v1
+              {:type     "ControlledAccessGrants",
+               :value    "urn:nbn:fi:lb-201403262",
+               :source   "https://ga4gh.org/duri/no_org",
+               :by       "rems",
+               :asserted 1568699331
+               }}
+             (first data))))))
+
