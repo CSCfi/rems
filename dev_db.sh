912d5c933e89310b96c25bb68f1b9c4fc5e278f1
Markku Rontu <markku.rontu@iki.fi>
2020-02-22 22:56:42 +0200

    fix: add host auth method trust
    To be able to use dev postgres without password
    
    See https://github.com/docker-library/postgres/pull/658

--- dev_db.sh
+++ dev_db.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xeu
-docker run --rm --name rems_test -p 127.0.0.1:5432:5432 -d postgres:9.6
+docker run --rm --name rems_test -p 127.0.0.1:5432:5432 -e POSTGRES_HOST_AUTH_METHOD=trust -d postgres:9.6
 docker run --rm --link rems_test postgres:9.6 /bin/bash -c "while ! psql -h rems_test -U postgres -c 'select 1;' 2>/dev/null; do sleep 1; done"
 docker run -i --rm --link rems_test postgres:9.6 psql -h rems_test -U postgres < resources/sql/init.sql
 if [ -z "${FOR_TESTS:-}" ]; then
