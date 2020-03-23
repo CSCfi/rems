e147e8aec380a1ac55f7d9c9063655821ee59119
Joel Kaasinen <joel.kaasinen@nitor.fi>
2020-03-23 13:48:47 +0200

    feat: ui for adding attachments to close actions

--- src/cljs/rems/actions/close.cljs
+++ src/cljs/rems/actions/close.cljs
@@ -1,24 +1,26 @@
 (ns rems.actions.close
   (:require [re-frame.core :as rf]
-            [rems.actions.action :refer [action-button action-form-view action-comment button-wrapper command!]]
+            [rems.actions.action :refer [action-attachment action-button action-comment action-form-view button-wrapper command!]]
             [rems.text :refer [text]]))
-
-(rf/reg-event-fx
- ::open-form
- (fn [{:keys [db]} _]
-   {:db (assoc db ::comment "")}))
-
-(rf/reg-sub ::comment (fn [db _] (::comment db)))
-(rf/reg-event-db ::set-comment (fn [db [_ value]] (assoc db ::comment value)))
 
 (def ^:private action-form-id "close")
 
 (rf/reg-event-fx
+ ::open-form
+ (fn [{:keys [db]} _]
+   {:db (assoc db ::comment "")
+    :dispatch [:rems.actions.action/set-attachment-id action-form-id nil]}))
+
+(rf/reg-sub ::comment (fn [db _] (::comment db)))
+(rf/reg-event-db ::set-comment (fn [db [_ value]] (assoc db ::comment value)))
+
+(rf/reg-event-fx
  ::send-close
- (fn [_ [_ {:keys [application-id comment on-finished]}]]
+ (fn [_ [_ {:keys [application-id attachments comment on-finished]}]]
    (command! :application.command/close
              {:application-id application-id
-              :comment comment}
+              :comment comment
+              :attachments attachments}
              {:description [text :t.actions/close]
               :collapse action-form-id
               :on-finished on-finished})
@@ -30,7 +32,7 @@
                   :on-click #(rf/dispatch [::open-form])}])
 
 (defn close-view
-  [{:keys [comment show-comment-field? on-set-comment on-send]}]
+  [{:keys [application-id comment show-comment-field? on-set-comment on-send]}]
   [action-form-view action-form-id
    (text :t.actions/close)
    [[button-wrapper {:id "close"
@@ -40,16 +42,22 @@
    [:div
     (text :t.actions/close-intro)
     (when show-comment-field?
-      [action-comment {:id action-form-id
-                       :label (text :t.form/add-comments-not-shown-to-applicant)
-                       :comment comment
-                       :on-comment on-set-comment}])]])
+      [:<>
+       [action-comment {:id action-form-id
+                        :label (text :t.form/add-comments-not-shown-to-applicant)
+                        :comment comment
+                        :on-comment on-set-comment}]
+       [action-attachment {:application-id application-id
+                           :key action-form-id}]])]])
 
 (defn close-form [application-id show-comment-field? on-finished]
-  (let [comment @(rf/subscribe [::comment])]
-    [close-view {:comment comment
+  (let [comment @(rf/subscribe [::comment])
+        attachments @(rf/subscribe [:rems.actions.action/attachments action-form-id])]
+    [close-view {:application-id application-id
+                 :comment comment
                  :show-comment-field? show-comment-field?
                  :on-set-comment #(rf/dispatch [::set-comment %])
                  :on-send #(rf/dispatch [::send-close {:application-id application-id
                                                        :comment comment
+                                                       :attachments attachments
                                                        :on-finished on-finished}])}]))
