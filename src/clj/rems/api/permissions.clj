6d496b67976fe614e4a894f4e79c6622b22be760
Leena Wooller <leena.wooller@csc.fi>
2020-03-24 14:37:07 +0200

    Less confusing swagger documentation for permissions.

--- src/clj/rems/api/permissions.clj
+++ src/clj/rems/api/permissions.clj
@@ -16,7 +16,7 @@
     (GET "/:user" []
       :summary "Returns user's permissions in ga4gh visa format. See https://github.com/ga4gh-duri/ga4gh-duri.github.io/"
       :roles #{:logged-in}
-      :path-params [user :- (describe s/Str "return permissions for this user (optional), ignored if the user doesn't have appropriate privileges")]
+      :path-params [user :- (describe s/Str "return permissions for this user")]
       :query-params [{expired :- (describe s/Bool "whether to include expired permissions") false}]
       :return GetPermissionsResponse
       (ok (entitlements/get-entitlements-for-permissions-api user nil expired)))))
