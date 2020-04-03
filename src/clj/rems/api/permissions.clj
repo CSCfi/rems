29e8aa88319fe016354b799362d000d75a45bab8
Leena Hyvönen <leena.hyvonen@csc.fi>
2020-04-03 10:07:41 +0300

    Add experimental to swagger documentation for permissions endpoint

--- src/clj/rems/api/permissions.clj
+++ src/clj/rems/api/permissions.clj
@@ -15,7 +15,7 @@
     :tags ["permissions"]
 
     (GET "/:user" []
-      :summary "Returns user's permissions in ga4gh visa format. Currently signed with fake key. See https://github.com/ga4gh-duri/ga4gh-duri.github.io/"
+      :summary "Experimental. Returns user's permissions in ga4gh visa format. Currently signed with fake key. See https://github.com/ga4gh-duri/ga4gh-duri.github.io/"
       :roles #{:handler :owner}
       :path-params [user :- (describe s/Str "return permissions for this user, required")]
       :query-params [{expired :- (describe s/Bool "whether to include expired permissions") false}]
