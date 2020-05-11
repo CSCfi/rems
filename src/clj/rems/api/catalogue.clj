0de78b0256bd0fbb981b422629182729deb5f19b
Margarita Obraztsova <margaritaobraztsova@Margaritas-MacBook-Pro.local>
2020-05-11 15:53:41 +0300

    feat: Add dev config to catalogue GET route

--- src/clj/rems/api/catalogue.clj
+++ src/clj/rems/api/catalogue.clj
@@ -4,7 +4,9 @@
             [rems.api.util] ; required for route :roles
             [rems.db.catalogue :as catalogue]
             [ring.util.http-response :refer :all]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [rems.config :refer [env]]
+            [rems.api.util/check-roles :as check-roles ]))
 
 (s/defschema GetCatalogueResponse
   [CatalogueItem])
@@ -12,8 +14,10 @@
 (def catalogue-api
   (context "/catalogue" []
     :tags ["catalogue"]
-
     (GET "/" []
       :summary "Get the catalogue of items for the UI (does not include archived items)"
       :return GetCatalogueResponse
-      (ok (catalogue/get-localized-catalogue-items {:archived false})))))
+        (cond 
+          (:catalogue-is-public env) (ok (catalogue/get-localized-catalogue-items {:archived false}))
+          (check-roles)  (ok (catalogue/get-localized-catalogue-items {:archived false}))
+          :else nil ))))
