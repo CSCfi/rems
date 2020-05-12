e0f821dd293211f87952bb9171b908cb0df93ad5
Margarita Obraztsova <margaritaobraztsova@Margaritas-MacBook-Pro.local>
2020-05-12 10:45:51 +0300

    Add fix from catalogue

--- src/clj/rems/api/catalogue.clj
+++ src/clj/rems/api/catalogue.clj
@@ -4,20 +4,28 @@
             [rems.api.util] ; required for route :roles
             [rems.db.catalogue :as catalogue]
             [ring.util.http-response :refer :all]
-            [schema.core :as s]
-            [rems.config :refer [env]]
-            [rems.api.util/check-roles :as check-roles ]))
+            [schema.core :as s]))
+
+            ; [rems.config :refer [env]]
+            ; [rems.api.util/check-roles :as check-roles ]
 
 (s/defschema GetCatalogueResponse
   [CatalogueItem])
 
+; (def catalogue-api
+;   (context "/catalogue" []
+;     :tags ["catalogue"]
+;     (GET "/" []
+;       :summary "Get the catalogue of items for the UI (does not include archived items)"
+;       :return GetCatalogueResponse
+;         (ok (catalogue/get-localized-catalogue-items {:archived false})))))
+
 (def catalogue-api
   (context "/catalogue" []
     :tags ["catalogue"]
+
     (GET "/" []
       :summary "Get the catalogue of items for the UI (does not include archived items)"
+      :roles #{:logged-in}
       :return GetCatalogueResponse
-        (cond 
-          (:catalogue-is-public env) (ok (catalogue/get-localized-catalogue-items {:archived false}))
-          (check-roles)  (ok (catalogue/get-localized-catalogue-items {:archived false}))
-          :else nil ))))
+      (ok (catalogue/get-localized-catalogue-items {:archived false})))))
