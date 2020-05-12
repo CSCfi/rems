aab6b8445e4189e6d48b1243bd953235e92cbaa0
Margarita Obraztsova <margaritaobraztsova@Margaritas-MacBook-Pro.local>
2020-05-12 13:10:58 +0300

    fix: add validation to new field

--- src/clj/rems/api/catalogue.clj
+++ src/clj/rems/api/catalogue.clj
@@ -3,22 +3,16 @@
             [rems.api.schema :refer :all]
             [rems.api.util] ; required for route :roles
             [rems.db.catalogue :as catalogue]
+            [rems.roles :as roles]
+            [rems.auth.util :refer [throw-forbidden]]
             [ring.util.http-response :refer :all]
-            [schema.core :as s]))
-
-            ; [rems.config :refer [env]]
-            ; [rems.api.util/check-roles :as check-roles ]
+            [schema.core :as s]
+            [rems.config :refer [env]]
+            [rems.api.util :refer [check-roles]]))
 
 (s/defschema GetCatalogueResponse
-  [CatalogueItem])
-
-; (def catalogue-api
-;   (context "/catalogue" []
-;     :tags ["catalogue"]
-;     (GET "/" []
-;       :summary "Get the catalogue of items for the UI (does not include archived items)"
-;       :return GetCatalogueResponse
-;         (ok (catalogue/get-localized-catalogue-items {:archived false})))))
+  [CatalogueItem]
+)
 
 (def catalogue-api
   (context "/catalogue" []
@@ -26,6 +20,8 @@
 
     (GET "/" []
       :summary "Get the catalogue of items for the UI (does not include archived items)"
-      :roles #{:logged-in}
       :return GetCatalogueResponse
-      (ok (catalogue/get-localized-catalogue-items {:archived false})))))
+      (if (or (:catalogue-is-public env)
+          (roles/has-roles? :logged-in))
+        (ok (catalogue/get-localized-catalogue-items {:archived false}))
+        (throw-forbidden)))))
