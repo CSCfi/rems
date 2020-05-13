99f0943dced076f9428a41ee56177bb3a32c865b
Leena Hyvönen <leena.hyvonen@csc.fi>
2020-05-13 13:21:56 +0300

    Test for setting value for option list.

--- test/clj/rems/api/test_applications.clj
+++ test/clj/rems/api/test_applications.clj
@@ -533,6 +533,20 @@
                                                                        :fi "val"
                                                                        :sv "fri"}
                                                          :field/type :text
+                                                         :field/optional true}
+                                                        {:field/id "optionlist"
+                                                         :field/title {:en "Option list."
+                                                                       :fi "Valintalista."}
+                                                         :field/type :option
+                                                         :field/options [{:key "Option1"
+                                                                          :label {:en "First"
+                                                                                  :fi "Ensimmäinen"}}
+                                                                         {:key "Option2"
+                                                                          :label {:en "Second"
+                                                                                  :fi "Toinen"}}
+                                                                         {:key "Option3"
+                                                                          :label {:en "Third"
+                                                                                  :fi "Kolmas "}}]
                                                          :field/optional true}]})
         wf-id (test-data/create-workflow! {})
         cat-id (test-data/create-catalogue-item! {:form-id form-id :workflow-id wf-id})
@@ -562,6 +576,17 @@
                                                    {:form form-id :field "req1" :value "req"}
                                                    {:form form-id2 :field "opt2" :value "opt"}
                                                    {:form form-id2 :field "req2" :value "req"}]}))))
+
+    (testing "set existing value of option list"
+      (is (= {:success true}
+             (send-command user-id {:type :application.command/save-draft
+                                    :application-id app-id
+                                    :field-values [{:form form-id :field "opt1" :value "opt"}
+                                                   {:form form-id :field "req1" :value "req"}
+                                                   {:form form-id2 :field "opt2" :value "opt"}
+                                                   {:form form-id2 :field "req2" :value "req"}
+                                                   {:form form-id2 :field "optionlist" :value "Option2"}]}))))
+
     (testing "can submit with required field"
       (is (= {:success true}
              (send-command user-id {:type :application.command/submit
