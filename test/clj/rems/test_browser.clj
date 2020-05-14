731229285a217d006a7d11f4dc485f9e1342f258
Margarita Obraztsova <margaritaobraztsova@Margaritas-MacBook-Pro.local>
2020-05-14 10:19:44 +0300

    Uncomment tests

--- test/clj/rems/test_browser.clj
+++ test/clj/rems/test_browser.clj
@@ -450,7 +450,7 @@
                         :resid (str "browser.testing.resource/" (btu/get-seed)))
     (create-license)
     (create-resource)
-    ; (testing "create form") ; TODO
-    ; (testing "create workflow") ; TODO
-    ; (testing "create catalogue item")
+    (testing "create form") ; TODO
+    (testing "create workflow") ; TODO
+    (testing "create catalogue item")
     )) ; TODO
