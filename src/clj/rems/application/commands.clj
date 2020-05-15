b46d5435dae8deb7566f94bb083c28fd3eba7b77
Leena Hyvönen <leena.hyvonen@csc.fi>
2020-05-15 11:38:40 +0300

    Add validation to save-draft command. Tests failing, WIP

--- src/clj/rems/application/commands.clj
+++ src/clj/rems/application/commands.clj
@@ -341,9 +341,10 @@
                         [event]))))))
 
 (defmethod command-handler :application.command/save-draft
-  [cmd _application _injections]
-  (ok {:event/type :application.event/draft-saved
-       :application/field-values (:field-values cmd)}))
+  [cmd application injections]
+  (or (merge-with concat (validation-error application injections))
+      (ok {:event/type :application.event/draft-saved
+           :application/field-values (:field-values cmd)})))
 
 (defmethod command-handler :application.command/accept-licenses
   [cmd _application _injections]
