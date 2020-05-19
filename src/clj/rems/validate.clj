bc400717ad124ba4152e9d019ceca6ba85a56c1b
Markku Rontu <markku.rontu@iki.fi>
2020-05-19 10:21:22 +0300

    feat: use service layer for the validations
    These have the organizations joined etc.

--- src/clj/rems/validate.clj
+++ src/clj/rems/validate.clj
@@ -6,11 +6,12 @@
             [rems.application.events :as events]
             [rems.common.form :as common-form]
             [rems.config :refer [env]]
-            [rems.db.catalogue :as catalogue]
-            [rems.db.core :as db]
+            [rems.api.schema]
+            [rems.api.services.catalogue :as catalogue]
+            [rems.api.services.form :as form]
+            [rems.api.services.licenses :as licenses]
+            [rems.api.services.resource :as resources]
             [rems.db.events :as events-db]
-            [rems.db.form :as form]
-            [rems.db.licenses :as licenses]
             [rems.db.organizations :as organizations]
             [schema.core :as s]))
 
@@ -32,12 +33,13 @@
 
 (defn validate-organizations []
   ;; only warning for now
+  ;; NB: do not validate user organizations, they come from the idp
   (let [organizations (->> (organizations/get-organizations-raw) (map :organization/id) set)
-        valid-organization? (fn [id] (contains? organizations id))]
+        valid-organization? (fn [organization] (contains? organizations (:organization/id organization)))]
     (doseq [form (form/get-form-templates {})]
       (when-not (valid-organization? (:form/organization form))
         (log/warn "Unrecognized organization in form:" (pr-str form))))
-    (doseq [resource (db/get-resources {})]
+    (doseq [resource (resources/get-resources nil)]
       (when-not (valid-organization? (:organization resource))
         (log/warn "Unrecognized organization in resource:" (pr-str resource))))
     (doseq [license (licenses/get-all-licenses {})]
