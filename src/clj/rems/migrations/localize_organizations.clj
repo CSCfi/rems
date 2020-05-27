1fa29e50cea6f6b8580fd2c1206241c3f0f189d6
Markku Rontu <markku.rontu@iki.fi>
2020-05-27 22:18:45 +0300

    feat: generate all configured languages in migration

--- src/clj/rems/migrations/localize_organizations.clj
+++ src/clj/rems/migrations/localize_organizations.clj
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer [deftest is]]
             [hugsql.core :as hugsql]
             [rems.json :as json]
+            [rems.config :refer [env]]
             [rems.db.core :as db]))
 
 (hugsql/def-db-fns-from-string
@@ -16,46 +17,51 @@ FROM organization
 UPDATE organization SET data = :data::jsonb WHERE id = :id;
 ")
 
-(defn- migrate-organization-name [organization]
+(defn- migrate-organization-name [organization languages]
   (let [organization-name (get-in organization [:data :organization/name])]
     (if (map? organization-name)
       organization
-      (assoc-in organization [:data :organization/name] {:en organization-name}))))
+      (assoc-in organization [:data :organization/name] (into {} (for [language languages] [language organization-name]))))))
 
-(defn- migrate-organization-review-email [email]
+(defn- migrate-organization-review-email [email languages]
   (let [email-name (:name email)]
     (if (map? email-name)
       email
-      (assoc email :name {:en email-name}))))
+      (assoc email :name (into {} (for [language languages] [language email-name]))))))
 
-(defn- migrate-organization-review-emails [organization]
-  (update-in organization [:data :organization/review-emails] (partial map migrate-organization-review-email)))
+(defn- migrate-organization-review-emails [organization languages]
+  (update-in organization [:data :organization/review-emails] (partial map #(migrate-organization-review-email % languages))))
 
-(defn- migrate-organization [organization]
+(defn- migrate-organization [organization languages]
   (-> organization
-      migrate-organization-name
-      migrate-organization-review-emails))
+      (migrate-organization-name languages)
+      (migrate-organization-review-emails languages)))
 
 (deftest test-migrate-organization
   (is (= {:id "csc" :data {:something 42
-                           :organization/name {:en "CSC"}
-                           :organization/review-emails [{:name {:en "CSC Office"} :email "csc-office@csc.fi"}]}}
+                           :organization/name {:fi "CSC"
+                                               :en "CSC"}
+                           :organization/review-emails [{:name {:fi "CSC Office"
+                                                                :en "CSC Office"}
+                                                         :email "csc-office@csc.fi"}]}}
          (migrate-organization {:id "csc" :data {:something 42
                                                  :organization/name "CSC"
-                                                 :organization/review-emails [{:name "CSC Office" :email "csc-office@csc.fi"}]}}))))
+                                                 :organization/review-emails [{:name "CSC Office"
+                                                                               :email "csc-office@csc.fi"}]}}
+                               [:en :fi]))))
 
-(defn migrate-organizations! [conn organization]
+(defn migrate-organizations! [conn organization languages]
   (doseq [{:keys [data] :as organization} organization
           :let [data (json/parse-string data)
                 organization (assoc organization :data data)]
           :when (not (:organizations data))]
     (set-organization-data! conn
-                            (update (migrate-organization organization)
+                            (update (migrate-organization organization languages)
                                     :data
                                     json/generate-string))))
 
 (defn migrate-up [{:keys [conn]}]
-  (migrate-organizations! conn (get-organizations conn)))
+  (migrate-organizations! conn (get-organizations conn) (:languages env)))
 
 (comment
   (migrate-up {:conn rems.db.core/*db*}))
