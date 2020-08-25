e552ba28db379ddcc5bdea1d49e1801346e93983
Joel Kaasinen <joel.kaasinen@nitor.fi>
2020-08-25 13:01:50 +0300

    refactor: move form template serialisation to rems.db.form

--- src/clj/rems/api/services/form.clj
+++ src/clj/rems/api/services/form.clj
@@ -1,7 +1,6 @@
 (ns rems.api.services.form
   (:require [clojure.test :refer :all]
             [com.rpl.specter :refer [ALL transform]]
-            [medley.core :refer [filter-vals]]
             [rems.api.services.dependencies :as dependencies]
             [rems.api.services.util :as util]
             [rems.api.services.workflow :as workflow]
@@ -12,71 +11,13 @@
             [rems.db.core :as db]
             [rems.db.form :as form]
             [rems.db.organizations :as organizations]
-            [rems.json :as json]
-            [schema.core :as s])
-  (:import rems.InvalidRequestException))
+            [rems.json :as json]))
 
 (defn form-editable [form-id]
   (or (dependencies/in-use-error {:form/id form-id})
       {:success true}))
 
-(defn- validate-given-ids
-  "Check that `:field/id` values are distinct, not empty (or not given)."
-  [fields]
-  (let [fields-with-given-ids (filter #(contains? % :field/id) fields)
-        id-counts (frequencies (map :field/id fields-with-given-ids))
-        duplicates (keys (filter-vals #(< 1 %) id-counts))]
-    (when (some empty? (map :field/id fields-with-given-ids))
-      (throw (InvalidRequestException. "field id must not be empty")))
-    (when (seq duplicates)
-      (throw (InvalidRequestException. (pr-str duplicates))))
-    fields))
-
-(deftest test-validate-given-ids
-  (testing "when no fields or ids are given"
-    (is (= [] (validate-given-ids [])))
-    (is (= [{}] (validate-given-ids [{}])))
-    (is (= [{} {}] (validate-given-ids [{} {}])))
-    (is (= [{:foo 42} {:bar 42}] (validate-given-ids [{:foo 42} {:bar 42}]))))
-  (testing "empty id is not valid"
-    (is (thrown? InvalidRequestException
-                 (validate-given-ids [{:field/id ""}])))
-    (is (thrown? InvalidRequestException
-                 (validate-given-ids [{:field/id nil}]))))
-  (testing "when distinct ids are given"
-    (is (= [{:field/id "abc"}] (validate-given-ids [{:field/id "abc"}])))
-    (is (= [{:field/id "abc"}
-            {:field/id "xyz" :foo 42}]
-           (validate-given-ids [{:field/id "abc"}
-                                {:field/id "xyz" :foo 42}]))))
-  (testing "when duplicates are given"
-    (is (thrown? InvalidRequestException
-                 (validate-given-ids [{:field/id "abc"}
-                                      {:field/id "xyz"}
-                                      {:field/id "abc"}])))))
-
-(defn- normalize-field-definition [field]
-  (cond-> field
-    (= :public (:field/privacy field))
-    (dissoc :field/privacy)
-
-    (= :always (get-in field [:field/visibility :visibility/type]))
-    (dissoc :field/visibility)))
-
-(defn- normalize-field-definitions [fields]
-  (map normalize-field-definition fields))
-
-(def ^:private validate-fields
-  (s/validator [FieldTemplate]))
-
-(defn- serialize-fields [form]
-  (->> (:form/fields form)
-       (validate-given-ids)
-       (common-form/assign-field-ids)
-       (normalize-field-definitions)
-       (validate-fields)
-       (json/generate-string)))
-
+;; TODO should this be in rems.db.form?
 (defn- validation-error [form]
   (when-let [error-map (common-form/validate-form-template form (:languages env))]
     {:success false
@@ -86,10 +27,7 @@
   (let [organization (:organization form)]
     (util/check-allowed-organization! organization)
     (or (validation-error form)
-        (let [form-id (:id (db/save-form-template! {:organization (:organization/id organization)
-                                                    :title (:form/title form)
-                                                    :user user-id
-                                                    :fields (serialize-fields form)}))]
+        (let [form-id (form/save-form-template! user-id form)]
           ;; reset-cache! not strictly necessary since forms don't depend on anything, but here for consistency
           (dependencies/reset-cache!)
           {:success (not (nil? form-id))
@@ -109,19 +47,13 @@
        (mapv join-dependencies)))
 
 (defn edit-form! [user-id form]
-  (let [form-id (:form/id form)
-        organization (:organization form)]
-    ;; need to check both previous and new organization
-    (util/check-allowed-organization! (:organization (get-form-template form-id)))
-    (util/check-allowed-organization! organization)
-    (or (dependencies/in-use-error {:form/id form-id})
-        (validation-error form)
-        (do (db/edit-form-template! {:id form-id
-                                     :organization (:organization/id organization)
-                                     :title (:form/title form)
-                                     :user user-id
-                                     :fields (serialize-fields form)})
-            {:success true}))))
+  ;; need to check both previous and new organization
+  (util/check-allowed-organization! (:organization (get-form-template (:form/id form))))
+  (util/check-allowed-organization! (:organization form))
+  (or (dependencies/in-use-error {:form/id (:form/id form)})
+      (validation-error form)
+      (do (form/edit-form-template! user-id form)
+          {:success true})))
 
 (defn set-form-enabled! [{:keys [id enabled]}]
   (util/check-allowed-organization! (:organization (get-form-template id)))
--- src/clj/rems/db/form.clj
+++ src/clj/rems/db/form.clj
@@ -1,12 +1,14 @@
 (ns rems.db.form
   (:require [clojure.test :refer :all]
-            [medley.core :refer [map-keys]]
+            [medley.core :refer [map-keys filter-vals]]
             [rems.api.schema :refer [FieldTemplate]]
             [rems.common.form :as common-form]
             [rems.config :refer [env]]
             [rems.db.core :as db]
             [rems.json :as json]
-            [schema.coerce :as coerce]))
+            [schema.coerce :as coerce]
+            [schema.core :as s])
+  (:import rems.InvalidRequestException))
 
 (def ^:private coerce-fields
   (coerce/coercer! [FieldTemplate] coerce/string-coercion-matcher))
@@ -38,3 +40,73 @@
   (let [row (db/get-form-template {:id id})]
     (when row
       (add-validation-errors (parse-db-row row)))))
+
+(defn- validate-given-ids
+  "Check that `:field/id` values are distinct, not empty (or not given)."
+  [fields]
+  (let [fields-with-given-ids (filter #(contains? % :field/id) fields)
+        id-counts (frequencies (map :field/id fields-with-given-ids))
+        duplicates (keys (filter-vals #(< 1 %) id-counts))]
+    (when (some empty? (map :field/id fields-with-given-ids))
+      (throw (InvalidRequestException. "field id must not be empty")))
+    (when (seq duplicates)
+      (throw (InvalidRequestException. (pr-str duplicates))))
+    fields))
+
+(deftest test-validate-given-ids
+  (testing "when no fields or ids are given"
+    (is (= [] (validate-given-ids [])))
+    (is (= [{}] (validate-given-ids [{}])))
+    (is (= [{} {}] (validate-given-ids [{} {}])))
+    (is (= [{:foo 42} {:bar 42}] (validate-given-ids [{:foo 42} {:bar 42}]))))
+  (testing "empty id is not valid"
+    (is (thrown? InvalidRequestException
+                 (validate-given-ids [{:field/id ""}])))
+    (is (thrown? InvalidRequestException
+                 (validate-given-ids [{:field/id nil}]))))
+  (testing "when distinct ids are given"
+    (is (= [{:field/id "abc"}] (validate-given-ids [{:field/id "abc"}])))
+    (is (= [{:field/id "abc"}
+            {:field/id "xyz" :foo 42}]
+           (validate-given-ids [{:field/id "abc"}
+                                {:field/id "xyz" :foo 42}]))))
+  (testing "when duplicates are given"
+    (is (thrown? InvalidRequestException
+                 (validate-given-ids [{:field/id "abc"}
+                                      {:field/id "xyz"}
+                                      {:field/id "abc"}])))))
+
+(defn- normalize-field-definition [field]
+  (cond-> field
+    (= :public (:field/privacy field))
+    (dissoc :field/privacy)
+
+    (= :always (get-in field [:field/visibility :visibility/type]))
+    (dissoc :field/visibility)))
+
+(defn- normalize-field-definitions [fields]
+  (map normalize-field-definition fields))
+
+(def ^:private validate-fields
+  (s/validator [FieldTemplate]))
+
+(defn- serialize-fields [form]
+  (->> (:form/fields form)
+       (validate-given-ids)
+       (common-form/assign-field-ids)
+       (normalize-field-definitions)
+       (validate-fields)
+       (json/generate-string)))
+
+(defn save-form-template! [user-id form]
+  (:id (db/save-form-template! {:organization (:organization/id (:organization form))
+                                :title (:form/title form)
+                                :user user-id
+                                :fields (serialize-fields form)})))
+
+(defn edit-form-template! [user-id form]
+  (db/edit-form-template! {:id (:form/id form)
+                           :organization (:organization/id (:organization form))
+                           :title (:form/title form)
+                           :user user-id
+                           :fields (serialize-fields form)}))
