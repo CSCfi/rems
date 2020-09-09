afa262a8147d5ba04eb57d17380821f58daccf50
Margarita Obraztsova <marharyt.obraztsova@gmail.com>
2020-09-09 14:52:29 +0300

    test: fix tests on for the info text

--- test/clj/rems/test_browser.clj
+++ test/clj/rems/test_browser.clj
@@ -681,6 +681,9 @@
       (btu/fill-human :fields-0-placeholder-en "Placeholder (EN)")
       (btu/fill-human :fields-0-placeholder-fi "Placeholder (FI)")
       (btu/fill-human :fields-0-placeholder-sv "Placeholder (SV)")
+      (btu/fill-human :fields-0-info-text-en "Info text (EN)")
+      (btu/fill-human :fields-0-info-text-fi "Info text (FI)")
+      (btu/fill-human :fields-0-info-text-sv "Info text (SV)")
       (btu/scroll-and-click :fields-0-type-texta)
       (btu/scroll-and-click :fields-0-optional)
       (btu/fill-human :fields-0-max-length "127")
@@ -690,6 +693,9 @@
       (btu/fill-human :fields-1-title-en "Option list (EN)")
       (btu/fill-human :fields-1-title-fi "Option list (FI)")
       (btu/fill-human :fields-1-title-sv "Option list (SV)")
+      (btu/fill-human :fields-1-info-text-en "Info text (EN)")
+      (btu/fill-human :fields-1-info-text-fi "Info text (FI)")
+      (btu/fill-human :fields-1-info-text-sv "Info text (SV)")
       (btu/scroll-and-click :fields-1-type-option)
       (btu/scroll-and-click {:class :add-option})
       (btu/wait-visible :fields-1-options-0-key)
@@ -747,20 +753,20 @@
                 :form/title "Form editor test"
                 :form/fields [{:field/placeholder {:fi "" :en "" :sv ""}
                                :field/title {:fi "Description (FI)" :en "Description (EN)" :sv "Description (SV)"}
-                               :field/info-text {:en "Info test (en)", :fi "Info test (fi)", :sv "Info test (sv)"}
+                               :field/info-text {:en "Info text (EN)", :fi "Info text (FI)", :sv "Info text (SV)"}
                                :field/type "description"
                                :field/id "fld3"
                                :field/max-length nil
                                :field/optional false}
                               {:field/placeholder {:fi "Placeholder (FI)" :en "Placeholder (EN)" :sv "Placeholder (SV)"}
                                :field/title {:fi "Text area (FI)" :en "Text area (EN)" :sv "Text area (SV)"}
-                               :field/info-text {:en "Info test (en)", :fi "Info test (fi)", :sv "Info test (sv)"}
+                               :field/info-text {:en "Info text (EN)", :fi "Info text (FI)", :sv "Info text (SV)"}
                                :field/type "texta"
                                :field/id "fld1"
                                :field/max-length 127
                                :field/optional true}
                               {:field/title {:fi "Option list (FI)" :en "Option list (EN)" :sv "Option list (SV)"}
-                               :field/info-text {:en "Info test (en)", :fi "Info test (fi)", :sv "Info test (sv)"}
+                               :field/info-text {:en "Info text (EN)", :fi "Info text (FI)", :sv "Info text (SV)"}
                                :field/type "option"
                                :field/id "fld2"
                                :field/options [{:key "true" :label {:fi "Kyllä" :en "Yes" :sv "Ja"}}
