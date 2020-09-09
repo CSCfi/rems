1898af707be9d7f08bbf07da6554cd42884e2811
Leena Hyvönen <leena.hyvonen@csc.fi>
2020-09-09 11:30:16 +0300

    Add missing argument to not clause.

--- src/clj/rems/application/model.clj
+++ src/clj/rems/application/model.clj
@@ -664,15 +664,15 @@
         visible? (comp visible-ids :attachment/id)]
     (update application :application/attachments #(filterv visible? %))))
 
-(defn user-is-applicant-or-member [application user-id]
-  (let [roles (permissions/user-roles application user-id)]
-    (or (contains? roles :applicant)
-        (contains? roles :member))))
+(defn user-is-applicant-or-member [roles]
+  (or (contains? roles :applicant)
+      (contains? roles :member)))
 
 (defn see-application? [application user-id]
+  (let [permissions (permissions/user-roles application user-id)]
   (if (= :application.state/draft (:application/state application))
-    (user-is-applicant-or-member application user-id)
-    (not= #{:everyone-else})))
+    (user-is-applicant-or-member permissions)
+    (not= #{:everyone-else} permissions))))
 
 (defn apply-user-permissions [application user-id]
   (let [see-application? (see-application? application user-id)
