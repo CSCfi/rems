93d77f514365b3ce4c28fc647fb397922d281b13
Valtteri Valtia <valtteri.valtia@csc.fi>
2020-09-09 07:26:26 +0300

    Split docker actions and rahti deploy to two scripts
    Signed-off-by: Valtteri Valtia <valtteri.valtia@csc.fi>

--- .circleci/rahti-deploy.sh
+++ .circleci/rahti-deploy.sh
@@ -1,36 +1,17 @@
 #!/bin/bash
+
 set -euxo pipefail
 
 instance=${1}
 
-if [ "${instance}" == "dev" ] ; then
-    tag1=circle
-    tag2=$(git rev-parse HEAD)
-    token=${rahti_dev_pod_delete_token}
-elif [ "${instance}" == "demo" ] ; then
-    tag1=release
-    tag2=$(git describe --tags --exact-match)
+if [ "${instance}" == "demo" ] ; then
     token=${rahti_demo_pod_delete_token}
+elif [ "${instance}" == "dev" ] ; then
+    token=${rahti_dev_pod_delete_token}
 else
     echo "ERROR: Only \"dev\" and \"demo\" arguments supported!"
     echo "USAGE: ${0} [ dev | demo ]"
     exit 1
-fi
-
-docker build --pull \
-    --tag rems:latest \
-    --tag rems:${tag2} \
-    --tag docker-registry.rahti.csc.fi/rems/rems:${tag1} \
-    --tag docker-registry.rahti.csc.fi/rems/rems:${tag2} .
-
-docker login -p $rahtitoken -u unused docker-registry.rahti.csc.fi
-docker push docker-registry.rahti.csc.fi/rems/rems:${tag1}
-docker push docker-registry.rahti.csc.fi/rems/rems:${tag2}
-
-if [ "${tag1}" == "release" ] ; then
-    docker login -u remspush -p ${dockerhub}
-    docker push rems:latest
-    docker push rems:${tag2}
 fi
 
 CONTAINER=$(curl -k -s \
