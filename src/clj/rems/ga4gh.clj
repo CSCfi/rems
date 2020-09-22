da1063c2b90301ef8da034f893e1cebad2d13999
Joel Kaasinen <joel.kaasinen@nitor.fi>
2020-09-22 09:35:39 +0300

    test: unit test for ga4gh visa claim contents

--- src/clj/rems/ga4gh.clj
+++ src/clj/rems/ga4gh.clj
@@ -3,7 +3,9 @@
    <https://github.com/ga4gh-duri/ga4gh-duri.github.io/tree/master/researcher_ids>"
   (:require [clj-time.coerce]
             [clj-time.core :as time]
+            [clojure.test :refer :all]
             [rems.jwt :as jwt]
+            [rems.testing-util :refer [with-fixed-time]]
             [schema.core :as s])
   (:import org.joda.time.DateTime))
 
@@ -38,18 +40,46 @@
 
 (def +issuer+ "TODO")
 
-(defn- entitlement->visa [{:keys [resid _catappid start end _mail userid _approvedby]}]
-  ;; TODO should this send the end time also if we know it?
-  (sign-visa {:iss +issuer+
-              :sub userid
-              :iat (clj-time.coerce/to-long (time/now))
-              :exp (clj-time.coerce/to-long (or end (time/plus (time/now) +default-length+)))
+(defn- entitlement->visa-claims [{:keys [resid _catappid start end _mail userid _approvedby]}]
+  {:iss +issuer+
+   :sub userid
+   :iat (clj-time.coerce/to-long (time/now))
+   :exp (clj-time.coerce/to-long (or end (time/plus (time/now) +default-length+)))
+   :scope "openid"
+   :ga4gh_visa_v1 {:type "ControlledAccessGrants"
+                   :value (str resid)
+                   :source "https://no.organization" ;; TODO
+                   :by "system" ;; TODO or "so"?
+                   :asserted (clj-time.coerce/to-long start)}})
+
+(deftest test-entitlement->visa-claims
+  (with-fixed-time (time/date-time 2010 01 01)
+    (fn []
+      (is (= {:iss "TODO"
+              :sub "user@example.com"
+              :iat (clj-time.coerce/to-long "2010")
+              :exp (clj-time.coerce/to-long "2011")
               :scope "openid"
               :ga4gh_visa_v1 {:type "ControlledAccessGrants"
-                              :value (str resid)
-                              :source "https://no.organization" ;; TODO
-                              :by "system" ;; TODO or "so"?
-                              :asserted (clj-time.coerce/to-long start)}}))
+                              :value "urn:1234"
+                              :source "https://no.organization"
+                              :by "system"
+                              :asserted (clj-time.coerce/to-long "2009")}}
+             (entitlement->visa-claims {:resid "urn:1234" :start (time/date-time 2009) :userid "user@example.com"})))
+      (is (= {:iss "TODO"
+              :sub "user@example.com"
+              :iat (clj-time.coerce/to-long "2010")
+              :exp (clj-time.coerce/to-long "2010-06-02")
+              :scope "openid"
+              :ga4gh_visa_v1 {:type "ControlledAccessGrants"
+                              :value "urn:1234"
+                              :source "https://no.organization"
+                              :by "system"
+                              :asserted (clj-time.coerce/to-long "2009")}}
+             (entitlement->visa-claims {:resid "urn:1234" :start (time/date-time 2009) :end (time/date-time 2010 6 2) :userid "user@example.com"}))))))
+
+(defn- entitlement->visa [entitlement]
+  (sign-visa (entitlement->visa-claims entitlement)))
 
 (defn entitlements->passport [entitlements]
   {:ga4gh_passport_v1 (mapv entitlement->visa entitlements)})
