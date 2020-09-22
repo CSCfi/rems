35720747f72bcb210cf033d2df745ce951401f33
Joel Kaasinen <joel.kaasinen@nitor.fi>
2020-09-22 17:03:33 +0300

    test: typos in fill-human

--- test/clj/rems/browser_test_util.clj
+++ test/clj/rems/browser_test_util.clj
@@ -175,9 +175,15 @@
 ;; probably due to the lack of a _minimum_ delay between keypresses.
 ;; This is a reimplementation.
 (def +character-delay+ 0.1)
+(def +typo-probability+ 0.05)
 
 (defn fill-human [q text]
   (doseq [c text]
+    (when (< (rand) +typo-probability+)
+      (et/wait +character-delay+)
+      (et/fill (get-driver) q (char (inc (int c))))
+      (et/wait +character-delay+)
+      (et/fill (get-driver) q \backspace))
     (et/wait +character-delay+)
     (et/fill (get-driver) q c))
   (et/wait +character-delay+)
