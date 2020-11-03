7a83da6416fd1a3f39b082f11dcd9a1d4dfd2988
Joel Kaasinen <joel.kaasinen@nitor.fi>
2020-11-03 17:28:49 +0200

    refactor: use comment-field in rems.actions.{decide,review}

--- src/cljs/rems/actions/decide.cljs
+++ src/cljs/rems/actions/decide.cljs
@@ -1,18 +1,15 @@
 (ns rems.actions.decide
   (:require [re-frame.core :as rf]
-            [rems.actions.components :refer [action-attachment action-button comment-field-view action-form-view button-wrapper command!]]
+            [rems.actions.components :refer [action-attachment action-button comment-field action-form-view button-wrapper command!]]
             [rems.text :refer [text]]))
 
 (def ^:private action-form-id "decide")
 
 (rf/reg-event-fx
  ::open-form
- (fn [{:keys [db]} _]
-   {:db (assoc db ::comment "")
-    :dispatch [:rems.actions.components/set-attachments action-form-id []]}))
-
-(rf/reg-sub ::comment (fn [db _] (::comment db)))
-(rf/reg-event-db ::set-comment (fn [db [_ value]] (assoc db ::comment value)))
+ (fn [_ _]
+   {:dispatch-n [[:rems.actions.components/set-comment action-form-id nil]
+                 [:rems.actions.components/set-attachments action-form-id []]]}))
 
 (rf/reg-event-fx
  ::send-decide
@@ -33,7 +30,7 @@
                   :on-click #(rf/dispatch [::open-form])}])
 
 (defn decide-view
-  [{:keys [application-id comment on-set-comment on-send]}]
+  [{:keys [application-id on-send]}]
   [action-form-view action-form-id
    (text :t.actions/decide)
    [[button-wrapper {:id "decide-reject"
@@ -45,19 +42,15 @@
                      :class "btn-success"
                      :on-click #(on-send :approved)}]]
    [:<>
-    [comment-field-view {:id action-form-id
-                         :label (text :t.form/add-comments-not-shown-to-applicant)
-                         :comment comment
-                         :on-comment on-set-comment}]
-    [action-attachment {:application-id application-id
-                        :key action-form-id}]]])
+    [comment-field {:key action-form-id
+                    :label (text :t.form/add-comments-not-shown-to-applicant)}]
+    [action-attachment {:key action-form-id
+                        :application-id application-id}]]])
 
 (defn decide-form [application-id on-finished]
-  (let [comment @(rf/subscribe [::comment])
+  (let [comment @(rf/subscribe [:rems.actions.components/comment action-form-id])
         attachments @(rf/subscribe [:rems.actions.components/attachments action-form-id])]
     [decide-view {:application-id application-id
-                  :comment comment
-                  :on-set-comment #(rf/dispatch [::set-comment %])
                   :on-send #(rf/dispatch [::send-decide {:application-id application-id
                                                          :comment comment
                                                          :attachments attachments
