99386b40961351ac8b02099c6777ff33b30cba5d
Margarita <marharyta.obraztsova@gmail.com>
2020-11-13 15:12:24 +0200

    fix: wait-ivisible instead of not-visible

--- test/clj/rems/test_browser.clj
+++ test/clj/rems/test_browser.clj
@@ -897,7 +897,7 @@
           (btu/wait-visible {:tag :div :fn/has-class :info-collapse})
           (is (btu/visible? {:tag :div :fn/has-text "Info text (EN)"}))
           (btu/click-el button)
-          (is (not (btu/visible? {:tag :div :fn/has-text "Info text (EN)"})))
+          (is (btu/wait-invisible {:tag :div :fn/has-text "Info text (EN)"}))
           (change-language :fi)
           (btu/wait-visible {:tag :label :class :application-field-label :fn/has-text "Text area (FI)"})
           (is (btu/visible? {:tag :label :class :application-field-label :fn/has-text "Text area (FI)"}))
