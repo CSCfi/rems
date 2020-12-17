eaa6c96adb4fd65802e9369245dcf982c944a27b
Joel Kaasinen <joel.kaasinen@nitor.fi>
2020-12-17 09:18:27 +0200

    test: test cache-busting

--- test/clj/rems/test_handler.clj
+++ test/clj/rems/test_handler.clj
@@ -1,9 +1,18 @@
 (ns rems.test-handler
   (:require [clojure.test :refer :all]
+            [rems.api.testing :refer [read-ok-body]]
+            [rems.common.git :as git]
             [rems.handler :refer :all]
             [ring.mock.request :refer :all]))
 
-(deftest test-cache-headers
-  (let [response (-> (request :get "/")
-                     handler)]
-    (is (= "no-store" (get-in response [:headers "Cache-Control"])))))
+(deftest test-caching
+  (with-redefs [git/+version+ {:version "0.0.0" :revision "abcd1234"}]
+    (let [response (-> (request :get "/")
+                       handler)
+          body (read-ok-body response)]
+      (testing "cache header for / resource"
+        (is (= "no-store" (get-in response [:headers "Cache-Control"]))))
+      (testing "cache-busting for app.js"
+        (is (.contains body "app.js?abcd1234")))
+      (testing "cache-busting for screen.css"
+        (is (.contains body "screen.css?abcd1234"))))))
