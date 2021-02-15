75ca37146c7d2b0212aef41cee6cf8615b355d8f
Joel Kaasinen <joel.kaasinen@nitor.fi>
2021-02-15 14:40:03 +0200

    refactor: rems.form-validation
    Convert from boolean predicates to validation functions that return
    either nil or an error. This pattern is already used in e.g.
    rems.application.commands.

--- src/clj/rems/form_validation.clj
+++ src/clj/rems/form_validation.clj
@@ -9,43 +9,54 @@
     (or (= "" (:field/value field)) ; need to tolerate the default value
         (every? valid-row? (:field/value field)))))
 
-(defn- required? [field]
+(defn- required-error [field]
   (case (:field/type field)
     (:header :label)
-    false
+    nil
 
     :table
     (or
      ;; a non-optional table must have at least one row
-     (and (not (:field/optional field))
-          (empty? (:field/value field)))
+     (when (and (not (:field/optional field))
+                (empty? (:field/value field)))
+       {:field-id (:field/id field)
+        :type     :t.form.validation/required})
      ;; all tables must have all columns set for all fields
      ;; TODO we should generate a different error and not :t.form.validation/required
-     (not (all-columns-set? field)))
+     (when (not (all-columns-set? field))
+       {:field-id (:field/id field)
+       :type     :t.form.validation/required}))
 
     ;; default:
-    (and (not (:field/optional field))
-         (str/blank? (:field/value field)))))
+    (when (and (not (:field/optional field))
+               (str/blank? (:field/value field)))
+      {:field-id (:field/id field)
+       :type     :t.form.validation/required})))
 
-(defn- too-long? [field]
-  (and (:field/max-length field)
-       (> (count (:field/value field))
-          (:field/max-length field))))
+(defn- too-long-error [field]
+  (when-let [limit (:field/max-length field)]
+    (when (> (count (:field/value field)) limit)
+      {:field-id (:field/id field)
+       :type     :t.form.validation/toolong})))
 
-(defn- invalid-email-address? [field]
-  (and (= (:field/type field) :email)
-       (not (str/blank? (:field/value field)))
-       (not (re-matches +email-regex+ (:field/value field)))))
+(defn- invalid-email-address-error [field]
+  (when (= (:field/type field) :email)
+    (when-not (or (str/blank? (:field/value field))
+                  (re-matches +email-regex+ (:field/value field)))
+      {:field-id (:field/id field)
+       :type     :t.form.validation/invalid-email})))
 
 (defn- option-value-valid? [field]
   (let [allowed-values (set (conj (map :key (:field/options field)) ""))]
     (contains? allowed-values (:field/value field))))
 
-(defn- invalid-option-value? [field]
-  (and (= (:field/type field) :option)
-       (not (option-value-valid? field))))
+(defn- invalid-option-error [field]
+  (when (= (:field/type field) :option)
+    (when-not (option-value-valid? field)
+      {:field-id (:field/id field)
+       :type     :t.form.validation/invalid-value})))
 
-(defn- invalid-column-value? [field]
+(defn- missing-columns-error [field]
   (when (= (:field/type field) :table)
     (let [columns (set (map :key (:field/columns field)))
           row-ok? (fn [row] (= columns (set (map :column row))))
@@ -54,40 +65,39 @@
       ;; a [[{:column s/Str :value s/Str}]] so we don't need to check
       ;; the shape of the data here. However, the default value
       ;; for :field/value is "", which we do need to tolerate.
-      (if (string? value)
-        (not (str/blank? value))
-        (not (every? row-ok? (:field/value field)))))))
+      (when (or (and (string? value)
+                     (not (str/blank? value)))
+                (not (every? row-ok? (:field/value field))))
+        ;; TODO more specific error?
+        {:field-id (:field/id field)
+         :type     :t.form.validation/invalid-value}))))
 
 ;; TODO: validate that attachments are actually valid?
-(defn- invalid-attachment-value? [field]
-  (and (= (:field/type field) :attachment)
-       (not (every? number? (form/parse-attachment-ids (:field/value field))))))
+(defn- invalid-attachment-error [field]
+  (when (= (:field/type field) :attachment)
+    (when (not (every? number? (form/parse-attachment-ids (:field/value field))))
+      {:field-id (:field/id field)
+       :type     :t.form.validation/invalid-value})))
 
-(defn- string-required? [field]
-  (and (not= :table (:field/type field))
-       (not (string? (:field/value field)))
-       (not (nil? (:field/value field)))))
+(defn- wrong-value-type-error [field]
+  (when-not (= :table (:field/type field))
+    (when (and (not (string? (:field/value field)))
+               (not (nil? (:field/value field))))
+      ;; TODO more specific error?
+      {:field-id (:field/id field)
+       :type :t.form.validation/invalid-value})))
 
 (defn- validate-field-content [field]
-  (cond
-    (string-required? field) {:field-id (:field/id field)
-                              :type :t.form.validation/invalid-value} ; TODO better error?
-    (invalid-email-address? field) {:field-id (:field/id field)
-                                    :type     :t.form.validation/invalid-email}
-    (too-long? field) {:field-id (:field/id field)
-                       :type     :t.form.validation/toolong}
-    (invalid-option-value? field) {:field-id (:field/id field)
-                                   :type     :t.form.validation/invalid-value}
-    (invalid-column-value? field) {:field-id (:field/id field)
-                                   :type     :t.form.validation/invalid-value} ; TODO better error?
-    (invalid-attachment-value? field) {:field-id (:field/id field)
-                                       :type     :t.form.validation/invalid-value}))
+  (or (wrong-value-type-error field)
+      (invalid-email-address-error field)
+      (too-long-error field)
+      (invalid-option-error field)
+      (missing-columns-error field)
+      (invalid-attachment-error field)))
 
 (defn- validate-field-submit [field]
-  (if (required? field)
-    {:field-id (:field/id field)
-     :type     :t.form.validation/required}
-    (validate-field-content field)))
+  (or (required-error field)
+      (validate-field-content field)))
 
 (defn- validate-draft-field [field]
   (validate-field-content field))
