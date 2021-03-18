0593ed07b6456147e45fec783d23ffdec4b36d76
Joel Kaasinen <joel.kaasinen@nitor.fi>
2021-03-18 10:18:56 +0200

    feat: show previous assigned external id in assign external id ui
    We could also show the generated-external-id, but I imagine editing
    the REMS-generated id will be less common compared to assigning a
    completely new id. However once and id has been assigned, it might
    need to be edited (due to a typo?).

--- src/cljs/rems/actions/assign_external_id.cljs
+++ src/cljs/rems/actions/assign_external_id.cljs
@@ -6,8 +6,8 @@
 
 (rf/reg-event-fx
  ::open-form
- (fn [{:keys [db]} _]
-   {:db (assoc db ::external-id "")}))
+ (fn [{:keys [db]} [_ initial-id]]
+   {:db (assoc db ::external-id initial-id)}))
 
 (rf/reg-sub ::external-id (fn [db _] (::external-id db)))
 (rf/reg-event-db ::set-external-id (fn [db [_ value]] (assoc db ::external-id value)))
@@ -25,10 +25,10 @@
               :on-finished on-finished})
    {}))
 
-(defn assign-external-id-button []
+(defn assign-external-id-button [initial-id]
   [action-button {:id action-form-id
                   :text (text :t.actions/assign-external-id)
-                  :on-click #(rf/dispatch [::open-form])}])
+                  :on-click #(rf/dispatch [::open-form initial-id])}])
 
 (defn assign-external-id-view
   [{:keys [external-id on-set-external-id on-send]}]
