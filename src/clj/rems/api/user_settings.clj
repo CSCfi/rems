dfd09dbe5cd6253000b791b2fa2e47af0d4b6c58
Markku Rontu <markku.rontu@iki.fi>
2021-03-18 16:26:36 +0200

    feat: separate read, write and db model of user settings
    This keeps it backwards compatible with the assumptions of the "old way".

--- src/clj/rems/api/user_settings.clj
+++ src/clj/rems/api/user_settings.clj
@@ -3,7 +3,19 @@
             [rems.api.schema :as schema]
             [rems.db.user-settings :as user-settings]
             [rems.util :refer [getx-user-id get-user-id]]
-            [ring.util.http-response :refer :all]))
+            [ring.util.http-response :refer :all]
+            [schema.core :as s])
+  (:import (org.joda.time DateTime)))
+
+(s/defschema GetUserSettings
+  {:language s/Keyword
+   :notification-email (s/maybe s/Str)
+   (s/optional-key :ega) {:api-key-expiration-date DateTime}})
+
+(s/defschema UpdateUserSettings
+  {(s/optional-key :language) s/Keyword
+   (s/optional-key :notification-email) (s/maybe s/Str)
+   (s/optional-key :ega) {:api-key-expiration-date DateTime}})
 
 (def user-settings-api
   (context "/user-settings" []
@@ -12,12 +24,12 @@
     (GET "/" []
       :summary "Get user settings"
       :roles #{:logged-in}
-      :return user-settings/UserSettings
+      :return GetUserSettings
       (ok (user-settings/get-user-settings (get-user-id))))
 
     (PUT "/" []
       :summary "Update user settings"
       :roles #{:logged-in}
-      :body [settings user-settings/UserSettings]
+      :body [settings UpdateUserSettings]
       :return schema/SuccessResponse
       (ok (user-settings/update-user-settings! (getx-user-id) settings)))))
