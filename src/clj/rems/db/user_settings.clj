24bf0b7496ddabb8fd826158fe994f2abb2c5fc2
Markku Rontu <markku.rontu@iki.fi>
2021-04-15 08:44:02 +0300

    refactor: move key check into validate-new-settings
    Also clarify the local var naming to consistently talk about new settings.

--- src/clj/rems/db/user_settings.clj
+++ src/clj/rems/db/user_settings.clj
@@ -40,24 +40,27 @@
          (when user
            (json->settings (:settings (db/get-user-settings {:user user}))))))
 
-(defn validate-new-settings [{:keys [language notification-email] :as settings}]
-  (into {} [(when (contains? (set (:languages env)) language)
-              {:language language})
-            (when (and notification-email (re-matches +email-regex+ notification-email))
-              {:notification-email notification-email})
-            (when (and (contains? settings :notification-email)
-                       (str/blank? notification-email))
-              ;; clear notification email to use identity provider's email instead
-              {:notification-email nil})
-            (when-let [ega (:ega settings)]
-              {:ega (select-keys ega [:api-key-expiration-date])})]))
+(defn validate-new-settings [{:keys [language notification-email] :as new-settings}]
+  (let [valid-new-settings (merge (when (contains? (set (:languages env)) language)
+                                    {:language language})
+                                  (when (and notification-email (re-matches +email-regex+ notification-email))
+                                    {:notification-email notification-email})
+                                  (when (and (contains? new-settings :notification-email)
+                                             (str/blank? notification-email))
+                                    ;; clear notification email to use identity provider's email instead
+                                    {:notification-email nil})
+                                  (when-let [ega (:ega new-settings)]
+                                    {:ega (select-keys ega [:api-key-expiration-date])}))]
+    (if (= (set (keys valid-new-settings))
+           (set (keys new-settings)))
+      valid-new-settings
+      nil))) ; fail completely if there was even one error
 
 (defn update-user-settings! [user new-settings]
   (assert user "User missing!")
   (let [old-settings (get-user-settings user)
         validated (validate-new-settings new-settings)]
-    (if (= (set (keys validated))
-           (set (keys new-settings)))
+    (if validated
       (do
         (db/update-user-settings!
          {:user user
