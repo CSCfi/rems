75fd4c3694711fa5f66d0c0fce615ca2fdecc639
Markku Rontu <markku.rontu@iki.fi>
2021-06-24 12:35:20 +0300

    feat: test partial settings

--- test/clj/rems/db/test_users.clj
+++ test/clj/rems/db/test_users.clj
@@ -1,8 +1,10 @@
 (ns ^:integration rems.db.test-users
   (:require [clojure.test :refer :all]
+            [rems.db.core :as db]
             [rems.db.roles :as roles]
             [rems.db.testing :refer [rollback-db-fixture test-db-fixture]]
-            [rems.db.users :as users]))
+            [rems.db.users :as users]
+            [rems.db.user-settings :as user-settings]))
 
 (use-fixtures :once test-db-fixture)
 (use-fixtures :each rollback-db-fixture)
@@ -15,7 +17,19 @@
   (users/add-user-raw! "user-with-org" {:eppn "user-with-org"
                                         :commonName "User Org"
                                         :mail "user@org"
+                                        ;;:notification-email "user@alt"
                                         :organizations [{:organization/id "org"}]})
+
+  (testing "survives partial user settings"
+    (db/update-user-settings! {:user "user1" :settings "{\"language\": \"fi\"}"}) ; base with partial data
+
+    (is (= {:success true}
+           (user-settings/update-user-settings! "user1" {:language :en})))
+
+    (is (= {:userid "user1"
+            :name "What Ever"
+            :email nil}
+           (users/get-user "user1"))))
 
   (testing "get-raw-user-attributes"
     (is (= {:eppn "whatever"
@@ -53,6 +67,13 @@
     (roles/add-role! "user1" :owner)
     (is (= ["user1"] (users/get-users-with-role :owner)))
     (is (= [] (users/get-users-with-role :reporter))))
+
+  (testing "get-deciders"
+    (is (= #{{:userid "whatever", :name "What Ever", :email nil}
+             {:userid "user-with-org",
+              :name "User Org",
+              :email "user@org",
+              :organizations [#:organization{:id "org"}]}} (set (users/get-deciders)))))
 
   (testing "update user with add-user-raw!"
     (users/add-user-raw! "user1" {:eppn "user1"
