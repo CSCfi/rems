db2099d6c98f370a7b4f9785e85c682ba9e416d7
Markku Rontu <markku.rontu@iki.fi>
2021-10-15 17:05:18 +0300

    feat: initial Mondo code load

--- project.clj
+++ project.clj
@@ -51,6 +51,7 @@
                  [org.clojure/core.cache "1.0.207"]
                  [org.clojure/core.memoize "1.0.236"]
                  [org.clojure/data.csv "1.0.0"]
+                 [org.clojure/data.xml "0.0.8"]
                  [org.clojure/tools.cli "1.0.194"]
                  [org.clojure/tools.logging "1.1.0"]
                  [org.postgresql/postgresql "42.2.16"]
---
+++ src/clj/rems/ext/mondo.clj
@@ -0,0 +1,139 @@
+(ns rems.ext.mondo
+  "Loading and processing Mondo Disease Ontology.
+
+  See https://github.com/monarch-initiative/mondo/"
+  (:require [clojure.data.xml :as xml]
+            [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.string :as str]
+            [medley.core :refer [find-first update-existing]]
+            [mount.core :as mount]
+            [rems.config]
+            [rems.common.util :refer [build-index]]
+            [rems.github :as github]))
+
+(defn- strip-bom
+  "Strips the byte order mark (BOM) (or whatever non-characters exist)
+  from the start of the XML."
+  [s]
+  (-> s
+      str/trim
+      (str/replace-first #"^[\\W]+<" "<")))
+
+(defn- mondo-class?
+  "Is `x` an XML element representing a Mondo code?"
+  [x]
+  (and (= :xmlns.http%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23/Class (:tag x))
+       (str/starts-with? (get-in x [:attrs :xmlns.http%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23/about] "")
+                         "http://purl.obolibrary.org/obo/MONDO_")))
+
+(defn- mondo-class-deprecated?
+  "Is `x` an XML element representing something deprecated?"
+  [x]
+  (some? (some (comp #{:xmlns.http%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23/deprecated} :tag)
+               (:content x))))
+
+(defn- format-mondo-class
+  "Takes the Mondo class XML element `x` and formats it for our internal use."
+  [x]
+  (let [id (-> x :attrs :xmlns.http%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23/about)
+        label (->> x
+                   :content
+                   (find-first (comp #{:xmlns.http%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23/label} :tag))
+                   :content
+                   (str/join ""))]
+    (assert (not (str/blank? id)))
+    (assert (not (str/blank? label)) label)
+    {:id id
+     :label label}))
+
+(defn- compressed-format
+  "Takes `coll` of internal use Mondo codes and compresses
+  them to a sequence sorted by `:id`. This is useful for storage in EDN."
+  [coll]
+  (let [skip-prefix (count "http://purl.obolibrary.org/obo/MONDO_")]
+    (->> coll
+         (mapv (fn [x] (update-existing x :id #(subs % skip-prefix))))
+         (mapv (juxt :id :label))
+         (sort-by :id))))
+
+(def ^:private uninteresting-tags
+  "The OWL file contains a lot of model that we are not interested in."
+  #{:xmlns.http%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23/AnnotationProperty
+    :xmlns.http%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23/ObjectProperty
+    :xmlns.http%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23/Axiom
+    :xmlns.http%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23/NamedIndividual
+    :xmlns.http%3A%2F%2Fwww.w3.org%2F2002%2F07%2Fowl%23/Restriction
+    :xmlns.http%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23/Description})
+
+(defn- parse-mondo
+  "Parses a Mondo OWL ontology from XML string `s`.
+
+  Returns a simplified, compressed format vector."
+  [s]
+  (with-open [reader (io/reader (java.io.ByteArrayInputStream. (.getBytes s "UTF-8")))]
+    (let [root (xml/parse reader :validating false)]
+      (->> root
+           :content
+           (remove string?)
+           (remove (comp uninteresting-tags :tag))
+           (filter mondo-class?)
+           (remove mondo-class-deprecated?)
+           (mapv format-mondo-class)
+           compressed-format
+           vec))))
+
+(def ^:private supported-mondo-release-tag "the version of Mondo we support so far" "v2021-10-01")
+
+
+
+
+
+(defn- load-codes
+  "Load and index Mondo codes."
+  []
+  (->> (slurp "mondo.edn")
+       edn/read-string
+       (build-index {:keys [first]
+                     :value-fn (fn [x] {:id (first x)
+                                        :label (second x)})})))
+
+(mount/defstate code-by-id
+  :start (when (:enable-duo rems.config/env)
+           (load-codes)))
+
+
+(defn get-mondo-codes
+  []
+  (->> code-by-id
+       vals
+       (sort-by :id)))
+
+(defn join-mondo-codes
+  "Join the Mondo codes under key `k` in `x`."
+  [k x]
+  (let [unknown-value {:id "unknown"
+                       :label "unknown"}
+        code-or-default (fn [mondo] (code-by-id (:id mondo) (merge unknown-value mondo)))]
+    (update-in x [k :mondo/codes] (partial mapv code-or-default))))
+
+
+
+(comment
+  ;; Here is code to load the latest Mondo release
+  ;; check it and potentially update application.
+  ;; This is a manual process for the developers
+  ;; as a new release may bring codes that need special handling.
+
+  (def latest-release (github/fetch-releases-latest-asset "monarch-initiative/mondo" "mondo.owl"))
+
+  ;; check to see if the new release is still the
+  (assert (= supported-mondo-release-tag (:tag latest-release)))
+
+  ;; save the codes to the file
+  (->> latest-release
+       :asset
+       slurp
+       strip-bom
+       parse-mondo
+       (spit "mondo.edn")))
