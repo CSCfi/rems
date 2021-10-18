c62fd4915341e12ff93878ed03939e428eb732db
Markku Rontu <markku.rontu@iki.fi>
2021-10-18 12:10:24 +0300

    refactor: extract fetch-latest

--- src/clj/rems/github.clj
+++ src/clj/rems/github.clj
@@ -2,34 +2,35 @@
   (:require [clj-http.client :as http]
             [medley.core :refer [find-first]]))
 
+(defn- fetch-latest [repository]
+  (:body (http/get (str "https://api.github.com/repos/" repository "/releases/latest")
+                   {:accept :json
+                    :as :json})))
+
 (defn fetch-releases-latest
   "Fetches the latest release from the GitHub repository.
 
   Returns the tag and zip file."
   [repository]
-  (let [release-response (http/get (str "https://api.github.com/repos/" repository "/releases/latest")
-                                   {:accept :json
-                                    :as :json})
-        release-body (:body release-response)
-        release-tag (:tag_name release-body)
+  (let [release-body (fetch-latest repository)
         zip-response (http/get (:zipball_url release-body)
                                {:as :stream})]
-    {:tag release-tag
-     :zip (zip-response :body)}))
+    {:tag (:tag_name release-body)
+     :zip (:body zip-response)}))
 
 (defn fetch-releases-latest-asset
   "Fetches an asset from the latest release from the GitHub repository.
 
   Returns the tag and asset contents."
   [repository asset]
-  (let [release-response (http/get (str "https://api.github.com/repos/" repository "/releases/latest")
-                                   {:accept :json
-                                    :as :json})
-        release-body (:body release-response)
-        release-tag (:tag_name release-body)
+  (let [release-body (fetch-latest repository)
         asset-definition (find-first (comp #{asset} :name) (release-body :assets))
         asset-response (http/get (:browser_download_url asset-definition)
                                  {:content-type "application/octet-stream"
                                   :as :stream})]
-    {:tag release-tag
-     :asset (asset-response :body)}))
+    {:tag (:tag_name release-body)
+     :asset (:body asset-response)}))
+
+(comment
+  (:tag (fetch-releases-latest "EBISPOT/DUO"))
+  (fetch-releases-latest-asset "monarch-initiative/mondo" "mondo.owl"))
