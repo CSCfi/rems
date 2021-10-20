48333b8a590c51a6d57b6f53069656d1eece7b77
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2021-10-20 10:40:14 +0300

    chore: remove re-frame state from client keepalive

--- src/cljs/rems/keepalive.cljs
+++ src/cljs/rems/keepalive.cljs
@@ -1,29 +1,11 @@
 (ns rems.keepalive
-  (:require [cljs-time.core :as time]
-            [re-frame.core :as rf]
-            [rems.util :refer [fetch]]
+  (:require [rems.util :refer [fetch]]
             [goog.functions :refer [throttle]]))
-
-(def keepalive-interval (time/minutes 1))
 
 (defn keepalive! []
   (fetch "/api/keepalive" {:response-format nil})) ;; the response is empty
 
-(rf/reg-event-db
- ::activity
- (fn [db _]
-   (let [now (time/now)
-         deadline (get db ::next-keepalive (time/date-time 1970))]
-     (if (time/before? now deadline)
-       db
-       (do
-         (keepalive!)
-         (assoc db ::next-keepalive (time/plus now keepalive-interval)))))))
-
-(defn- keepalive-listener [_]
-  (rf/dispatch [::activity]))
-
 (defn register-keepalive-listeners! []
-  (let [throttled-keepalive-listener (throttle keepalive-listener 10000)]
+  (let [throttled-keepalive-listener (throttle keepalive! 60000)]
     (doseq [event ["mousedown", "mousemove", "keydown", "scroll", "touchstart"]]
       (.addEventListener js/document event throttled-keepalive-listener true))))
