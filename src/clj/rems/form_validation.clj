01258f634ea4d3cb0bd2c69bd207cae625db0bdd
Oscar Silva <oscar.silva@netum.fi>
2022-01-23 23:16:27 +0200

    fix: change error message format and translation path

--- src/clj/rems/form_validation.clj
+++ src/clj/rems/form_validation.clj
@@ -121,11 +121,12 @@
     (let [bound-type (:field/date-bound field)
           dt (:field/value field)
           [valid-dt? dt-error] (case bound-type
-                                 :past [time/before? :t.actions.errors/date-not-in-past]
-                                 :future [time/after? :t.actions.errors/date-not-in-future]
+                                 :past [time/before? :t.form.validation/date-not-in-past]
+                                 :future [time/after? :t.form.validation/date-not-in-future]
                                  [(constantly true) nil])]
       (when-not (valid-dt? dt (time/today-at 23 59 59))
-        {:errors [{:type dt-error}]}))))
+        {:field-id (:field/id field)
+         :type dt-error}))))
 
 (defn- wrong-value-type-error [field]
   (let [value (:field/value field)]
