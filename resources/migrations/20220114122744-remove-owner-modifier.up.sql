0091fe0916acdda30cf660e030bd263c6ebde9d4
Markku Rontu <markku.rontu@iki.fi>
2022-01-24 18:23:34 +0200

    fix: use consistent case in the migration

--- resources/migrations/20220114122744-remove-owner-modifier.up.sql
+++ resources/migrations/20220114122744-remove-owner-modifier.up.sql
@@ -1,21 +1,21 @@
 ALTER TABLE resource
-DROP COLUMN IF EXISTS owneruserid,
-DROP COLUMN IF EXISTS modifieruserid;
+DROP COLUMN IF EXISTS ownerUserId,
+DROP COLUMN IF EXISTS modifierUserId;
 --;;
 ALTER TABLE form_template
-DROP COLUMN IF EXISTS owneruserid,
-DROP COLUMN IF EXISTS modifieruserid;
+DROP COLUMN IF EXISTS ownerUserId,
+DROP COLUMN IF EXISTS modifierUserId;
 --;;
 ALTER TABLE license
-DROP COLUMN IF EXISTS owneruserid,
-DROP COLUMN IF EXISTS modifieruserid;
+DROP COLUMN IF EXISTS ownerUserId,
+DROP COLUMN IF EXISTS modifierUserId;
 --;;
 ALTER TABLE workflow
-DROP COLUMN IF EXISTS owneruserid,
-DROP COLUMN IF EXISTS modifieruserid;
+DROP COLUMN IF EXISTS ownerUserId,
+DROP COLUMN IF EXISTS modifierUserId;
 --;;
 ALTER TABLE organization
-DROP COLUMN IF EXISTS modifieruserid,
+DROP COLUMN IF EXISTS modifierUserId,
 DROP COLUMN IF EXISTS modified;
 --;;
 ALTER TABLE attachment
