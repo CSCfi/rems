0c65bf5164f35f1f74d4b78689ab388404b27df8
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2022-03-31 15:58:49 +0300

    chore: revert unnecessary change to application duo block rendering

--- src/cljs/rems/application.cljs
+++ src/cljs/rems/application.cljs
@@ -1001,8 +1001,7 @@
      [application-state application config highlight-request-id]
      [:div.mt-3 [applicants-info application]]
      (when (:enable-duo config)
-       (if (or (= userid (-> application :application/applicant :userid))
-               (some #{:member} @(rf/subscribe [:roles])))
+       (if (= userid (-> application :application/applicant :userid))
          [:div.mt-3 [edit-application-duo-codes]]
          [:div.mt-3 [application-duo-codes]]))
      [:div.mt-3 [applied-resources application userid language]]
