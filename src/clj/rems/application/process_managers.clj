e531e43183f3587b753f8f9f07fe2bdce724c734
Markku Rontu <markku.rontu@iki.fi>
2022-10-18 11:08:40 +0300

    fix: use namespace alias

--- src/clj/rems/application/process_managers.clj
+++ src/clj/rems/application/process_managers.clj
@@ -31,7 +31,7 @@
 
 (defn delete-orphan-attachments [application-id]
   (let [application (applications/get-application-internal application-id)
-        attachments-in-use (rems.api.services.attachment/get-attachments-in-use application)
+        attachments-in-use (attachment/get-attachments-in-use application)
         all-attachments (set (map :attachment/id (:application/attachments application)))]
     (doseq [attachment-id (difference all-attachments attachments-in-use)]
       (attachments/delete-attachment! attachment-id))))
