3865263e38f1d0134c8468a07b11321cb012f5f2
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2023-01-18 16:43:52 +0200

    refactor: move permissions api enabled check into handler

--- src/clj/rems/api/permissions.clj
+++ src/clj/rems/api/permissions.clj
@@ -7,11 +7,9 @@
             [schema.core :as s])
   (:import [rems NotImplementedException]))
 
-(defn- check-permissions-api-enabled [handler]
-  (fn [request]
-    (if-not (:enable-permissions-api env)
-      (throw (NotImplementedException. "Permissions API is not enabled. #{:enable-permissions-api}"))
-      (handler request))))
+(defn- permissions-api-not-enabled-error []
+  (when-not (:enable-permissions-api env)
+    (throw (NotImplementedException. "Permissions API is not enabled. #{:enable-permissions-api}"))))
 
 (s/defschema GetJWKSResponse
   {:keys [s/Any]})
@@ -23,12 +21,12 @@
   (routes
    (context "/" []
      :tags ["permissions"]
-     :middleware [check-permissions-api-enabled]
      :responses {501 {:description "Permissions API is not enabled. #{:enable-permissions-api}"}}
      (GET "/jwk" []
        :summary "Experimental. Get JSON Web Key Set (JWKS) (RFC 7517) containing the keys used for signing GA4GH Visas."
        :return GetJWKSResponse
-       (ok (get-jwks)))
+       (or (permissions-api-not-enabled-error)
+           (ok (get-jwks))))
      (GET "/permissions/:user" []
        ;; We're trying to replicate https://github.com/CSCfi/elixir-rems-proxy/#get-permissionsusername here
        :summary (str "Experimental. Returns user's permissions in ga4gh visa format. "
@@ -38,5 +36,6 @@
        :path-params [user :- (describe s/Str "return permissions for this user, required")]
        :query-params [{expired :- (describe s/Bool "whether to include expired permissions") false}]
        :return GetPermissionsResponse
-       (ok (get-user-permissions {:user user
-                                  :expired expired}))))))
+       (or (permissions-api-not-enabled-error)
+           (ok (get-user-permissions {:user user
+                                      :expired expired})))))))
