d9b781a9687d17868b242a0ee0ed2fef68496bd8
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2023-03-17 11:06:07 +0200

    deps: add jquery to karma unit tests
    js/$ is not exposed in karma tests when karma loads headless browser, and shadow-cljs project currently doesnt seem to have plans for supporting webjars.

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,9 @@
 module.exports = function (config) {
   config.set({
     browsers: ['ChromeHeadless'],
-    basePath: 'target/shadow',
-    files: ['cljs-test.js'],
+    files: [
+      'node_modules/jquery/dist/jquery.min.js', // XXX: workaround for unit tests expecting js/$
+      'target/shadow/cljs-test.js'],
     frameworks: ['cljs-test'],
     plugins: ['karma-cljs-test', 'karma-chrome-launcher', 'karma-junit-reporter'],
     reporters: ['progress', 'junit'],
