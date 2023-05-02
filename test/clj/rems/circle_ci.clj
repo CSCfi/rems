d84b879120a25f9addb3368398b510252ae04ef4
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2023-05-02 13:59:49 +0300

    refactor: clean up kaocha plugin code

--- test/clj/rems/circle_ci.clj
+++ test/clj/rems/circle_ci.clj
@@ -9,64 +9,56 @@
             [kaocha.testable :refer [test-seq]]
             [medley.core :refer [assoc-some update-existing]]))
 
-(def test-ids (atom #{}))
-
-(defn get-test-ids [test]
-  (->> (test-seq test)
-       (sequence (comp (filter (comp #{:kaocha.type/var} :kaocha.testable/type))
-                       (map :kaocha.testable/id)))))
+(def is-kaocha-var (comp #{:kaocha.type/var} :kaocha.testable/type))
 
 (defn parse-keyword [s]
   (cond-> s
     (str/starts-with? s ":") (subs 1)
     :always (keyword)))
 
-(defn get-split-test-ids! [test-plan]
+(defn split-test-ids [test-plan]
   (->> (sh/sh "circleci" "tests" "split" "--split-by=timings"
-              :in (str/join "\n" (get-test-ids test-plan)))
+              :in (str/join "\n" (for [test (test-seq test-plan)
+                                       :when (is-kaocha-var test)]
+                                   (:kaocha.testable/id test))))
        :out
        (str/split-lines)
-       (into #{} (map parse-keyword))
-       (reset! test-ids)))
+       (into #{} (map parse-keyword))))
 
-(defn exclude [test]
-  (when-some [id (:kaocha.testable/id test)]
-    (not-any? #{id} @test-ids)))
+(defn walk-kaocha-tests [m f]
+  (letfn [(recurse [tests]
+            (keep #(walk-kaocha-tests % f) tests))]
+    (some-> m
+            (f)
+            (update-existing :kaocha/tests recurse)
+            (update-existing :kaocha.test-plan/tests recurse)
+            (update-existing :kaocha.result/tests recurse))))
 
-(defn skip-excluded-tests [test]
-  (cond
-    (:kaocha.testable/skip test)
-    test
+(defn is-excluded [test test-ids]
+  (when (is-kaocha-var test)
+    (when-some [id (:kaocha.testable/id test)]
+      (not (contains? test-ids id)))))
 
-    (= :kaocha.type/var (:kaocha.testable/type test))
-    (-> test
-        (assoc-some :kaocha.testable/skip (exclude test)))
+(defn skip-excluded-testables [test-plan test-ids]
+  (-> test-plan
+      (walk-kaocha-tests (fn [suite]
+                           (if-not (:kaocha.testable/skip suite)
+                             (assoc-some suite :kaocha.testable/skip (is-excluded suite test-ids))
+                             suite)))))
 
-    :else
-    (-> test
-        (update-existing :kaocha.test-plan/tests #(map skip-excluded-tests %)))))
-
-(defn without-skipped-tests [suite]
-  (cond
-    (:kaocha.testable/skip suite)
-    false
-
-    :else
-    (-> suite
-        (update-existing :kaocha.result/tests #(filter without-skipped-tests %)))))
+(defn remove-skipped-testables [test]
+  (-> test
+      (walk-kaocha-tests (fn [suite]
+                           (when-not (:kaocha.testable/skip suite)
+                             suite)))))
 
 (defmethod p/-register :rems.circle-ci/plugin [_name plugins]
   (conj plugins
         {:kaocha.hooks/post-load
-         (fn [test-plan]
-           (get-split-test-ids! test-plan)
-           test-plan)
-
-         :kaocha.hooks/pre-test
-         (fn [test _test-plan]
-           (skip-excluded-tests test))
+         (fn [test-plan] ; skip tests that are not included in set of test ids returned by circle ci
+           (skip-excluded-testables test-plan (split-test-ids test-plan)))
 
          :kaocha.hooks/post-test
-         (fn [test _test-plan]
-           (without-skipped-tests test))}))
+         (fn [test _test-plan] ; remove skipped tests so that kaocha-junit-xml does not count them again
+           (remove-skipped-testables test))}))
 
