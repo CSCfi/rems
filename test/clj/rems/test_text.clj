3dc4889196a1be7c7029b3c2ce07253832d37881
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2023-07-04 11:57:08 +0300

    refactor: change with-language to use macro form

--- test/clj/rems/test_text.clj
+++ test/clj/rems/test_text.clj
@@ -29,11 +29,10 @@
               (map keyword)
               (sort))))
   (with-language :en
-    (fn []
-      (is (not (valid-localization? (localize-state "foobar"))))
-      (doseq [s application-util/states]
-        (testing s
-          (is (valid-localization? (localize-state s))))))))
+    (is (not (valid-localization? (localize-state "foobar"))))
+    (doseq [s application-util/states]
+      (testing s
+        (is (valid-localization? (localize-state s)))))))
 
 (deftest test-all-event-localizations
   (let [event-types (keys events/event-schemas)]
@@ -46,59 +45,57 @@
                 (map keyword)
                 (sort))))
     (with-language :en
-      (fn []
-        (is (not (valid-localization? (localize-event {:event/type "foobar"}))))
-        (doseq [event-type event-types]
-          (testing event-type
-            (is (valid-localization? (localize-event
-                                      (merge {:event/type event-type}
-                                             (when (= event-type :application.event/decided)
-                                               {:application/decision :approved})))))))))))
+      (is (not (valid-localization? (localize-event {:event/type "foobar"}))))
+      (doseq [event-type event-types]
+        (testing event-type
+          (is (valid-localization? (localize-event
+                                    (merge {:event/type event-type}
+                                           (when (= event-type :application.event/decided)
+                                             {:application/decision :approved}))))))))))
 
 (deftest test-localize-event
   ;; check some event types that have special logic
   (with-language :en
-    (fn []
-      (is (= "Bob Bond added User Urquhart to the application."
-             (localize-event {:event/type :application.event/member-added
-                              :event/actor "bob"
-                              :event/actor-attributes {:userid "bob"
-                                                       :name "Bob Bond"}
-                              :application/member {:userid "User"
-                                                   :email "user@example.com"
-                                                   :name "User Urquhart"}})))
-      (is (= "Bob Bond changed the applicant to User Urquhart."
-             (localize-event {:event/type :application.event/applicant-changed
-                              :event/actor "bob"
-                              :event/actor-attributes {:userid "bob"
-                                                       :name "Bob Bond"}
-                              :application/applicant {:userid "User"
-                                                      :email "user@example.com"
-                                                      :name "User Urquhart"}})))
-      (is (= "Bob Bond invited User Urquhart <user@example.com> to the application."
-             (localize-event {:event/type :application.event/member-invited
-                              :event/actor "bob"
-                              :event/actor-attributes {:userid "bob"
-                                                       :name "Bob Bond"}
-                              :application/member {:email "user@example.com"
-                                                   :name "User Urquhart"}})))
-      (is (= "Bob Bond invited User Urquhart <user@example.com> to review the application."
-             (localize-event {:event/type :application.event/reviewer-invited
-                              :event/actor "bob"
-                              :event/actor-attributes {:userid "bob"
-                                                       :name "Bob Bond"}
-                              :application/reviewer {:email "user@example.com"
-                                                     :name "User Urquhart"}})))
-      (is (= "Bob Bond invited User Urquhart <user@example.com> to decide."
-             (localize-event {:event/type :application.event/decider-invited
-                              :event/actor "bob"
-                              :event/actor-attributes {:userid "bob"
-                                                       :name "Bob Bond"}
-                              :application/decider {:email "user@example.com"
+    (is (= "Bob Bond added User Urquhart to the application."
+           (localize-event {:event/type :application.event/member-added
+                            :event/actor "bob"
+                            :event/actor-attributes {:userid "bob"
+                                                     :name "Bob Bond"}
+                            :application/member {:userid "User"
+                                                 :email "user@example.com"
+                                                 :name "User Urquhart"}})))
+    (is (= "Bob Bond changed the applicant to User Urquhart."
+           (localize-event {:event/type :application.event/applicant-changed
+                            :event/actor "bob"
+                            :event/actor-attributes {:userid "bob"
+                                                     :name "Bob Bond"}
+                            :application/applicant {:userid "User"
+                                                    :email "user@example.com"
                                                     :name "User Urquhart"}})))
-      (is (= "Bob Bond approved the application. Access rights end 2020-01-02."
-             (localize-event {:event/type :application.event/approved
-                              :event/actor "bob"
-                              :event/actor-attributes {:userid "bob"
-                                                       :name "Bob Bond"}
-                              :entitlement/end (time/date-time 2020 1 2)}))))))
+    (is (= "Bob Bond invited User Urquhart <user@example.com> to the application."
+           (localize-event {:event/type :application.event/member-invited
+                            :event/actor "bob"
+                            :event/actor-attributes {:userid "bob"
+                                                     :name "Bob Bond"}
+                            :application/member {:email "user@example.com"
+                                                 :name "User Urquhart"}})))
+    (is (= "Bob Bond invited User Urquhart <user@example.com> to review the application."
+           (localize-event {:event/type :application.event/reviewer-invited
+                            :event/actor "bob"
+                            :event/actor-attributes {:userid "bob"
+                                                     :name "Bob Bond"}
+                            :application/reviewer {:email "user@example.com"
+                                                   :name "User Urquhart"}})))
+    (is (= "Bob Bond invited User Urquhart <user@example.com> to decide."
+           (localize-event {:event/type :application.event/decider-invited
+                            :event/actor "bob"
+                            :event/actor-attributes {:userid "bob"
+                                                     :name "Bob Bond"}
+                            :application/decider {:email "user@example.com"
+                                                  :name "User Urquhart"}})))
+    (is (= "Bob Bond approved the application. Access rights end 2020-01-02."
+           (localize-event {:event/type :application.event/approved
+                            :event/actor "bob"
+                            :event/actor-attributes {:userid "bob"
+                                                     :name "Bob Bond"}
+                            :entitlement/end (time/date-time 2020 1 2)})))))
