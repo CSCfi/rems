30b9b27bfcb77428231533447782832916fc7aa8
Markku Rontu <markku@beta.beta.superkoodari.com>
2023-10-08 14:47:15 +0300

    feat: scheduler support for busy hours for cache reload

--- src/clj/rems/scheduler.clj
+++ src/clj/rems/scheduler.clj
@@ -1,15 +1,58 @@
 (ns rems.scheduler
-  (:require [clojure.pprint :refer [pprint]]
-            [clojure.tools.logging :as log])
+  (:require [clj-time.core :as time-core]
+            [clj-time.format :as time-format]
+            [clojure.pprint :refer [pprint]]
+            [clojure.tools.logging :as log]
+            [clojure.test :refer [deftest is]])
   (:import [java.util.concurrent ScheduledThreadPoolExecutor TimeUnit ExecutorService]
-           [org.joda.time Duration]))
+           [org.joda.time Duration Interval]))
 
-(defn ^ExecutorService start! [name f ^Duration interval]
+(defn- seconds-since-midnight [hour-minute-string]
+  (->> hour-minute-string
+       (time-format/parse (time-format/formatters :hour-minute))
+       (time-core/interval (time-core/epoch))
+       time-core/in-seconds
+       time-core/seconds))
+
+(defn- time-on-day [dt hour-minute-string]
+  (time-core/plus
+   (time-core/with-time-at-start-of-day dt)
+   (seconds-since-midnight hour-minute-string)))
+
+(defn- intervals-on-day [dt hour-minute-strings]
+  (for [[start end] hour-minute-strings]
+    (time-core/interval (time-on-day dt start)
+                        (time-on-day dt end))))
+
+(defn- time-within-intervals? [dt intervals]
+  (some? (some #(time-core/within? ^Interval % dt) intervals)))
+
+(deftest test-time-within-intervals?
+  (let [buzy-hours (intervals-on-day (time-core/date-time 2023 10 7)
+                                     [["07:00" "11:00"]
+                                      ["12:00" "17:00"]])
+        at (fn [& [h m s]] (time-core/date-time 2023 10 7 (or h 0) (or m 0) (or s 0)))]
+    (is (not (time-within-intervals? (at 0) buzy-hours)))
+    (is (not (time-within-intervals? (at 6 59 59) buzy-hours)))
+    (is (time-within-intervals? (at 7) buzy-hours))
+    (is (time-within-intervals? (at 10 59 59) buzy-hours))
+    (is (not (time-within-intervals? (at 11) buzy-hours)))
+    (is (not (time-within-intervals? (at 11 15) buzy-hours)))
+    (is (time-within-intervals? (at 12) buzy-hours))
+    (is (not (time-within-intervals? (at 17) buzy-hours)))
+    (is (not (time-within-intervals? (at 22) buzy-hours)))))
+
+(defn ^ExecutorService start! [name f ^Duration interval & [opts]]
   (let [interval-millis (.getMillis interval)
         task (fn []
                (try
                  (log/debug "Scheduler starting work")
-                 (f)
+                 (let [now (time-core/now)
+                       buzy-hours (intervals-on-day now (:buzy-hours opts))
+                       buzy-hour? (time-within-intervals? now buzy-hours)]
+                   (if buzy-hour?
+                     (log/debug "Scheduler skipping because of buzy hour" (:buzy-hours opts))
+                     (f)))
                  (log/debug "Scheduler is done")
                  (catch InterruptedException e
                    (.interrupt (Thread/currentThread))
@@ -22,6 +65,7 @@
                     (let [thread (.newThread (java.util.concurrent.Executors/defaultThreadFactory) r)]
                       (.setName thread (str name "-" (.getName thread)))
                       thread)))]
+
     (doto (ScheduledThreadPoolExecutor. 1 factory)
       (.scheduleWithFixedDelay task interval-millis interval-millis TimeUnit/MILLISECONDS))))
 
