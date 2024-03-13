98959f0eb16946fc26974c61339723e74d0f5369
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2024-03-13 15:59:46 +0200

    release: v2.36

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rems "2.35.1"
+(defproject rems "2.36"
   :description "Resource Entitlement Management System is a tool for managing access rights to resources, such as research datasets."
   :url "https://github.com/CSCfi/rems"
 
