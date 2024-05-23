b8f10c86b5c9937d22f25307518a605401a9569a
Markku Rontu <markku.rontu@iki.fi>
2024-05-23 09:23:33 +0300

    fix: merge problem

--- test/clj/rems/test_performance.clj
+++ test/clj/rems/test_performance.clj
@@ -63,10 +63,9 @@
         ;; developer can view much more applications than alice, so it takes longer to filter reviews from all apps
         test-get-todos #(doall (todos/get-todos "developer"))
         no-cache (fn []
-
                    (mount/stop #'rems.db.applications/all-applications-cache 
                                #'rems.db.events/low-level-events-cache
-                               #'rems.db.user-settings/low-level-user-settings-cache)))
+                               #'rems.db.user-settings/low-level-user-settings-cache))
         cached (fn []
                  (mount/stop #'rems.db.applications/all-applications-cache #'rems.db.events/low-level-events-cache #'rems.db.user-settings/low-level-user-settings-cache))
                  (mount/start #'rems.db.applications/all-applications-cache #'rems.db.events/low-level-events-cache #'rems.db.user-settings/low-level-user-settings-cache))
