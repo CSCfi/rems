11a8937b06d4d170f2dc8511b8a8637b196931c0
Anssi Kinnunen <anssi.kinnunen01@gmail.com>
2024-10-10 13:28:40 +0300

    fix: ensure cache thread pool is always started

--- src/clj/rems/service/caches.clj
+++ src/clj/rems/service/caches.clj
@@ -1,17 +1,23 @@
 (ns rems.service.caches
   (:require [clojure.tools.logging.readable :as logr]
+            [mount.core :as mount]
             [rems.cache :as cache]))
 
-(defn get-all-caches [] (cache/get-all-caches))
+(defn get-all-caches []
+  (mount/start #'rems.cache/dependency-loaders)
+  (cache/get-all-caches))
 
 (defn start-all-caches! []
   (logr/info :start-all)
+  (mount/start #'rems.cache/dependency-loaders)
   (run! cache/ensure-initialized! (get-all-caches)))
 
 (defn reset-all-caches! []
   (logr/info :reset-all)
+  (mount/start #'rems.cache/dependency-loaders)
   (run! cache/set-uninitialized! (get-all-caches)))
 
 (defn export-all-cache-statistics! []
   (logr/info :statistics-all)
+  (mount/start #'rems.cache/dependency-loaders)
   (into {} (map (juxt :id cache/export-statistics!)) (get-all-caches)))
