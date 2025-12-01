593f31510edb58ed42409b640b338716e871dabc
Matias Vuorio <matias.vuorio@everfull.fi>
2025-12-01 19:05:10 +0200

    fix-userid-improvements

--- src/clj/rems/db/roles.clj
+++ src/clj/rems/db/roles.clj
@@ -61,7 +61,7 @@
   (cache/evict! role-cache userid))
 
 (defn update-roles! [userid roles]
-  (->> roles 
+  (->> roles
        (filter +db-roles+)
        (run! #(db/add-role! {:user userid
                              :role (role-to-db %)}))))
