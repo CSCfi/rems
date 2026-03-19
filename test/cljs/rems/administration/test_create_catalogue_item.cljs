e1abc61ec200903d43fd09a23254c0d65b0c2377
Olli Pikarinen <pikariop@users.noreply.github.com>
2026-03-19 12:11:43 +0200

    Check that possible child items don't have a parent item, except if the parent is the item being currently edited
    Also, add tests

--- test/cljs/rems/administration/test_create_catalogue_item.cljs
+++ test/cljs/rems/administration/test_create_catalogue_item.cljs
@@ -1,9 +1,10 @@
 (ns rems.administration.test-create-catalogue-item
   (:require [clojure.test :refer [deftest is testing use-fixtures]]
             [reagent.core :as r]
-            [rems.administration.create-catalogue-item :refer [build-request]]
+            [rems.administration.create-catalogue-item :refer [build-request filter-possible-child-items]]
             [rems.globals]
-            [rems.testing :refer [init-spa-fixture]]))
+            [rems.testing :refer [init-spa-fixture set-roles!]]
+            [rems.common.roles :as roles]))
 
 (defn- languages-fixture [langs]
   (fn [f]
@@ -67,3 +68,56 @@
       (is (nil? (build-request (assoc form :workflow nil)))))
     (testing "missing resource"
       (is (nil? (build-request (assoc form :resource nil)))))))
+
+(deftest test-filter-possible-child-items
+  (let [id-1-archived {:id 1 :archived true}
+        id-2 {:id 2 :wfid 1}
+        id-3-parent {:id 3 :wfid 1 :children [{:catalogue-item/id 4}]}
+        id-4-child {:id 4 :wfid 1 :part-of {:catalogue-item/id 3}}
+        id-5 {:id 5 :wfid 1}
+        id-6-diff-wfid {:id 6 :wfid 2}
+        id-7-parent {:id 7 :wfid 1 :children [{:catalogue-item/id 8}]}
+        id-8-child {:id 8 :wfid 1 :part-of {:catalogue-item/id 7}}
+        catalogue [id-1-archived id-2 id-3-parent id-4-child id-5 id-6-diff-wfid id-7-parent id-8-child]]
+
+    (set-roles! [:owner])
+
+    (testing "with archived items"
+      (is (= []
+             (filter-possible-child-items nil [id-1-archived])
+             (filter-possible-child-items id-2 [id-1-archived id-2]))
+          "doesn't return archived items"))
+
+    (testing "with catalogue items that have children or a parent"
+      (is (= [id-2]
+             (filter-possible-child-items nil [id-2 id-3-parent])
+             (filter-possible-child-items nil [id-2 id-4-child])
+             (filter-possible-child-items nil [id-2 id-3-parent id-4-child])))
+      (is (= []
+             (filter-possible-child-items id-2 [id-2 id-3-parent id-4-child id-7-parent id-8-child]))))
+
+    (testing "with different workflow id"
+      (is (= [id-2 id-6-diff-wfid]
+             (filter-possible-child-items nil [id-2 id-6-diff-wfid]))
+          "when creating, doesn't filter")
+      (is (= [id-2 id-5 id-6-diff-wfid]
+             (filter-possible-child-items nil catalogue)))
+      (is (= [id-5]
+             (filter-possible-child-items id-2 [id-5 id-6-diff-wfid])
+             (filter-possible-child-items id-2 catalogue))
+          "when editing, filters by parent workflow id"))
+
+    (testing "when editing"
+      (is (= []
+             (filter-possible-child-items id-2 [id-2]))
+          "doesn't suggest itself (circular dependency)")
+      (is (= [id-4-child]
+             (filter-possible-child-items id-3-parent [id-4-child])
+             (filter-possible-child-items id-3-parent [id-4-child id-7-parent id-8-child]))
+          "parent item can see it's child item"))
+
+    (testing "with different organization"
+      (set-roles! [:not-the-owner])
+      (is (= []
+             (filter-possible-child-items id-2 catalogue))
+          "with no organizations in test fixtures, `roles/can-modify-organization-item?` returns false to the same effect than not having write access to the organization"))))
