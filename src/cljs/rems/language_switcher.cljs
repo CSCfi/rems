dd00f87ca5c4d2375b55b5a38e7ffa31038b3ec6
Matias Vuorio <matias.vuorio@everfull.fi>
2026-09-11 07:20:15 +0300

    Add named languages to language switcher

--- src/cljs/rems/language_switcher.cljs
+++ src/cljs/rems/language_switcher.cljs
@@ -2,13 +2,27 @@
   (:require [clojure.string :as str]
             [rems.config]
             [rems.guide-util :refer [component-info example]]
-            [rems.text :refer [text-format]]
+            [rems.text :refer [text-format text]]
             [rems.user-settings]))
 
 (defn lang-link-classes [current-language language]
   (if (= current-language language)
     "btn btn-link active"
     "btn btn-link"))
+
+(defn get-language-name [language]
+  (or (text (keyword (name :t.language-names) (name language)))
+      (str/upper-case (name language))))
+
+(defn language-button [language]
+  [:button {:type :button
+            :class (lang-link-classes @rems.config/current-language language)
+            :on-click #(rems.user-settings/save-user-language! language)
+            :aria-label (text-format :t.navigation/change-language
+                                     (get-language-name language))
+            :data-toggle "collapse"
+            :data-target ".navbar-collapse.show"}
+   (get-language-name language)])
 
 (defn language-switcher
   "Language switcher widget"
@@ -19,13 +33,7 @@
             (for [language languages]
               (let [lang-str (str/upper-case (name language))]
                 [:form.inline
-                 [:button {:type :button
-                           :class (lang-link-classes @rems.config/current-language language)
-                           :on-click #(rems.user-settings/save-user-language! language)
-                           :aria-label (text-format :t.navigation/change-language lang-str)
-                           :data-toggle "collapse"
-                           :data-target ".navbar-collapse.show"}
-                  lang-str]]))))))
+                 (language-button language)]))))))
 
 (defn guide []
   [:div
